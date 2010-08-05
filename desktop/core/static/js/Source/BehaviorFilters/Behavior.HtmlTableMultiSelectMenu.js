5d1103b2eb4b54bcac04db533dc2b38aa8180a19
Aaron T. Myers <atm@cloudera.com>
2010-08-04 22:27:15 -0700

    HUE-111. Several files in the desktop/core/static/js/Source/BehaviorFilters are missing copyright headers

--- desktop/core/static/js/Source/BehaviorFilters/Behavior.HtmlTableMultiSelectMenu.js
+++ desktop/core/static/js/Source/BehaviorFilters/Behavior.HtmlTableMultiSelectMenu.js
@@ -1,3 +1,6 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
 // regarding copyright ownership.  Cloudera, Inc. licenses this file
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
