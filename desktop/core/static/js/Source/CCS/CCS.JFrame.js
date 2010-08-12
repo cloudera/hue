c148cf8af32c289c89ae4d9d17a10af20cbe0588
Marcus McLaughlin <marcus@cloudera.com>
2010-08-12 16:48:15 -0700

    HUE-153. Problems with meta refresh handling (cherry picked from commit 56a833d66cf5251f35d352708b765edcd565a6eb)

--- desktop/core/static/js/Source/CCS/CCS.JFrame.js
+++ desktop/core/static/js/Source/CCS/CCS.JFrame.js
@@ -629,7 +629,7 @@ CCS.JFrame = new Class({
 		},
 		meta: function(data) {
 			//grab any meta tags and remove them from the html
-			data.meta = Elements.from(data.html.getTags('meta').join(' ').replace('meta', 'span'));
+			data.meta = Elements.from(data.html.getTags('meta').join(' ').replace(/(<|<\/)meta/g, '$1span'));
 			data.html = data.html.stripTags('meta');
 		},
 		elements: function(data) {
