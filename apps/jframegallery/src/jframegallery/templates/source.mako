a8d1e9a846a57c563b8371cfee98d6b46c7181ca
bc Wong <bcwalrus@cloudera.com>
2010-10-28 14:43:50 -0700

    HUE-11. jframegallery should show the html source
    - Only a small portion of the gallery have JS resource declarations.
      We should add more later.

---
+++ apps/jframegallery/src/jframegallery/templates/source.mako
@@ -0,0 +1,57 @@
+## Licensed to Cloudera, Inc. under one
+## or more contributor license agreements.  See the NOTICE file
+## distributed with this work for additional information
+## regarding copyright ownership.  Cloudera, Inc. licenses this file
+## to you under the Apache License, Version 2.0 (the
+## "License"); you may not use this file except in compliance
+## with the License.  You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+  <head>
+    <title>JFrame Gallery Source -- ${name}</title>
+    </head>
+    <body>
+      <div class="jf-src_view">
+	<div data-filters="Tabs" data-tabs-selector=".tab" data-sections-selector=".section">
+	  <div data-filters="SplitView">
+	    <div class="left_col jframe_padded">
+	      <ul>
+		<li class="tab html-tab"><a>HTML/Template</a></li>
+		%for js_name in js_data.iterkeys():
+		  <li class="tab">
+		    <a>${js_name}</a>
+		  </li>
+		%endfor
+	      </ul>
+	    </div>
+	    <div class="right_col jframe_padded">
+	      <ul class="ccs-clear">
+		## Template data
+		<li class="section">
+		  <h3>Source for HTML</h3>
+		  <hr/>
+		  ${data|n}
+		</li>
+		## JS data
+		%for js_name, js in js_data.iteritems():
+		  <li class="section">
+		    <h3>Source for <code>${js_name}</code></h3>
+		    <hr/>
+		    ${js|n}
+		  </li>
+		%endfor
+	      </ul>
+	    </div>
+	  </div>
+	</div>
+      </div>
+    </body>
+</html>
--- apps/jframegallery/static/js/Source/JFrameGallery/CCS.JFrameGallery.js
+++ apps/jframegallery/static/js/Source/JFrameGallery/CCS.JFrameGallery.js
@@ -1,12 +1,12 @@
 // Licensed to Cloudera, Inc. under one
-// or more contributor license agreements.  See the NOTICE file
+// or more contributor license agreements.	See the NOTICE file
 // distributed with this work for additional information
-// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// regarding copyright ownership.	Cloudera, Inc. licenses this file
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
+// with the License.	You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,6 +35,8 @@ CCS.JFrameGallery = new Class({
 	},
 
 	initialize: function(path, options){
+		this.parent(path || '/jframegallery/', options);
+
 		this.addEvent('load', function(){
 			this.toolbar.adopt(
 				new Element('a', {
@@ -42,8 +44,52 @@ CCS.JFrameGallery = new Class({
 					'class': 'jf-logo'
 				})
 			);
+
+			/* Inject demo/source button bar */
+			var path = this.jframe.currentPath;
+			var do_inject = false;
+			var demopath;
+			var sourcepath;
+
+			if (path.indexOf('/jframegallery/gallery') === 0) {
+				/* This is the view of a demo */
+				demopath = path;
+				sourcepath = path.replace('/jframegallery/gallery', '/jframegallery/source');
+				do_inject = true;
+			} else if (path.indexOf('/jframegallery/source') === 0) {
+				/* This is the view of a demo source */
+				demopath = path.replace('/jframegallery/source', '/jframegallery/gallery');
+				sourcepath = path;
+				do_inject = true;
+			}
+
+			if (do_inject) {
+				var buttonBar = new Element('ul', {
+					'class': 'jf-buttonbar',
+					'data-filters': 'ArtButtonBar'
+				});
+
+				var demoLink = new Element('a', {
+					href: demopath,
+					'class': 'jf-button',
+					'data-filters': 'ArtButton',
+					'text': "View Demo"
+				});
+
+				var sourceLink = new Element('a', {
+					href: sourcepath,
+					'class': 'jf-button',
+					'data-filters': 'ArtButton',
+					'text': "View Source"
+				});
+
+				buttonBar.adopt(new Element('li').adopt(demoLink));
+				buttonBar.adopt(new Element('li').adopt(sourceLink));
+				this.toolbar.adopt(buttonBar);
+				this.jframe.behavior.apply(this.toolbar);
+			}
+			sourcepath = "foo";
 		}.bind(this));
-		this.parent(path || '/jframegallery/', options);
 	}
 
 });
