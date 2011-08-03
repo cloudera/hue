e46c4a429e432b07ced1b3efdef8ae80271f441b
Jon Natkins <natty@cloudera.com>
2011-08-02 23:42:20 -0700

    HUE-575. New version of IPython is causing Jenkins build to fail

--- tools/hudson/hudson.sh
+++ tools/hudson/hudson.sh
@@ -47,7 +47,7 @@ if python -V 2>&1 | grep -q -v 2.4; then
   # Install pylint from our archive
   build/env/bin/easy_install \
     -f http://archive.cloudera.com/desktop-sdk-python-packages/ \
-    -H pypi.python.org,archive.cloudera.com pylint
+    -H archive.cloudera.com pylint
   build/env/bin/hue runpylint all -- -f parseable > PYLINT.txt
 fi
 
