bdd617f0c3059e00489efead42294342a5246725
Jon Natkins <natty@cloudera.com>
2012-02-27 13:44:58 -0800

    Command-line utility for importing users and groups from LDAP

---
+++ apps/useradmin/src/useradmin/management/commands/sync_ldap_users_and_groups.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from useradmin.views import sync_ldap_users_and_groups
+
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+  """
+  Handler for syncing the Hue database with LDAP users and groups.
+
+  This will not import any users or groups that don't already exist in Hue. All
+  user information and group memberships will be updated based on the LDAP
+  server's current state.
+  """
+
+  def handle_noargs(self, **options):
+    sync_ldap_users_and_groups()
