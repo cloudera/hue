21acb99dafab51e3036cd56ca930c0c8871cdd64
bc Wong <bcwalrus@cloudera.com>
2012-03-18 19:20:22 -0700

    [jobsub] Remove stale references in test files

--- apps/jobbrowser/src/jobbrowser/tests.py
+++ apps/jobbrowser/src/jobbrowser/tests.py
@@ -16,7 +16,6 @@
 # limitations under the License.
 
 import time
-import re
 
 from nose.tools import assert_true, assert_false, assert_equal
 from nose.plugins.skip import SkipTest
@@ -25,8 +24,6 @@ from desktop.lib.django_test_util import make_logged_in_client
 from hadoop import mini_cluster
 from jobsub.models import JobDesign
 from jobsub.tests import parse_out_id, watch_till_complete
-from jobsub.views import in_process_jobsubd
-from jobsubd.ttypes import SubmissionHandle
 from jobbrowser import models, views
 
 def test_dots_to_camel_case():
