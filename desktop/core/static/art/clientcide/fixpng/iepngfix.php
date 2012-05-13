bfc5da88d8b315bd8f1c4f61f74038dca940964b
Enrico Berti <hello@enricoberti.com>
2012-05-14 00:14:24 +0200

    Deleted unused files from Desktop (cherry picked from commit d5f2dd234d9d5c53a335b497cb8921588792f6b0)

--- desktop/core/static/art/clientcide/fixpng/iepngfix.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Use this file in your CSS in place of the .HTC file if it works offline but not online.
-// It will send the correct MIME type so that IE will execute the script correctly.
-
-header('Content-type: text/x-component');
-include('iepngfix.htc');
-
-?>
