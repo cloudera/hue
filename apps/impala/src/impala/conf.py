1561dd73e884c6369c932a252a2efed75ce5e7b9
Romain Rigaux <romain@cloudera.com>
2012-11-28 13:41:58 -0800

    [impala] Adding Impala app

---
+++ apps/impala/src/impala/conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.utils.translation import ugettext_lazy as _
+
+from desktop.lib.conf import Config
+
+
+SERVER_HOST = Config(
+  key="server_host",
+  help=_("Host of the Impala Server."),
+  private=True,
+  default="localhost")
+
+SERVER_PORT = Config(
+  key="server_port",
+  help=_("Port of the Impala Server."),
+  default=21000,
+  type=int)
