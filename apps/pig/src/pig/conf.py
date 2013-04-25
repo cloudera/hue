b276c3a68d40f202120f841b8001e019725d3edb
Romain Rigaux <romain@cloudera.com>
2013-04-25 16:41:08 +0200

    [pig] Adding config check explicting that Oozie is required
    Same for Oozie

--- apps/pig/src/pig/conf.py
+++ apps/pig/src/pig/conf.py
@@ -17,18 +17,32 @@
 
 import os
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _, ugettext_lazy as _t
 
 from desktop.lib.conf import Config
+from liboozie.conf import get_oozie_status
+
+from pig.settings import NICE_NAME
 
 
 LOCAL_SAMPLE_DIR = Config(
   key="local_data_dir",
   default=os.path.join(os.path.dirname(__file__), "..", "..", "examples"),
-  help=_("Location on local filesystem where the examples are stored."),
+  help=_t("Location on local filesystem where the examples are stored."),
   private=True)
 
 REMOTE_SAMPLE_DIR = Config(
   key="remote_data_dir",
   default="/user/hue/pig/examples",
-  help=_("Location on HDFS where the Pig examples are stored."))
+  help=_t("Location on HDFS where the Pig examples are stored."))
+
+
+def config_validator():
+  res = []
+
+  status = get_oozie_status()
+
+  if 'NORMAL' not in status:
+    res.append((NICE_NAME, _("The app won't work without a running Oozie server")))
+
+  return res
