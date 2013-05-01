b9a41d0bce28a63d03ab4c10bb7dee107daa1b42
Abraham Elmahrek <abraham@elmahrek.com>
2013-04-30 19:58:19 -0700

    HUE-1212 [fb] Incorrect trash used
    Cache home directory on a per-user basis.

--- apps/filebrowser/src/filebrowser/views.py
+++ apps/filebrowser/src/filebrowser/views.py
@@ -1029,10 +1029,8 @@ def rmtree(request):
     recurring = []
     params = ["path"]
     def bulk_rmtree(*args, **kwargs):
-        original = request.fs.setskiptrash('skip_trash' in request.GET)
         for arg in args:
-            request.fs.do_as_user(request.user, request.fs.rmtree, arg['path'])
-        request.fs.setskiptrash(original)
+            request.fs.do_as_user(request.user, request.fs.rmtree, arg['path'], 'skip_trash' in request.GET)
     return generic_op(RmTreeFormSet, request, bulk_rmtree, ["path"], None,
                       data_extractor=formset_data_extractor(recurring, params),
                       arg_extractor=formset_arg_extractor,
