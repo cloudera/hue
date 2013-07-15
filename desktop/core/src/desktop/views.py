53dc350672c596334d30a826fc16eed6162e2b55
Abraham Elmahrek <abraham@elmahrek.com>
2013-07-15 11:58:50 -0700

    [core] Fix 500 handler
    Make 500 handler more resilient by checking for exception type and value
    before showing debugger. Also, if debugger cannot be shown, show real
    500 error instead.

--- desktop/core/src/desktop/views.py
+++ desktop/core/src/desktop/views.py
@@ -239,18 +239,25 @@ def serve_404_error(request, *args, **kwargs):
 
 def serve_500_error(request, *args, **kwargs):
   """Registered handler for 500. We use the debug view to make debugging easier."""
-  exc_info = sys.exc_info()
-  if desktop.conf.HTTP_500_DEBUG_MODE.get():
-    return django.views.debug.technical_500_response(request, *exc_info)
   try:
-    return render("500.mako", request, {'traceback': traceback.extract_tb(exc_info[2])})
-  except:
-    # Fallback to technical 500 response if ours fails
+    exc_info = sys.exc_info()
+    if exc_info:
+      if desktop.conf.HTTP_500_DEBUG_MODE.get() and exc_info[0] and exc_info[1]:
+        # If (None, None, None), default server error describing why this failed.
+        return django.views.debug.technical_500_response(request, *exc_info)
+      else:
+        # Could have an empty traceback
+        return render("500.mako", request, {'traceback': traceback.extract_tb(exc_info[2])})
+    else:
+      # exc_info could be empty
+      return render("500.mako", request, {})
+  finally:
+    # Fallback to default 500 response if ours fails
     # Will end up here:
     #   - Middleware or authentication backends problems
     #   - Certain missing imports
     #   - Packaging and install issues
-    return django.views.debug.technical_500_response(request, *exc_info)
+    pass
 
 _LOG_LEVELS = {
   "critical": logging.CRITICAL,
