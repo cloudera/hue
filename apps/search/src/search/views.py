c1ab118accc594e265c20b85df579e9d98238217
Enrico Berti <hello@enricoberti.com>
2013-07-15 18:49:59 +0200

    HUE-1386 [search] Searching with non-English queries is unavailable
    Added utf8 encoding

--- apps/search/src/search/views.py
+++ apps/search/src/search/views.py
@@ -58,7 +58,7 @@ def index(request):
 
   if search_form.is_valid():
     collection_id = search_form.cleaned_data['collection']
-    solr_query['q'] = search_form.cleaned_data['query']
+    solr_query['q'] = search_form.cleaned_data['query'].encode('utf8')
     solr_query['fq'] = search_form.cleaned_data['fq']
     if search_form.cleaned_data['sort']:
       solr_query['sort'] = search_form.cleaned_data['sort']
