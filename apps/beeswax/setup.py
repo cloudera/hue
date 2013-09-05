e6fab10b93531fa909cc6dc6384421e2c98fdee5
Abraham Elmahrek <abraham@elmahrek.com>
2013-09-04 17:22:28 -0700

    [beeswax] remove SUPERVISOR_SPEC

--- apps/beeswax/setup.py
+++ apps/beeswax/setup.py
@@ -26,7 +26,6 @@ setup(
       package_dir = {'hive_metastore': 'gen-py', 'beeswaxd': 'gen-py', 'fb303': 'gen-py', 'TCLIService': 'gen-py', '': 'src'},
       install_requires = ['setuptools', 'desktop'],
       entry_points = {
-        'desktop.sdk.application': 'beeswax=beeswax',
-        'desktop.supervisor.specs': [ 'beeswax_server = beeswax:SUPERVISOR_SPEC' ],
+        'desktop.sdk.application': 'beeswax=beeswax'
       }
 )
