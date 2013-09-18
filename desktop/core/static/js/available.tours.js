69f5bcfec36a63cef5ef80ebe965852acbb48c65
Romain Rigaux <romain@cloudera.com>
2013-09-18 12:01:27 -0700

    [core] Add some tours

--- desktop/core/static/js/available.tours.js
+++ desktop/core/static/js/available.tours.js
@@ -14,22 +14,105 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-// put the tours here
 $.jHueTour({
-   "tours":[
-      {
-         "name":"pigvideo1",
-         "desc":"Preparing the data for analysis with<br/> Pig and Python UDF",
-         "path":"/pig",
-         "video": "http://player.vimeo.com/video/73849021",
-         "blog": "http://gethue.tumblr.com/post/60376973455/hadoop-tutorials-ii-1-prepare-the-data-for-analysis"
-      },
-       {
-         "name":"pigvideo2",
-         "desc":"The Pig Editor",
-         "path":"/pig",
-         "video": "http://player.vimeo.com/video/66661052"
-      }
-   ]
+  "tours":[
+    {
+      "name": "twitter",
+      "desc": "Analyze Twitter data",
+      "path": "/home",
+      "video": "",
+      "blog": "http://gethue.tumblr.com/post/48706198060/how-to-analyze-twitter-data-with-hue"
+    },
+    {
+      "name": "pigudf",
+      "desc": "Preparing Yelp data for analysis with<br/> Pig and Python UDF",
+      "path": "/(home|pig|filebrowser|metastore)",
+      "video": "http://player.vimeo.com/video/73849021",
+      "blog": "http://gethue.tumblr.com/post/60376973455/hadoop-tutorials-ii-1-prepare-the-data-for-analysis"
+    },
+    {
+      "name": "pigeditor",
+      "desc": "The Pig Editor",
+      "path": "/pig",
+      "video": "http://player.vimeo.com/video/66661052",
+      "blog": "http://gethue.tumblr.com/post/51559235973/tutorial-apache-pig-editor-in-hue-2-3"
+    },
+    {
+      "name": "hiveudf",
+      "desc": "Build and use Hive UDF in 1 minute!",
+      "path": "/beeswax",
+      "video": "http://player.vimeo.com/video/72200781",
+      "blog": "http://gethue.tumblr.com/post/58711590309/hadoop-tutorial-hive-udf-in-1-minute"
+    },
+    {
+      "name": "hcatalog",
+      "desc": "How to use HCatalog in Hue with Pig",
+      "path": "/(metastore|pig)",
+      "video": "http://player.vimeo.com/video/71024770",
+      "blog": "http://gethue.tumblr.com/post/56804308712/hadoop-tutorial-how-to-access-hive-in-pig-with"
+    },
+    {
+      "name": "hbasebrowser",
+      "desc": "HBase Browser",
+      "path": "/hbase",
+      "video": "http://player.vimeo.com/video/72357888",
+      "blog": "http://gethue.tumblr.com/post/59071544309/the-web-ui-for-hbase-hbase-browser"
+     },
+     {
+       "name": "hbasetables",
+       "desc": "Create HBase tables",
+       "path": "/hbase",
+       "video": "http://player.vimeo.com/video/72200782",
+       "blog": "http://gethue.tumblr.com/post/58181985680/hadoop-tutorial-how-to-create-example-tables-in-hbase"
+     },
+     {
+       "name": "oozieworkflows",
+       "desc": "Oozie Workflow",
+       "path": "/(oozie|beeswax)",
+       "video": "http://player.vimeo.com/video/73849021",
+       "blog": "http://gethue.tumblr.com/post/60937985689/hadoop-tutorials-ii-2-execute-hive-queries-and"
+     },
+     {
+       "name": "ooziecoordinator",
+       "desc": "Oozie Coordinator",
+       "path": "/oozie",
+       "video": "http://player.vimeo.com/video/74215175",
+       "blog": "http://gethue.tumblr.com/post/61597968730/hadoop-tutorials-ii-3-schedule-hive-queries-with"
+     },
+     {
+       "name": "search",
+       "desc": "The search app with Solr",
+       "path": "/search",
+       "video": "http://player.vimeo.com/video/68257054",
+       "blog": "http://gethue.tumblr.com/post/52804483421/tutorial-search-hadoop-in-hue"
+     },
+     {
+       "name": "filebrowser",
+       "desc": "HDFS File Operations Made Easy",
+       "path": "/filebrowser",
+       "video": "http://player.vimeo.com/video/63343487",
+       "blog": "http://gethue.tumblr.com/post/48706244836/demo-hdfs-file-operations-made-easy-with-hue"
+     },
+     {
+       "name": "hueha",
+       "desc": "High availability in Hue",
+       "path": "/about",
+       "video": "http://player.vimeo.com/video/71813732",
+       "blog": "http://gethue.tumblr.com/post/57817118455/hadoop-tutorial-high-availability-of-hue"
+     },
+     {
+       "name": "hue25",
+       "desc": "What's new in Hue 2.5",
+       "path": "/home",
+       "video": "http://player.vimeo.com/video/70955652",
+       "blog": "http://gethue.tumblr.com/post/55581863077/hue-2-5-and-its-hbase-app-is-out"
+     },
+     {
+       "name": "hueperms",
+       "desc": "Manage users and group permissions",
+       "path": "/useradmin",
+       "video": "",
+       "blog": "http://gethue.tumblr.com/post/48706063756/how-to-manage-permissions-in-hue"
+     }
+  ]
 });
