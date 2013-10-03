6a2f9066e2cb639e5b392cb49a8c5b0c419ee858
Enrico Berti <hello@enricoberti.com>
2013-10-03 20:23:00 +0200

    [site] Added Sqoop2 video

--- static/404/js/script.js
+++ static/404/js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
+  var _boomCnt = 0;
   soundManager.setup({
-    url:'static/404/swf/',
-    flashVersion:9,
-    useFlashBlock:false,
-    onready:function () {
+    url: 'static/404/swf/',
+    flashVersion: 9,
+    useFlashBlock: false,
+    onready: function () {
       var dingSound = soundManager.createSound({
-        id:'dingSound',
-        url:'static/404/audio/boom.mp3'
+        id: 'dingSound',
+        url: 'static/404/audio/boom.mp3'
       });
 
       $("html").mousedown(function () {
@@ -15,10 +16,14 @@ $(document).ready(function () {
       }).mouseup(function () {
             window.setTimeout(function () {
               $("#bell img").attr("src", "static/404/img/ellie1.png");
+              _boomCnt++;
+              if (_boomCnt > 5){
+                location.href = 'http://gethue.com';
+              }
             }, 300);
           });
     },
-    ontimeout:function () {
+    ontimeout: function () {
       alert('Booo.');
     }
   });
