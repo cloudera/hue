97f826362fa4450d9a24df9fadbadaf06ffbb001
Abraham Elmahrek <abraham@elmahrek.com>
2013-10-22 16:13:05 -0700

    HUE-1673 [core] Update desktop/settings.py to sync with django 1.4

--- desktop/core/src/desktop/settings.py
+++ desktop/core/src/desktop/settings.py
@@ -34,6 +34,7 @@ NICE_NAME = "Hue"
 ENV_HUE_PROCESS_NAME = "HUE_PROCESS_NAME"
 ENV_DESKTOP_DEBUG = "DESKTOP_DEBUG"
 
+
 ############################################################
 # Part 1: Logging and imports.
 ############################################################
@@ -66,28 +67,32 @@ desktop.log.fancy_logging()
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-#LANGUAGE_CODE = 'it'
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
 USE_L10N = True
 
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
 
 ############################################################
 # Part 3: Django configuration
 ############################################################
+
+# Additional locations of static files
+STATICFILES_DIRS = ()
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -125,6 +130,9 @@ if os.environ.get(ENV_DESKTOP_DEBUG):
   logging.debug("Will try to validate generated HTML.")
 
 ROOT_URLCONF = 'desktop.urls'
+
+# Hue runs its own wsgi applications
+WSGI_APPLICATION = None
 
 TEMPLATE_DIRS = (
     get_desktop_root("core/templates")
@@ -180,6 +188,7 @@ FILE_UPLOAD_HANDLERS = (
   'django.core.files.uploadhandler.MemoryFileUploadHandler',
   'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 )
+
 
 ############################################################
 # Part 4: Installation of apps
