suhassatish/hue-mapr

HUE-1658 [oozie] Adding backward compatibility flag
Set this to false to use the old frequency representation instead of the crontab.

[oozie]
  # Use Cron format for defining the frequency of a Coordinator instead of the old frequency number/unit.
  editor_coordinator_use_cron=false

apps/oozie/src/oozie/conf.py
@@ -19,7 +19,7 @@ import os.path
 
 from django.utils.translation import ugettext as _, ugettext_lazy as _t
 
-from desktop.lib.conf import Config
+from desktop.lib.conf import Config, coerce_bool
 from desktop.lib import paths
 from liboozie.conf import get_oozie_status
 
@@ -30,30 +30,43 @@ DEFINITION_XSLT_DIR = Config(
   key="definition_xslt_dir",
   default=os.path.join(os.path.dirname(__file__), "importlib", "xslt"),
   help=_t("Location on local FS where the xslt files are stored for workflow import."),
-  private=True)
+  private=True
+)
 
 LOCAL_SAMPLE_DIR = Config(
   key="local_data_dir",
   default=os.path.join(os.path.dirname(__file__), "..", "..", "examples"),
   help=_t("Location on local filesystem where the examples are stored."),
-  private=True)
+  private=True
+)
 
 LOCAL_SAMPLE_DATA_DIR = Config(
   key="sample_data_dir",
   default=paths.get_thirdparty_root("sample_data"),
   help=_t("Location on local filesystem where the data for the examples is stored."),
-  private=True)
+  private=True
+)
 
 REMOTE_SAMPLE_DIR = Config(
   key="remote_data_dir",
   default="/user/hue/oozie/workspaces",
-  help=_t("Location on HDFS where the Oozie workflows are stored."))
+  help=_t("Location on HDFS where the Oozie workflows are stored.")
+)
 
 OOZIE_JOBS_COUNT = Config(
   key='oozie_jobs_count',
   default=100,
   type=int,
-  help=_t('Maximum number of Oozie workflows or coodinators or bundles to retrieve in one API call.'))
+  help=_t('Maximum number of Oozie workflows or coodinators or bundles to retrieve in one API call.')
+)
+
+EDITOR_COORDINATOR_USE_CRON = Config( # Until Hue 4
+  key='editor_coordinator_use_cron',
+  default=True,
+  type=coerce_bool,
+  help=_t('Use Cron format for defining the frequency of a Coordinator instead of the old frequency number/unit.')
+)
+
 
 def config_validator(user):
   res = []

apps/oozie/src/oozie/templates/editor/gen/coordinator.xml.mako
@@ -17,6 +17,7 @@
 
 <%!
   from oozie.utils import smart_path
+  from oozie.conf import EDITOR_COORDINATOR_USE_CRON
 %>
 
 <%namespace name="common" file="workflow-common.xml.mako" />
@@ -51,7 +52,11 @@
 
 
 <coordinator-app name="${ coord.name }"
+  % if EDITOR_COORDINATOR_USE_CRON.get():
   frequency="${ coord.cron_frequency['frequency'] }"
+  % else:
+  frequency="${ coord.frequency }"
+  % endif
   start="${ coord.start_utc }" end="${ coord.end_utc }" timezone="${ coord.timezone }"
   xmlns="${ 'uri:oozie:coordinator:0.4' if coord.sla_enabled else coord.schema_version | n,unicode }"
   ${ 'xmlns:sla="uri:oozie:sla:0.2"' if coord.sla_enabled else '' | n,unicode }>

apps/oozie/src/oozie/tests.py
@@ -44,6 +44,7 @@ from liboozie.oozie_api_test import OozieServerProvider
 from liboozie.types import WorkflowList, Workflow as OozieWorkflow, Coordinator as OozieCoordinator,\
   Bundle as OozieBundle, CoordinatorList, WorkflowAction, BundleList
 
+from oozie.conf import EDITOR_COORDINATOR_USE_CRON
 from oozie.models import Workflow, Node, Kill, Link, Job, Coordinator, History,\
   find_parameters, NODE_TYPES, Bundle
 from oozie.utils import workflow_to_dict, model_to_dict, smart_path
@@ -1469,6 +1470,45 @@ class TestEditor(OozieMockBase):
 
   def test_coordinator_gen_xml(self):
     coord = create_coordinator(self.wf, self.c, self.user)
+
+    finish = EDITOR_COORDINATOR_USE_CRON.set_for_testing(False)
+
+    try:
+      assert_true(
+  """
+<coordinator-app name="MyCoord"
+  frequency="${coord:days(1)}"
+  start="2012-07-01T00:00Z" end="2012-07-04T00:00Z" timezone="America/Los_Angeles"
+  xmlns="uri:oozie:coordinator:0.2"
+  >
+  <controls>
+    <timeout>100</timeout>
+    <concurrency>3</concurrency>
+    <execution>FIFO</execution>
+    <throttle>10</throttle>
+  </controls>
+  <action>
+    <workflow>
+      <app-path>${wf_application_path}</app-path>
+      <configuration>
+        <property>
+          <name>username</name>
+          <value>${coord:user()}</value>
+        </property>
+        <property>
+          <name>SLEEP</name>
+          <value>1000</value>
+        </property>
+        <property>
+          <name>market</name>
+          <value>US</value>
+        </property>
+      </configuration>
+   </workflow>
+  </action>
+</coordinator-app>""" in coord.to_xml(), coord.to_xml())
+    finally:
+      finish()
 
     assert_true(
 """<coordinator-app name="MyCoord"
@@ -3447,7 +3487,9 @@ COORDINATOR_DICT = {
     u'concurrency': [u'3'],
     u'execution': [u'FIFO'],
     u'throttle': [u'10'],
-    u'schema_version': [u'uri:oozie:coordinator:0.2']
+    u'schema_version': [u'uri:oozie:coordinator:0.2'],
+    'cron_frequency': ['0 0 * * *'],
+    'isAdvancedCron': ['on'],
 }
 BUNDLE_DICT = {
     u'name': [u'MyBundle'], u'description': [u'Description of my bundle'],

cloudera/hue

  enable_cron_scheduling=false

apps/oozie/src/oozie/conf.py
@@ -19,7 +19,7 @@ import os.path
 
 from django.utils.translation import ugettext as _, ugettext_lazy as _t
 
-from desktop.lib.conf import Config
+from desktop.lib.conf import Config, coerce_bool
 from desktop.lib import paths
 from liboozie.conf import get_oozie_status
 
@@ -30,30 +30,43 @@ DEFINITION_XSLT_DIR = Config(
   key="definition_xslt_dir",
   default=os.path.join(os.path.dirname(__file__), "importlib", "xslt"),
   help=_t("Location on local FS where the xslt files are stored for workflow import."),
-  private=True)
+  private=True
+)
 
 LOCAL_SAMPLE_DIR = Config(
   key="local_data_dir",
   default=os.path.join(os.path.dirname(__file__), "..", "..", "examples"),
   help=_t("Location on local filesystem where the examples are stored."),
-  private=True)
+  private=True
+)
 
 LOCAL_SAMPLE_DATA_DIR = Config(
   key="sample_data_dir",
   default=paths.get_thirdparty_root("sample_data"),
   help=_t("Location on local filesystem where the data for the examples is stored."),
-  private=True)
+  private=True
+)
 
 REMOTE_SAMPLE_DIR = Config(
   key="remote_data_dir",
   default="/user/hue/oozie/workspaces",
-  help=_t("Location on HDFS where the Oozie workflows are stored."))
+  help=_t("Location on HDFS where the Oozie workflows are stored.")
+)
 
 OOZIE_JOBS_COUNT = Config(
   key='oozie_jobs_count',
   default=100,
   type=int,
-  help=_t('Maximum number of Oozie workflows or coodinators or bundles to retrieve in one API call.'))
+  help=_t('Maximum number of Oozie workflows or coodinators or bundles to retrieve in one API call.')
+)
+
+ENABLE_CRON_SCHEDULING = Config( # Until Hue 4
+  key='enable_cron_scheduling',
+  default=True,
+  type=coerce_bool,
+  help=_t('Use Cron format for defining the frequency of a Coordinator instead of the old frequency number/unit.')
+)
+
 
 def config_validator(user):
   res = []

apps/oozie/src/oozie/templates/editor/gen/coordinator.xml.mako
@@ -17,6 +17,7 @@
 
 <%!
   from oozie.utils import smart_path
+  from oozie.conf import ENABLE_CRON_SCHEDULING
 %>
 
 <%namespace name="common" file="workflow-common.xml.mako" />
@@ -51,7 +52,11 @@
 
 
 <coordinator-app name="${ coord.name }"
+  % if ENABLE_CRON_SCHEDULING.get():
   frequency="${ coord.cron_frequency['frequency'] }"
+  % else:
+  frequency="${ coord.frequency }"
+  % endif
   start="${ coord.start_utc }" end="${ coord.end_utc }" timezone="${ coord.timezone }"
   xmlns="${ 'uri:oozie:coordinator:0.4' if coord.sla_enabled else coord.schema_version | n,unicode }"
   ${ 'xmlns:sla="uri:oozie:sla:0.2"' if coord.sla_enabled else '' | n,unicode }>

apps/oozie/src/oozie/tests.py
@@ -44,6 +44,7 @@ from liboozie.oozie_api_test import OozieServerProvider
 from liboozie.types import WorkflowList, Workflow as OozieWorkflow, Coordinator as OozieCoordinator,\
   Bundle as OozieBundle, CoordinatorList, WorkflowAction, BundleList
 
+from oozie.conf import ENABLE_CRON_SCHEDULING
 from oozie.models import Workflow, Node, Kill, Link, Job, Coordinator, History,\
   find_parameters, NODE_TYPES, Bundle
 from oozie.utils import workflow_to_dict, model_to_dict, smart_path
@@ -1469,6 +1470,45 @@ class TestEditor(OozieMockBase):
 
   def test_coordinator_gen_xml(self):
     coord = create_coordinator(self.wf, self.c, self.user)
+
+    finish = ENABLE_CRON_SCHEDULING.set_for_testing(False)
+
+    try:
+      assert_true(
+  """
+<coordinator-app name="MyCoord"
+  frequency="${coord:days(1)}"
+  start="2012-07-01T00:00Z" end="2012-07-04T00:00Z" timezone="America/Los_Angeles"
+  xmlns="uri:oozie:coordinator:0.2"
+  >
+  <controls>
+    <timeout>100</timeout>
+    <concurrency>3</concurrency>
+    <execution>FIFO</execution>
+    <throttle>10</throttle>
+  </controls>
+  <action>
+    <workflow>
+      <app-path>${wf_application_path}</app-path>
+      <configuration>
+        <property>
+          <name>username</name>
+          <value>${coord:user()}</value>
+        </property>
+        <property>
+          <name>SLEEP</name>
+          <value>1000</value>
+        </property>
+        <property>
+          <name>market</name>
+          <value>US</value>
+        </property>
+      </configuration>
+   </workflow>
+  </action>
+</coordinator-app>""" in coord.to_xml(), coord.to_xml())
+    finally:
+      finish()
 
     assert_true(
 """<coordinator-app name="MyCoord"
@@ -3447,7 +3487,9 @@ COORDINATOR_DICT = {
     u'concurrency': [u'3'],
     u'execution': [u'FIFO'],
     u'throttle': [u'10'],
-    u'schema_version': [u'uri:oozie:coordinator:0.2']
+    u'schema_version': [u'uri:oozie:coordinator:0.2'],
+    u'cron_frequency': [u'0 0 * * *'],
+    u'isAdvancedCron': [u'on'],
 }
 BUNDLE_DICT = {
     u'name': [u'MyBundle'], u'description': [u'Description of my bundle'],