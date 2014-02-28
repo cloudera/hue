a84eff56c3e1e0af61a1949a1e73e2bf4474915b
Enrico Berti <hello@enricoberti.com>
2014-02-28 10:10:00 +0100

    HUE-2005 [core] "Did you know?" box blocks form on login page
    Added path test on jHueTour plugin

--- desktop/core/static/js/jquery.tour.js
+++ desktop/core/static/js/jquery.tour.js
@@ -178,7 +178,7 @@
           _toursHtml += '<li>' + _this.options.labels.NO_AVAILABLE_TOURS + '</li>';
         }
       }
-      if (_added > 0 && typeof $.totalStorage !== "undefined" && ($.totalStorage("jHueTourHideModal") == null || $.totalStorage("jHueTourHideModal") == false)) {
+      if (_added > 0 && typeof $.totalStorage !== "undefined" && ($.totalStorage("jHueTourHideModal") == null || $.totalStorage("jHueTourHideModal") == false) && !RegExp("/accounts/login").test(location.pathname)) {
         $(document).ready(function () {
           $("#jHueTourModal").modal();
           $.totalStorage("jHueTourHideModal", true);
