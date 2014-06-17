6114332c4dc257ccba1fb09345e2a31d06405902
Abraham Elmahrek <abraham@elmahrek.com>
2014-06-17 16:16:26 -0700

    HUE-2166 [core] Oracle database support in doc model
    Defer large fields for oracle support

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -165,7 +165,7 @@ class DocumentTag(models.Model):
 class DocumentManager(models.Manager):
 
   def documents(self, user):
-    return Document.objects.filter(Q(owner=user) | Q(documentpermission__users=user) | Q(documentpermission__groups__in=user.groups.all())).distinct()
+    return Document.objects.filter(Q(owner=user) | Q(documentpermission__users=user) | Q(documentpermission__groups__in=user.groups.all())).defer('description', 'extra').distinct()
 
   def get_docs(self, user, model_class=None, extra=None):
     docs = Document.objects.documents(user).exclude(name='pig-app-hue-script')
