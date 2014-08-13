9d4f80e5b632745188d5d0f27f940a43de50761d
Enrico Berti <hello@enricoberti.com>
2014-08-13 02:08:05 +0200

    HUE-2116 [core] Improve jHueNotify stacking
    Error messages are never stacking now, the content is updated if an existing error message is already in the page

--- desktop/core/static/js/jquery.notify.js
+++ desktop/core/static/js/jquery.notify.js
@@ -17,114 +17,121 @@
  * jHue notify plugin
  *
  */
-;(function ($, window, document, undefined) {
+;
+(function ($, window, document, undefined) {
 
-    var pluginName = "jHueNotify",
-        TYPES = {
-            INFO: "INFO",
-            ERROR: "ERROR",
-            GENERAL: "GENERAL"
-        },
-        defaults = {
-            level: TYPES.GENERAL,
-            message: "",
-            sticky: false,
-            css: null
-        };
+  var pluginName = "jHueNotify",
+      TYPES = {
+        INFO: "INFO",
+        ERROR: "ERROR",
+        GENERAL: "GENERAL"
+      },
+      defaults = {
+        level: TYPES.GENERAL,
+        message: "",
+        sticky: false,
+        css: null
+      };
 
-    function Plugin(options) {
-        this.options = $.extend({}, defaults, options) ;
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.show();
+  function Plugin(options) {
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.show();
+  }
+
+  Plugin.prototype.setOptions = function (options) {
+    this.options = $.extend({}, defaults, options);
+  };
+
+  Plugin.prototype.show = function () {
+    var _this = this;
+    var MARGIN = 10;
+
+    if (_this.options.level == TYPES.ERROR && $(".jHueNotify.alert-error").length > 0) {
+      $(".jHueNotify.alert-error").find(".message").html("<i class='fa fa-exclamation-triangle'></i> <strong>" + _this.options.message + "</strong>");
     }
+    else {
+      var el = $("#jHueNotify").clone();
+      el.removeAttr("id");
 
-    Plugin.prototype.setOptions = function(options) {
-        this.options = $.extend({}, defaults, options) ;
-    };
+      // stops all the current animations and resets the style
+      el.stop(true);
+      el.attr("class", "alert jHueNotify");
+      el.find(".close").hide();
 
-    Plugin.prototype.show = function () {
-        var _this = this;
-        var MARGIN = 10;
-        var el = $("#jHueNotify").clone();
-        el.removeAttr("id");
+      if ($(".jHueNotify").last().position() != null) {
+        el.css("top", $(".jHueNotify").last().position().top + $(".jHueNotify").last().outerHeight() + MARGIN - $(window).scrollTop());
+      }
 
-        // stops all the current animations and resets the style
-        el.stop(true);
-        el.attr("class", "alert jHueNotify");
-        el.find(".close").hide();
+      _this.options.message = $("<span>").html(_this.options.message).text();
 
-        if ($(".jHueNotify").last().position() != null) {
-            el.css("top", $(".jHueNotify").last().position().top + $(".jHueNotify").last().outerHeight() + MARGIN - $(window).scrollTop());
-        }
+      if (_this.options.level == TYPES.ERROR) {
+        el.addClass("alert-error");
+        el.find(".message").html("<i class='fa fa-exclamation-triangle'></i> <strong>" + _this.options.message + "</strong>");
+      }
+      else if (_this.options.level == TYPES.INFO) {
+        el.addClass("alert-info");
+        el.find(".message").html("<i class='fa fa-info-circle'></i> <strong>" + _this.options.message + "</strong>");
+      }
+      else {
+        el.find(".message").html(_this.options.message);
+      }
 
-        _this.options.message = $("<span>").html(_this.options.message).text();
+      if (_this.options.css != null) {
+        el.attr("style", _this.options.css);
+      }
 
-        if (_this.options.level == TYPES.ERROR){
-            el.addClass("alert-error");
-            el.find(".message").html("<i class='fa fa-exclamation-triangle'></i> <strong>" + _this.options.message + "</strong>");
-        }
-        else if (_this.options.level == TYPES.INFO){
-            el.addClass("alert-info");
-            el.find(".message").html("<i class='fa fa-info-circle'></i> <strong>" + _this.options.message + "</strong>");
-        }
-        else {
-            el.find(".message").html(_this.options.message);
-        }
+      if (_this.options.sticky) {
+        el.find(".close").click(function () {
+          el.fadeOut();
+          el.nextAll(".jHueNotify").animate({
+            top: '-=' + (el.outerHeight() + MARGIN)
+          }, 200);
+          el.remove();
+        }).show();
+        el.show();
+      }
+      else {
+        var t = window.setTimeout(function () {
+          el.fadeOut();
+          el.nextAll(".jHueNotify").animate({
+            top: '-=' + (el.outerHeight() + MARGIN)
+          }, 200);
+          el.remove();
 
-        if (_this.options.css != null){
-            el.attr("style", _this.options.css);
-        }
+        }, 3000);
+        el.click(function () {
+          window.clearTimeout(t);
+          $(this).stop(true);
+          $(this).fadeOut();
+          $(this).nextAll(".jHueNotify").animate({
+            top: '-=' + ($(this).outerHeight() + MARGIN)
+          }, 200);
+        });
+        el.show();
+      }
+      el.appendTo($("body"));
+    }
+  };
 
-        if (_this.options.sticky){
-            el.find(".close").click(function(){
-                el.fadeOut();
-                el.nextAll(".jHueNotify").animate({
-                    top:'-=' + (el.outerHeight() + MARGIN)
-                }, 200);
-                el.remove();
-            }).show();
-            el.show();
-        }
-        else {
-            var t = window.setTimeout(function(){
-                el.fadeOut();
-                el.nextAll(".jHueNotify").animate({
-                    top:'-=' + (el.outerHeight() + MARGIN)
-                }, 200);
-                el.remove();
+  $[pluginName] = function () {
+  };
 
-            }, 3000);
-            el.click(function(){
-                window.clearTimeout(t);
-                $(this).stop(true);
-                $(this).fadeOut();
-                $(this).nextAll(".jHueNotify").animate({
-                    top:'-=' + ($(this).outerHeight() + MARGIN)
-                }, 200);
-            });
-            el.show();
-        }
-        el.appendTo($("body"));
-    };
+  $[pluginName].info = function (message) {
+    new Plugin({ level: TYPES.INFO, message: message});
+  };
 
-    $[pluginName] = function () {
-    };
+  $[pluginName].warn = function (message) {
+    new Plugin({ level: TYPES.GENERAL, message: message, sticky: true});
+  };
 
-    $[pluginName].info = function (message) {
-        new Plugin({ level: TYPES.INFO, message: message});
-    };
+  $[pluginName].error = function (message) {
+    new Plugin({ level: TYPES.ERROR, message: message, sticky: true});
+  };
 
-    $[pluginName].warn = function (message) {
-        new Plugin({ level: TYPES.GENERAL, message: message, sticky: true});
-    };
-
-    $[pluginName].error = function (message) {
-        new Plugin({ level: TYPES.ERROR, message: message, sticky: true});
-    };
-
-    $[pluginName].notify = function (options) {
-        new Plugin(options);
-    };
+  $[pluginName].notify = function (options) {
+    new Plugin(options);
+  };
 
 })(jQuery, window, document);
