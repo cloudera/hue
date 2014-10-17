c8b0ce89947c8edcbd403f5855599f8b4f536983
Enrico Berti <hello@enricoberti.com>
2014-10-17 16:11:40 +0200

    HUE-2210 [search] Improve reliability of the timeline start/date fields
    All the changes are sync'd between custom and pickers
    Added 6hours interval
    Changed non-edit mode interval to dropdown
    Group by hidden if just with one value
    Removed Custom from the picker interval
    Updated timeline chart formatting to 24H format

--- apps/search/static/js/search.ko.js
+++ apps/search/static/js/search.ko.js
@@ -863,6 +863,8 @@ var RANGE_SELECTABLE_WIDGETS = ['histogram-widget', 'bar-widget', 'line-widget']
 var SearchViewModel = function (collection_json, query_json, initial_json) {
   var self = this;
 
+  self.intervalOptions = ko.observableArray(ko.bindingHandlers.daterangepicker.INTERVAL_OPTIONS);
+
   // Models
   self.collection = new Collection(self, collection_json.collection);
   self.query = new Query(self, query_json);
