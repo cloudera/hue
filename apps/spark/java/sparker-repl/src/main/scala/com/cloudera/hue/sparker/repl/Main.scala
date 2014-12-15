1d45de13d3628ed06c860c546eb931756dadc578
Erick Tryzelaar <erickt@cloudera.com>
2014-12-15 11:59:11 -0800

    [spark] Initial web server working

--- apps/spark/java/sparker-repl/src/main/scala/com/cloudera/hue/sparker/repl/Main.scala
+++ apps/spark/java/sparker-repl/src/main/scala/com/cloudera/hue/sparker/repl/Main.scala
@@ -1,19 +1,25 @@
 package com.cloudera.hue.sparker.repl
 
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.DefaultServlet
+import org.eclipse.jetty.servlet.{ServletHolder, DefaultServlet}
 import org.eclipse.jetty.webapp.WebAppContext
-import org.scalatra.servlet.ScalatraListener
+import org.scalatra.servlet.{AsyncSupport, ScalatraListener}
+
+import scala.concurrent.ExecutionContext
 
 object Main {
   def main(args: Array[String]): Unit = {
     val port = 8087
     val server = new Server(port)
     val context = new WebAppContext()
+
     context.setContextPath("/")
     context.setResourceBase("src/main/com/cloudera/hue/sparker/repl")
     context.addEventListener(new ScalatraListener)
+
     context.addServlet(classOf[DefaultServlet], "/")
+
+    context.setAttribute(AsyncSupport.ExecutionContextKey, ExecutionContext.global)
 
     server.setHandler(context)
 
