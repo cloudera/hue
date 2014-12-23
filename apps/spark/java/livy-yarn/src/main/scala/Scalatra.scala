abf86e1d25120105fc27a6a84b791223d805d7ce
Erick Tryzelaar <erickt@cloudera.com>
2014-12-22 16:35:23 -0800

    [spark] Get the spark repl to run inside yarn (!)

--- apps/spark/java/livy-yarn/src/main/scala/Scalatra.scala
+++ apps/spark/java/livy-yarn/src/main/scala/Scalatra.scala
@@ -1,14 +1,20 @@
 import javax.servlet.ServletContext
 
-import com.cloudera.hue.livy.yarn.WebApp
+import com.cloudera.hue.livy.repl.interpreter.SparkInterpreter
+import com.cloudera.hue.livy.repl.LivyApp
 import org.scalatra.LifeCycle
 
 class ScalatraBootstrap extends LifeCycle {
 
+  //val system = ActorSystem()
+  val sparkInterpreter = new SparkInterpreter
+
   override def init(context: ServletContext): Unit = {
-    context.mount(new WebApp, "/*")
+    context.mount(new LivyApp(sparkInterpreter), "/*")
   }
 
   override def destroy(context: ServletContext): Unit = {
+    sparkInterpreter.close()
+    //system.shutdown()
   }
 }
