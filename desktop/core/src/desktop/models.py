bc4d5d137579d12edfb71c3bff2f883642a1bdf3
Romain Rigaux <romain@cloudera.com>
2015-03-11 11:49:09 -0700

    [oozie] Add HiveServer2 example for the New Editor

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -339,7 +339,8 @@ class DocumentManager(models.Manager):
               extra = ''
             doc = Document.objects.link(job, owner=job.owner, name=job.name, description=job.description, extra=extra)
           if job.owner.username == SAMPLE_USERNAME:
-            job.doc.get().share_to_default()
+            doc = job.doc.get()
+            doc.share_to_default()
             tag = DocumentTag.objects.get_example_tag(user=job.owner)
             doc.tags.add(tag)
     except Exception, e:
