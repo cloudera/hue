7c75d278fa339a58b6be3ac3bf1cf643f060c5a5
Erick Tryzelaar <erickt@cloudera.com>
2015-03-27 16:44:34 -0700

    [livy] ProcessSession should read output to determine the repl url
    This allows us to add a ProcessSessionSpec test

---
+++ apps/spark/java/livy-server/src/test/scala/com/cloudera/hue/livy/server/ProcessSessionSpec.scala
@@ -0,0 +1,14 @@
+package com.cloudera.hue.livy.server
+
+import com.cloudera.hue.livy.LivyConf
+import com.cloudera.hue.livy.server.sessions.{ProcessSession, Session}
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{Matchers, FunSpecLike, BeforeAndAfter, FunSpec}
+
+class ProcessSessionSpec extends BaseSessionSpec with FunSpecLike with Matchers with BeforeAndAfter {
+
+  val livyConf = new LivyConf()
+  livyConf.set("livy.repl.driverClassPath", sys.props("java.class.path"))
+
+  def createSession() = ProcessSession.create(livyConf, "0", "spark")
+}
