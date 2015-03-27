983fb7a1b722843f4a7861102c154bdab5e8977b
Erick Tryzelaar <erickt@cloudera.com>
2015-03-27 13:37:18 -0700

    [livy] Tests should wait for the sessions to close

--- apps/spark/java/livy-repl/src/test/scala/com/cloudera/hue/livy/repl/PythonSessionSpec.scala
+++ apps/spark/java/livy-repl/src/test/scala/com/cloudera/hue/livy/repl/PythonSessionSpec.scala
@@ -19,7 +19,7 @@ class PythonSessionSpec extends FunSpec with Matchers with BeforeAndAfter {
   }
 
   after {
-    session.close()
+    Await.result(session.close(), Duration.Inf)
   }
 
   describe("A python session") {
