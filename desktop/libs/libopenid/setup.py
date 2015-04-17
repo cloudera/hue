05adddd445a446a76e19e69d8d68f164fcd1fb2e
Erick Tryzelaar <erickt@cloudera.com>
2015-04-17 11:19:39 -0700

    [desktop] Change lib setup.py files to use the VERSION file
    Conflicts:
    
    	desktop/libs/hadoop/setup.py
    	desktop/libs/indexer/setup.py
    	desktop/libs/liboozie/setup.py
    	desktop/libs/librdbms/setup.py
    	desktop/libs/libsaml/setup.py
    	desktop/libs/libsentry/setup.py
    	desktop/libs/libsolr/setup.py

--- desktop/libs/libopenid/setup.py
+++ desktop/libs/libopenid/setup.py
@@ -15,10 +15,11 @@
 # limitations under the License.
 
 from setuptools import setup, find_packages
+from hueversion import VERSION
 
 setup(
       name = "libopenid",
-      version = "1.0",
+      version = VERSION,
       url = 'http://github.com/cloudera/hue',
       description = "OPENID Libraries",
       packages = find_packages('src'),
