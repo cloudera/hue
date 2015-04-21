cc640f896312f8c81efbe4be2dfbdbde088ff685
Erick Tryzelaar <erickt@cloudera.com>
2015-04-21 11:04:12 -0700

    [desktop] Add 'from __future__ import absolute_import'
    This is part of a very gradual process to get Hue compatible
    with future versions of Python.

--- desktop/core/src/desktop/urls.py
+++ desktop/core/src/desktop/urls.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 import logging
 import os
 import re
