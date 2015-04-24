8dd6f9b63f3db6e6c84ae37b7a146c1793aa25a3
Enrico Berti <hello@enricoberti.com>
2015-04-24 18:32:24 +0300

    [search] Implemented text squeezer for the Hit Widget

--- desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
+++ desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
@@ -1319,3 +1319,18 @@ ko.bindingHandlers.timepicker = {
 
     }
 }
+
+ko.bindingHandlers.textSqueezer = {
+  init: function (element, valueAccessor) {
+    var value = valueAccessor();
+    $(element).text(ko.unwrap(value));
+    $(element).textSqueezer({
+      decimalPrecision: 2
+    });
+  },
+  update: function (element, valueAccessor) {
+    var value = valueAccessor();
+    $(element).text(ko.unwrap(value));
+    $(element).trigger("redraw");
+  }
+};
