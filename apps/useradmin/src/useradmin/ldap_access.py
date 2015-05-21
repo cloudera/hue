066f837bb041eb1b443b2eda642ed0b53fccd5ae
Scott Kahler <skahler@gopivotal.com>
2015-05-21 17:12:11 -0500

    Sanitization process added to groups
    The same sanitization process we do in users we run into in groups so the fix needs to be applied there additionally

--- apps/useradmin/src/useradmin/ldap_access.py
+++ apps/useradmin/src/useradmin/ldap_access.py
@@ -326,7 +326,9 @@ class LdapConnection(object):
       group_filter = '(' + group_filter + ')'
 
     # Allow wild cards on non distinguished names
-    sanitized_name = ldap.filter.escape_filter_chars(groupname_pattern).replace(r'\2a', r'*')
+    sanitized_name = ldap.filter.escape_filter_chars(username_pattern).replace(r'\2a', r'*')
+    # Fix issue where \, is converted to \5c,
+    sanitized_name = sanitized_name.replace(r'\5c,', r'\2c')
     search_dn, group_name_filter = self._get_search_params(sanitized_name, search_attr, find_by_dn)
     ldap_filter = '(&' + group_filter + group_name_filter + ')'
     attrlist = ['objectClass', 'dn', 'memberUid', group_member_attr, group_name_attr]
