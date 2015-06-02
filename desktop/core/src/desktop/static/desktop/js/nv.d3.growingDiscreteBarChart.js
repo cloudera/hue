b8e03cd1de037334b4eae900a22e988ee4725814
Enrico Berti <hello@enricoberti.com>
2015-06-02 16:00:09 +0200

    [search] Workaround for wrong X positioning of bar tooltips

--- desktop/core/src/desktop/static/desktop/js/nv.d3.growingDiscreteBarChart.js
+++ desktop/core/src/desktop/static/desktop/js/nv.d3.growingDiscreteBarChart.js
@@ -65,7 +65,7 @@ nv.models.growingDiscreteBarChart = function() {
   //------------------------------------------------------------
 
   var showTooltip = function(e, offsetElement) {
-    var left = e.pos[0] + ( offsetElement.offsetLeft || 0 ),
+    var left = e.e.clientX,
         top = e.pos[1] + ( offsetElement.offsetTop || 0),
         x = xAxis.tickFormat()(discretebar.x()(e.point, e.pointIndex)),
         y = yAxis.tickFormat()(discretebar.y()(e.point, e.pointIndex)),
