34888ed34ce028c045e67c133e3b60b28f5228ac
Romain Rigaux <romain@cloudera.com>
2015-06-19 12:51:50 -0700

    [libzookeeper] Move the ZooKeeper specific properties to its own lib
    The goal is to move ZooKeeper infos into a lib that is always available even if
    the ZooKeeper app is disabled.
    
    Indexer, Sentry needs to know some ZooKeeper properties.
    
    The ZooKeeper app will use libzookeeper properties in Hue 4. Only its
    app specific properties should be there.
    
    This is similar to liboozie which is used by the Oozie, JobSub and Pig apps.

--- desktop/libs/libzookeeper/src/libzookeeper/conf.py
+++ desktop/libs/libzookeeper/src/libzookeeper/conf.py
@@ -16,34 +16,21 @@
 # limitations under the License.
 
 import logging
-import os
+
 from urlparse import urlparse
 
-from django.utils.translation import ugettext_lazy as _t
+from desktop.lib.conf import Config, UnspecifiedConfigSection, ConfigSection
 
-from desktop.lib.conf import Config
 
 LOG = logging.getLogger(__name__)
 
 
-def solrctl():
-  """
-  solrctl path
-  """
-  for dirname in os.environ.get('PATH', '').split(os.path.pathsep):
-    path = os.path.join(dirname, 'solrctl')
-
-    if os.path.exists(path):
-      return path
-
-  return None
-
-
 def zkensemble():
   """
-  ZooKeeper Ensemble
+  Try to guess the value if no values are specified.
   """
   try:
+    # Backward compatibility until Hue 4
     from zookeeper.conf import CLUSTERS
     clusters = CLUSTERS.get()
     if clusters['default'].HOST_PORTS.get() != 'localhost:2181':
@@ -58,36 +45,19 @@ def zkensemble():
   except:
     LOG.exception('failed to get solr url')
 
+  return "localhost:2181"
 
 
-# Unused
-BATCH_INDEXER_PATH = Config(
-  key="batch_indexer_path",
-  help=_t("Batch indexer path in HDFS."),
-  type=str,
-  default="/var/lib/search/search-mr-job.jar")
+ENSEMBLE=Config(
+    "ensemble",
+    help="ZooKeeper ensemble. Comma separated list of Host/Port, e.g. localhost:2181,localhost:2182,localhost:2183",
+    dynamic_default=zkensemble,
+    type=str,
+)
 
-CORE_INSTANCE_DIR = Config(
-  key="core_instance_dir",
-  help=_t("Local path to Hue folder where Solr instance directories will be created in non-solrcloud mode."),
-  type=str,
-  default=os.path.join(os.path.dirname(__file__), '../data/collections'))
-
-CONFIG_TEMPLATE_PATH = Config(
-  key="config_template_path",
-  help=_t("Default template used at collection creation."),
-  type=str,
-  default=os.path.join(os.path.dirname(__file__), '..', 'data', 'solrconfigs'))
-
-SOLRCTL_PATH = Config(
-  key="solrctl_path",
-  help=_t("Location of the solrctl binary."),
-  type=str,
-  dynamic_default=solrctl)
-
-# Deprecated
-SOLR_ZK_ENSEMBLE = Config(
-  key="solr_zk_ensemble",
-  help=_t("Zookeeper ensemble."),
-  type=str,
-  dynamic_default=zkensemble)
+PRINCIPAL_NAME=Config(
+    "principal_name",
+    help="Name of Kerberos principal when using security",
+    default="zookeeper",
+    type=str,
+)
