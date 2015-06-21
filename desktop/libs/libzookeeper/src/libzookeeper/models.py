c69253867c18d4b5e9de0740a78128c2657c3f2c
Romain Rigaux <romain@cloudera.com>
2015-06-21 12:07:27 -0700

    [libzookeeper] Move namespace children data retrieval to libzookeeper
    HiveServer2 HA uses similar logic

--- desktop/libs/libzookeeper/src/libzookeeper/models.py
+++ desktop/libs/libzookeeper/src/libzookeeper/models.py
@@ -14,3 +14,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import json
+
+from kazoo.client import KazooClient
+from libzookeeper.conf import PRINCIPAL_NAME
+
+
+def get_children_data(ensemble, namespace, read_only=True):
+  zk = KazooClient(hosts=ensemble, read_only=read_only, sasl_server_principal=PRINCIPAL_NAME.get())
+
+  zk.start()
+
+  children_data = []
+
+  children = zk.get_children(namespace)
+
+  for node in children:
+    data, stat = zk.get("%s/%s" % (namespace, node))
+    children_data.append(data)
+
+  zk.stop()
+  
+  return children_data
