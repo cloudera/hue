1a100617abff4b025756c2710c8cdd03207020c4
Jenny Kim <jennykim@cloudera.com>
2015-08-03 15:32:30 -0700

    [tests] Change order of packages, traverse namespace, and restrict nose from changing sys path

--- desktop/core/src/desktop/settings.py
+++ desktop/core/src/desktop/settings.py
@@ -335,8 +335,9 @@ if 'test' in sys.argv:
 
 # Limit Nose coverage to Hue apps
 NOSE_ARGS = [
-  '--cover-package=%s' % ','.join([app.name for app in appmanager.DESKTOP_MODULES]),
-  '--cover-inclusive',
+  '--cover-package=%s' % ','.join([app.name for app in appmanager.DESKTOP_APPS + appmanager.DESKTOP_LIBS]),
+  '--no-path-adjustment',
+  '--traverse-namespace'
 ]
 
 TIME_ZONE = desktop.conf.TIME_ZONE.get()
