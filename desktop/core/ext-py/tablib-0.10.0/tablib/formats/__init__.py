dd893264dda362890f9526c69ed7b523d67ddddd
Romain Rigaux <romain@cloudera.com>
2015-10-16 01:38:57 +0200

    HUE-2523 [core] Remove tablib XLS packages

--- desktop/core/ext-py/tablib-0.10.0/tablib/formats/__init__.py
+++ desktop/core/ext-py/tablib-0.10.0/tablib/formats/__init__.py
@@ -5,11 +5,11 @@
 
 from . import _csv as csv
 from . import _json as json
-from . import _xls as xls
+#from . import _xls as xls
 from . import _yaml as yaml
 from . import _tsv as tsv
 from . import _html as html
-from . import _xlsx as xlsx
+#from . import _xlsx as xlsx
 from . import _ods as ods
 
-available = (json, xls, yaml, csv, tsv, html, xlsx, ods)
+available = (json, yaml, csv, tsv, html, ods)
