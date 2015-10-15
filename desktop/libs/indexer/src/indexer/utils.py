0f777743d85f20780adbb628ea6d3144d3ba1603
Romain Rigaux <romain@cloudera.com>
2015-10-15 10:19:31 +0200

    [indexer] Improve parsing of binary or non unicode data
    Adding tests.

--- desktop/libs/indexer/src/indexer/utils.py
+++ desktop/libs/indexer/src/indexer/utils.py
@@ -197,11 +197,6 @@ def get_type_from_morphline_type(morphline_type):
     return 'string'
 
 
-def utf_8_encoder(unicode_csv_data):
-  for line in unicode_csv_data:
-    yield force_unicode(line, errors='ignore') # Even 'replace' seems to break the DictReader
-
-
 def field_values_from_separated_file(fh, delimiter, quote_character, fields=None):
   if fields is None:
     field_names = None
@@ -257,10 +252,12 @@ def field_values_from_separated_file(fh, delimiter, quote_character, fields=None
       headers = [name.strip() for name in headers]
 
     # User dict reader
-    reader = csv.DictReader(utf_8_encoder(csvfile), fieldnames=headers, delimiter=smart_str(delimiter), quotechar=smart_str(quote_character))
+    reader = csv.DictReader(csvfile, fieldnames=headers, delimiter=smart_str(delimiter), quotechar=smart_str(quote_character))
 
     remove_keys = None
     for row in reader:
+      row = dict([(force_unicode(k), force_unicode(v, errors='ignore')) for k, v in row.iteritems()]) # Get rid of invalid binary chars and convert to unicode from DictReader
+
       # Remove keys that aren't in collection
       if remove_keys is None:
         if field_names is None:
