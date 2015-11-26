e7a390bc0d64efa18e221818fa25426e40fa9581
Jenny Kim <jennykim@cloudera.com>
2015-11-25 16:18:56 -0800

    [impala] Initial implementation of Impala Thrift service
    Adds ImpalaService and dependent thrift files, as well as generated Python classes.
    
    Adds ImpalaServerClient and overrides initialization and security configs

--- apps/impala/src/impala/server.py
+++ apps/impala/src/impala/server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Licensed to Cloudera, Inc. under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,7 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import os
+import logging
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'gen-py'))
+from beeswax.server.hive_server2_lib import HiveServerClient
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ImpalaServerClient(HiveServerClient):
+
+  def resetCatalog(self):
+    return self._client.ResetCatalog()
