5c3049715c577144fe5cb416062de6355195634f
airokey <595731254@qq.com>
2015-11-27 16:09:47 +0800

    [notebook] Add all snippets execute function
    In notebooks, there's a function named 'Execute all snippets', but it's not been finished.
    If an error occurred, the next will continue.

--- desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
+++ desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
@@ -950,6 +950,25 @@
       });
     };
 
+    self.executeAll = function () {
+      if (self.snippets().length < 1)
+        return;
+  
+      var index = 0;
+      self.snippets()[index].execute();
+      var clock = setInterval(next, 100);
+  
+      function next() {
+        if (self.snippets()[index].status() == 'available' || self.snippets()[index].status() == 'failed') {
+          index = index + 1;
+          if (self.snippets().length > index)
+            self.snippets()[index].execute();
+          else
+            clearInterval(clock);
+        }
+      }
+    };
+
     self.closeAndRemoveSession = function (session) {
       self.closeSession(session, false, function() {
         self.sessions.remove(session);
