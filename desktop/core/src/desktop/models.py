a9c5768749431a225824d96756f5ae5c205f44db
Romain Rigaux <romain@cloudera.com>
2015-12-23 15:42:43 -0800

    [doc2] Disable permission short link for now

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -967,7 +967,8 @@ class Document2Permission(models.Model):
     (COMMENT_PERM, 'comment'), # PLAYER PERM?
   ))
 
-  link = models.CharField(default=uuid_default, max_length=255, unique=True) # Short link like dropbox
+  # link = models.CharField(default=uuid_default, max_length=255, unique=True) # Short link like dropbox
+  # embed
 
   class Meta:
     unique_together = ('doc', 'perms')
