1b98ad7484a8179f60e884cccbf0908672dd455c
Romain Rigaux <romain@cloudera.com>
2016-01-25 10:05:18 -0800

    [metadata] API to support pulling the top tables
    $.post('/metadata/api/optimizer_api/top_tables')
    
    {"status": 0, "top_tables": [{"columnCount": 5, "name": "part", "patternCount": 4, "workloadPercent": 28, "total": 2, "type": "Dimension", "eid": "65"}, {"columnCount": 5, "name": "emps", "patternCount": 4, "workloadPercent": 28, "total": 2, "type": "Dimension", "eid": "64"},...]}

---
+++ desktop/libs/metadata/src/metadata/optimizer_api.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+
+from django.http import Http404
+from django.utils.translation import ugettext as _
+from django.views.decorators.http import require_POST
+
+from desktop.lib.django_util import JsonResponse
+from desktop.lib.i18n import force_unicode
+
+from metadata.optimizer_client import OptimizerApi
+
+LOG = logging.getLogger(__name__)
+
+
+def error_handler(view_fn):
+  def decorator(*args, **kwargs):
+    try:
+      return view_fn(*args, **kwargs)
+    except Http404, e:
+      raise e
+    except Exception, e:
+      LOG.exception(e)
+      response = {
+        'status': -1,
+        'message': force_unicode(str(e))
+      }
+    return JsonResponse(response, status=500)
+  return decorator
+
+
+@require_POST
+@error_handler
+def top_tables(request):
+  response = {'status': -1}
+
+  api = OptimizerApi()
+
+  response['top_tables'] = api.top_tables()
+  response['status'] = 0
+
+  return JsonResponse(response)
