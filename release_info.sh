f679217ac839dc82b2e20b2d4847d7ce21365452
Jenkins <dev-kitchen@cloudera.com>
2016-04-19 16:57:36 -0700

    Update to 5.5.5-SNAPSHOT on Tue Apr 19 16:57:32 PDT 2016
    JOB_NAME : 'Cut-Release-Branches'
    BUILD_NUMBER : '298'
    CODE_BRANCH : ''
    OLD_CDH_BRANCH : 'cdh5_5.5.x'
    
    Pushed to remote origin	git@github.sf.cloudera.com:CDH/hue.git (push)

---
+++ release_info.sh
@@ -0,0 +1,6 @@
+# Branch in cdh.git that has metadata about this product release
+PRODUCT_CDH_BRANCH=cdh5_5.5.x
+
+# Maven version for this release (specified in the pom.xml file in the
+# cdh.git branch specified above)
+PRODUCT_RELEASE_VERSION=5.5.5-SNAPSHOT
