67fd38f574d5301e8fb5e9baa670e2caabc3d40c
Romain Rigaux <romain@cloudera.com>
2016-05-19 12:57:16 -0700

    HUE-3935 [core] Use error code 10 for missing Oracle connector in the is_db_alive command

--- desktop/core/src/desktop/manage_entry.py
+++ desktop/core/src/desktop/manage_entry.py
@@ -59,7 +59,7 @@ def entry():
       execute_from_command_line(sys.argv)
   except ImproperlyConfigured, e:
     if len(sys.argv) > 1 and sys.argv[1] == 'is_db_alive' and 'oracle' in str(e).lower():
-      sys.exit(3)
+      sys.exit(10)
     else:
       raise e
 
