b47dbf71f98791de644b5118867a6a5a47d9f3cc
Jenny Kim <jennykim@cloudera.com>
2016-05-19 17:52:51 -0400

    [jb] Fix references to LOG in yarn_models

--- apps/jobbrowser/src/jobbrowser/yarn_models.py
+++ apps/jobbrowser/src/jobbrowser/yarn_models.py
@@ -33,7 +33,7 @@ from hadoop.yarn.clients import get_log_client
 from jobbrowser.models import format_unixtime_ms
 
 
-LOGGER = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 
 class Application(object):
@@ -143,7 +143,7 @@ class Job(object):
     for attr in attrs.keys():
       if attr == 'acls':
         # 'acls' are actually not available in the API
-        LOGGER.warn('Not using attribute: %s' % attrs[attr])
+        LOG.warn('Not using attribute: %s' % attrs[attr])
       else:
         setattr(self, attr, attrs[attr])
 
@@ -410,7 +410,7 @@ class Attempt:
         try:
           debug_info = '\nLog Link: %s' % log_link
           debug_info += '\nHTML Response: %s' % response
-          LOGGER.error(debug_info)
+          LOG.error(debug_info)
         except:
           LOG.exception('failed to build debug info')
 
