0bd6f9b58b788b24e716dbcc7bc92c4399308617
Romain Rigaux <romain@cloudera.com>
2016-06-16 09:44:46 -0700

    HUE-3797 [editor] Add refresh parameter call
    (cherry picked from commit 6b5649b75f6f33d3477abcee13eab767c0c8a306)

--- apps/oozie/src/oozie/static/oozie/js/coordinator-editor.ko.js
+++ apps/oozie/src/oozie/static/oozie/js/coordinator-editor.ko.js
@@ -87,6 +87,9 @@ var Coordinator = function (vm, coordinator) {
     self.end_date.value(self.properties.endDateUI() + "T" + self.properties.endTimeUI());
   }
 
+  self.refreshParameters = function() {
+    self.properties.workflow.valueHasMutated();
+  }
 
   self.properties.workflow.subscribe(function (newVal) {
     if (newVal) {
@@ -188,7 +191,7 @@ var CoordinatorEditorViewModel = function (coordinator_json, credentials_json, w
   self.availableSettings = ko.observableArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]);
 
   if (coordinator_json.id == null && self.coordinator.properties.workflow()) {
-    self.coordinator.properties.workflow.valueHasMutated();
+    self.coordinator.refreshParameters();
   }
 
   self.workflowModalFilter = ko.observable("");
@@ -213,7 +216,6 @@ var CoordinatorEditorViewModel = function (coordinator_json, credentials_json, w
     }
     return null;
   }
-
 
   self.save = function (cb) {
     if (!self.isSaving()) {
