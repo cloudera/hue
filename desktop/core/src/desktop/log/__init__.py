8cd264fbdf41d89ee3690821cb9c7c6ca8d7f73a
Jenny Kim <jennykim@cloudera.com>
2016-06-16 12:09:55 -0700

    HUE-4130 [core] Triple amount of lines in the /logs page
    (cherry picked from commit 724f8f7b0e44426c176aba46f2e6a334ce3e2df9)

--- desktop/core/src/desktop/log/__init__.py
+++ desktop/core/src/desktop/log/__init__.py
@@ -190,7 +190,7 @@ def fancy_logging():
   """Configure logging into a buffer for /logs endpoint."""
   from log_buffer import FixedBufferHandler
 
-  BUFFER_SIZE = 500 * 200 # This is the size in characters, not bytes. Targets about 500 rows.
+  BUFFER_SIZE = 1500 * 200 # This is the size in characters, not bytes. Targets about 1500 rows.
   buffer_handler = FixedBufferHandler(BUFFER_SIZE)
   _formatter = formatter.Formatter(LOG_FORMAT, DATE_FORMAT)
 
