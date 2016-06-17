c482c25c328048917a24f633bfc307b1925bc79c
krish <krish@cloudera.com>
2016-06-17 09:40:07 -0700

    Revert "[beeswax] Set download limit to 10k rows for save query to HDFS"
    This reverts commit b27503a3b0eafe0d6873c82af757d59c5c77805d.

--- apps/beeswax/src/beeswax/data_export.py
+++ apps/beeswax/src/beeswax/data_export.py
@@ -57,7 +57,7 @@ def upload(path, handle, user, db, fs):
   else:
     fs.do_as_user(user.username, fs.create, path)
 
-  content_generator = HS2DataAdapter(handle, db, conf.DOWNLOAD_ROW_LIMIT.get(), start_over=True)
+  content_generator = HS2DataAdapter(handle, db, -1, start_over=True)
   for header, data in content_generator:
     dataset = export_csvxls.dataset(None, data)
     fs.do_as_user(user.username, fs.append, path, dataset.csv)
