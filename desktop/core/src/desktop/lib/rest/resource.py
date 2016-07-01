c1268631b9125b252b0da183a69bd2da35a0c897
Jenny Kim <jennykim@cloudera.com>
2016-07-01 13:37:51 -0700

    HUE-4252 [core] Handle 307 redirect from YARN upon standby failover
    Don't enable allow_redirects by default, since it causes non-HA setups to fail.
    
    (cherry picked from commit e5e7eba89bd2cf11a5c0e30a48a18a74bb3995db)

--- desktop/core/src/desktop/lib/rest/resource.py
+++ desktop/core/src/desktop/lib/rest/resource.py
@@ -122,7 +122,7 @@ class Resource(object):
 
     @return: A dictionary of the JSON result.
     """
-    return self.invoke("POST", relpath, params, data, self._make_headers(contenttype, headers), files)
+    return self.invoke("POST", relpath, params, data, self._make_headers(contenttype, headers), files, allow_redirects)
 
 
   def put(self, relpath=None, params=None, data=None, contenttype=None, allow_redirects=False):
