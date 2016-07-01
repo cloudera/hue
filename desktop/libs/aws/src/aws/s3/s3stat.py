c711b13d47511062fd6cf6209dc4599309ec907c
Jenny Kim <jennykim@cloudera.com>
2016-06-30 17:37:40 -0700

    HUE-4170 [aws] Fix S3 object timestamp

--- desktop/libs/aws/src/aws/s3/s3stat.py
+++ desktop/libs/aws/src/aws/s3/s3stat.py
@@ -80,7 +80,14 @@ class S3Stat(object):
       path = 's3://%s' % key.bucket.name
 
     size = key.size or 0
-    mtime = s3datetime_to_timestamp(key.last_modified) if key.last_modified else 0
+
+    s3_date = None
+    if key.last_modified is not None:
+      s3_date = key.last_modified
+    elif hasattr(key, 'date') and key.date is not None:
+      s3_date = key.date
+    mtime = s3datetime_to_timestamp(s3_date) if s3_date else 0
+
     return cls(name, path, is_dir, size, mtime)
 
   @classmethod
@@ -96,9 +103,3 @@ class S3Stat(object):
     for k in keys:
       res[k] = self[k]
     return res
-
-
-
-
-
-
