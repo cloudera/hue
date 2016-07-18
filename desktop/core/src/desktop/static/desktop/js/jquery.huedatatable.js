98a9a01af078b5b62732b4babe690e01d28ab2a5
Enrico Berti <hello@enricoberti.com>
2016-07-18 21:07:09 +0200

    HUE-3960 [editor] Implement virtual rendering of the result table

--- desktop/core/src/desktop/static/desktop/js/jquery.huedatatable.js
+++ desktop/core/src/desktop/static/desktop/js/jquery.huedatatable.js
@@ -24,7 +24,7 @@
     var self = this;
     self.$table = null;
 
-    self.fnSetColumnVis = function(index, visible) {
+    self.fnSetColumnVis = function (index, visible) {
       var aoColumns = this.$table.data('aoColumns');
       var change = aoColumns[index].bVisible !== visible;
       aoColumns[index].bVisible = visible;
@@ -38,12 +38,74 @@
       }
     }
 
-    self.fnDraw = function () {};
+    self.isDrawing = false;
+    self.fnDraw = function () {
+      if (!self.isDrawing) {
+        self.isDrawing = true;
+        var rowHeight = 29;
+        var $t = self.$table;
+
+        var aoColumns = self.$table.data('aoColumns');
+        var appendable = $t.children('tbody').length > 0 ? $t.children('tbody') : $t;
+
+        var invisibleOffset = aoColumns.length > 30 ? 1 : 10;
+
+
+        var scrollable = $t.parents($t.data('oInit')['scrollable']);
+        var visibleRows = Math.ceil(scrollable.height() / rowHeight) + invisibleOffset;
+
+        var startRow = $t.offset().top - 73 < 0 ? Math.max(Math.floor(Math.abs($t.offset().top - 73) / rowHeight) - invisibleOffset, 0) : 0;
+        var endRow = startRow + visibleRows;
+
+        var northSpace = rowHeight * startRow;
+        if ($t.find('.ht-north-spacer').length === 0) {
+          $('<tr>').addClass('ht-north-spacer').html('<td colspan="' + (aoColumns.length) + '" style="display: none"></td>').appendTo(appendable);
+        }
+        else {
+          if (northSpace > 0) {
+            $t.find('.ht-north-spacer td').show().height(northSpace);
+          }
+          else {
+            $t.find('.ht-north-spacer td').hide();
+          }
+        }
+
+        $t.find('.ht-visible-row').remove();
+
+        var html = '';
+        for (var i = startRow; i < Math.min(startRow + visibleRows, $t.data('data').length); i++) {
+          var row = $t.data('data')[i];
+          html += '<tr class="ht-visible-row ht-visible-row-' + i + '">';
+          for (var j = 0; j < aoColumns.length; j++) {
+            html += '<td class="ht-cell-' + i + '-' + j + '">' + row[j] + '</td>';
+          }
+          html += '</tr>';
+        }
+
+        $t.find('.ht-north-spacer').after(html);
+
+        var southSpace = rowHeight * ($t.data('data').length - endRow);
+        if ($t.find('.ht-south-spacer').length === 0) {
+          $('<tr>').addClass('ht-south-spacer').html('<td colspan="' + (aoColumns.length) + '" style="height: ' + southSpace + 'px"></td>').appendTo(appendable);
+        }
+        else {
+          $t.find('.ht-south-spacer td').height(southSpace);
+        }
+
+        if ($t.data('oInit')['fnDrawCallback']) {
+          $t.data('oInit')['fnDrawCallback']();
+        }
+        self.isDrawing = false;
+      }
+    };
 
     self.fnAddData = function (mData, bRedraw) {
-      var aoColumns = this.$table.data('aoColumns');
+      var aoColumns = self.$table.data('aoColumns') || [];
 
       var $t = self.$table;
+
+      $t.data('data', $t.data('data').concat(mData));
+
       if (mData.length === 0) {
         return;
       }
@@ -56,26 +118,8 @@
         })
       }
 
+      self.fnDraw();
 
-      var appendable = $t.children('tbody').length > 0 ? $t.children('tbody') : $t;
-      var html = '';
-      mData.forEach(function (row) {
-        html += '<tr>';
-        var index = 0;
-        row.forEach(function (cell) {
-          if (aoColumns[index].bVisible) {
-            html += '<td>' + cell + '</td>';
-          } else {
-            html += '<td style="display: none;">' + cell + '</td>';
-          }
-          index++;
-        });
-        html += '</tr>';
-      });
-      appendable.append(html);
-      if (oInit['fnDrawCallback']){
-        oInit['fnDrawCallback']();
-      }
     };
 
     self.fnSettings = function () {
@@ -93,9 +137,11 @@
       else {
         $t.children('tr').remove();
       }
+      $t.data('data', []);
     };
 
     self.fnDestroy = function () {
+      self.fnClearTable();
       self.$table.unwrap();
       self.$table.removeClass('table-huedatatable');
     };
@@ -106,9 +152,22 @@
       if (parent.hasClass('dataTables_wrapper')) {
         return;
       }
+      self.$table.data('data', []);
       self.$table.data('aoRows', []);
       self.$table.data('aoColumns', []);
       self.$table.wrap('<div class="dataTables_wrapper"></div>');
+
+      if (typeof oInit !== 'undefined') {
+        self.$table.data('oInit', oInit);
+        var drawTimeout = -1;
+        if (self.$table.data('oInit')['scrollable'] && !self.$table.data('isScrollAttached')) {
+          self.$table.data('isScrollAttached', true);
+          self.$table.parents(oInit['scrollable']).on('scroll', function () {
+            window.clearTimeout(drawTimeout);
+            drawTimeout = window.setTimeout(self.fnDraw, 50);
+          });
+        }
+      }
       self.$table.addClass('table-huedatatable');
     });
   };
