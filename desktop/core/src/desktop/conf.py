3d0004f37183d5894519141aa3f79872a8c03c8f
Romain Rigaux <romain@cloudera.com>
2016-08-26 12:39:21 +0200

    Revert "HUE-4704 [security] Fixed Arbitrary host header accepted in Hue"
    This reverts commit f4bc5f832c01d76ae1a421241886c7340e2ad196.

--- desktop/core/src/desktop/conf.py
+++ desktop/core/src/desktop/conf.py
@@ -64,16 +64,6 @@ def coerce_file(path):
 def coerce_timedelta(value):
   return datetime.timedelta(seconds=int(value))
 
-def get_dn():
-  """This function returns fqdn(if possible)"""
-  val = []
-  LOG = logging.getLogger(__name__)
-  try:
-    val.append(socket.getfqdn())
-  except:
-    LOG.warning("allowed_hosts value to '*'. It is a security risk")
-    val.append('*')
-  return val
 
 def coerce_positive_integer(integer):
   integer = int(integer)
@@ -398,7 +388,7 @@ LOG_REDACTION_FILE = Config(
 
 ALLOWED_HOSTS = Config(
   key='allowed_hosts',
-  dynamic_default=get_dn,
+  default=['*'],
   type=coerce_csv,
   help=_('Comma separated list of strings representing the host/domain names that the Hue server can serve.')
 )
