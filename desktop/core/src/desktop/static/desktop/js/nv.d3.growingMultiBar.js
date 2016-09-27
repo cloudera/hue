4f073c23b68e4405a18ac940cd9c276c33e65309
Enrico Berti <hello@enricoberti.com>
2016-09-27 12:09:31 +0200

    HUE-4890 [editor] Graph bars are not in the same order as the column legend

--- desktop/core/src/desktop/static/desktop/js/nv.d3.growingMultiBar.js
+++ desktop/core/src/desktop/static/desktop/js/nv.d3.growingMultiBar.js
@@ -88,6 +88,7 @@ nv.models.growingMultiBar = function() {
       data.forEach(function(series, i) {
         series.values.forEach(function(point) {
           point.series = i;
+          point.seriesKey = series.key;
         });
       });
 
