63f584982bebd346c36c9db381841e1dca19f236
Johan Ahlen <johan@johanahlen.com>
2016-10-14 10:37:14 +0200

    HUE-5085 [editor] Fix context popover for backticked identifiers
    (cherry picked from commit ca2877441093f6b9ed71f559ef13f2b4a2c6c910)

--- desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
+++ desktop/core/src/desktop/static/desktop/js/ko.hue-bindings.js
@@ -2942,6 +2942,10 @@
           huePubSub.publish('editor.active.locations', e.data.locations);
           e.data.locations.forEach(function (location) {
             var token = editor.session.getTokenAt(location.location.first_line - 1, location.location.first_column);
+            if (/`$/.test(token.value)) {
+              // Ace getTokenAt() thinks the first ` is a token, column +1 will include the first and last.
+              token = editor.session.getTokenAt(location.location.first_line - 1, location.location.first_column + 1);
+            }
             if (token !== null) {
               if (location.type === 'column' && typeof location.tables !== 'undefined' && location.identifierChain.length === 1) {
 
