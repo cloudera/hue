073fa17f28bede66312846532ae0a6cde4b8cf65
Romain Rigaux <romain@cloudera.com>
2016-11-11 13:50:47 +0900

    HUE-4866 [editor] Do not able execute from a file when only query string is set

--- desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
+++ desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
@@ -795,14 +795,14 @@ var EditorViewModel = (function() {
 
     self.wasBatchExecuted = ko.observable(typeof snippet.wasBatchExecuted != "undefined" && snippet.wasBatchExecuted != null ? snippet.wasBatchExecuted : false);
     self.isReady = ko.computed(function() {
-      return (self.statementType() == 'text' &&
+      return (self.statementType() == 'text' && (
           (self.isSqlDialect() && self.statement() !== '') ||
           (['jar', 'java', 'spark2', 'distcp'].indexOf(self.type()) == -1 && self.statement() !== '') ||
           (['jar', 'java'].indexOf(self.type()) != -1 && (self.properties().app_jar() != '' && self.properties().class() != '')) ||
           (['spark2'].indexOf(self.type()) != -1 && self.properties().jars().length > 0) ||
           (['shell'].indexOf(self.type()) != -1 && self.properties().command_path().length > 0) ||
           (['mapreduce'].indexOf(self.type()) != -1 && self.properties().app_jar().length > 0) ||
-          (['distcp'].indexOf(self.type()) != -1 && self.properties().source_path().length > 0 && self.properties().destination_path().length > 0)) ||
+          (['distcp'].indexOf(self.type()) != -1 && self.properties().source_path().length > 0 && self.properties().destination_path().length > 0))) ||
         (self.statementType() == 'file' && self.statementPath().length > 0);
     });
     self.lastExecuted = ko.observable(typeof snippet.lastExecuted != "undefined" && snippet.lastExecuted != null ? snippet.lastExecuted : 0);
