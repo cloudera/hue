dfb6ace24225d91e89ed65ccec277e9d777dcc2b
Johan Ahlen <johan@johanahlen.com>
2016-11-21 20:10:18 +0100

    HUE-5305 [home] Fix empty share document modal and improve sharing UX
    - Removed the “add” button
    - Make the read / modify the primary button
    
    (cherry picked from commit ad2326a46a9f320e3c0c14889dee466c1871d6af)

--- desktop/core/src/desktop/static/desktop/js/fileBrowser/hueFileEntry.js
+++ desktop/core/src/desktop/static/desktop/js/fileBrowser/hueFileEntry.js
@@ -252,9 +252,7 @@
       entry.selected(true);
     }
     if (self.selectedEntry()) {
-      if (! self.selectedEntry().document()) {
-        self.selectedEntry().loadDocument();
-      }
+      self.selectedEntry().loadDocument();
       $('#shareDocumentModal').modal('show');
     }
   };
