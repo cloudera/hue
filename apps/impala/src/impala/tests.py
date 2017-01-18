c0894ee589eb97be043541320884ce8804f3ed54
Romain Rigaux <romain@cloudera.com>
2017-01-18 11:29:30 -0800

    HUE-5679 [impala] Do not truncate the last part of rows in result downloads
    (cherry picked from commit 44cbbb30d31b37a2981f0724e4c0aafd305f9ca0)
    
    Conflicts:
    	apps/impala/src/impala/tests.py
    
    (cherry picked from commit 043fda1d38fff5818d3cdbeca10d3c75681552eb)

--- apps/impala/src/impala/tests.py
+++ apps/impala/src/impala/tests.py
@@ -33,7 +33,10 @@ from hadoop import cluster
 from hadoop.pseudo_hdfs4 import get_db_prefix, is_live_cluster
 
 from beeswax import hive_site
+from beeswax import data_export
 from beeswax.design import hql_query
+
+from beeswax.data_export import download
 from beeswax.models import SavedQuery, QueryHistory
 from beeswax.server import dbms
 from beeswax.test_base import get_query_server_config, wait_for_query_to_finish, fetch_query_result_data
@@ -211,6 +214,47 @@ class TestImpalaIntegration:
     resp = self.client.post(reverse('impala:api_cancel_query', kwargs={'query_history_id': query_history.id}))
     content = json.loads(resp.content)
     assert_equal(0, content['status'])
+
+
+  def test_data_download(self):
+    hql = 'SELECT * FROM tweets %(limit)s'
+
+    FETCH_SIZE = data_export.FETCH_SIZE
+    data_export.FETCH_SIZE = 2 # Decrease fetch size to validate last fetch logic
+
+    try:
+      query = hql_query(hql % {'limit': ''})
+
+      handle = self.db.execute_and_wait(query)
+      # Get the result in csv. Should have 5 + 1 header row.
+      csv_resp = download(handle, 'csv', self.db)
+      csv_content = ''.join(csv_resp.streaming_content)
+      assert_equal(len(csv_content.strip().split('\n')), 5 + 1)
+
+
+      query = hql_query(hql % {'limit': 'LIMIT 0'})
+
+      handle = self.db.execute_and_wait(query)
+      csv_resp = download(handle, 'csv', self.db)
+      csv_content = ''.join(csv_resp.streaming_content)
+      assert_equal(len(csv_content.strip().split('\n')), 1)
+
+      query = hql_query(hql % {'limit': 'LIMIT 1'})
+
+      handle = self.db.execute_and_wait(query)
+      csv_resp = download(handle, 'csv', self.db)
+      csv_content = ''.join(csv_resp.streaming_content)
+      assert_equal(len(csv_content.strip().split('\n')), 1 + 1)
+
+      query = hql_query(hql % {'limit': 'LIMIT 2'})
+
+      handle = self.db.execute_and_wait(query)
+      csv_resp = download(handle, 'csv', self.db)
+      csv_content = ''.join(csv_resp.streaming_content)
+      assert_equal(len(csv_content.strip().split('\n')), 1 + 2)
+    finally:
+      data_export.FETCH_SIZE = FETCH_SIZE
+
 
   def test_explain(self):
     QUERY = """
