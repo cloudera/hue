ba78c26650fba8fa23ef35665d8a715c6c1f0c79
Johan Ahlen <johan@johanahlen.com>
2017-01-25 11:26:41 +0100

    HUE-5710 [core] Move the notebook styles to less
    (cherry picked from commit 82998023cf0d4e2e32f926fae9019907534aa322)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,21 @@
+/*
+ Licensed to Cloudera, Inc. under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  Cloudera, Inc. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 module.exports = function(grunt) {
   'use strict';
 
@@ -17,7 +35,9 @@ module.exports = function(grunt) {
         files: {
           'desktop/core/src/desktop/static/desktop/css/hue.css': 'desktop/core/src/desktop/static/desktop/less/hue.less',
           'desktop/core/src/desktop/static/desktop/css/hue3-extra.css': 'desktop/core/src/desktop/static/desktop/less/hue3-extra.less',
-          'apps/metastore/src/metastore/static/metastore/css/metastore.css': 'apps/metastore/src/metastore/static/metastore/less/metastore.less'
+          'apps/metastore/src/metastore/static/metastore/css/metastore.css': 'apps/metastore/src/metastore/static/metastore/less/metastore.less',
+          'desktop/libs/notebook/src/notebook/static/notebook/css/notebook.css': 'desktop/libs/notebook/src/notebook/static/notebook/less/notebook.less',
+          'desktop/libs/notebook/src/notebook/static/notebook/css/notebook-layout.css': 'desktop/libs/notebook/src/notebook/static/notebook/less/notebook-layout.less'
         }
       }
     },
@@ -25,7 +45,11 @@ module.exports = function(grunt) {
       less: {
         files: [
           'desktop/core/src/desktop/static/desktop/less/*.less',
-          'desktop/core/src/desktop/static/desktop/less/**/*.less'
+          'desktop/core/src/desktop/static/desktop/less/**/*.less',
+          'apps/metastore/src/metastore/static/metastore/less/*.less',
+          'apps/metastore/src/metastore/static/metastore/less/**/*.less',
+          'desktop/libs/notebook/src/notebook/static/notebook/less/*.less',
+          'desktop/libs/notebook/src/notebook/static/notebook/less/**/*.less'
         ],
         tasks: ['less']
       }
