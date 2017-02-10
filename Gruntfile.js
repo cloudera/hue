28eaaa2a06729e313889afdf0d22f6d3b21fb922
Enrico Berti <hello@enricoberti.com>
2017-02-10 09:15:27 +0100

    HUE-5821 [core] Compress the generated CSS files and add a banner to them

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,8 @@ module.exports = function(grunt) {
       hue: {
         options: {
           paths: ['desktop/core/src/desktop/static/desktop/less'],
-          compress: false
+          compress: true,
+          banner: '/*!\nLicensed to Cloudera, Inc. under one\nor more contributor license agreements.  See the NOTICE file\ndistributed with this work for additional information\nregarding copyright ownership.  Cloudera, Inc. licenses this file\nto you under the Apache License, Version 2.0 (the\n"License"); you may not use this file except in compliance\nwith the License.  You may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0\n\nUnless required by applicable law or agreed to in writing, software\ndistributed under the License is distributed on an "AS IS" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.\n */'
         },
         files: {
           'desktop/core/src/desktop/static/desktop/css/hue.css': 'desktop/core/src/desktop/static/desktop/less/hue.less',
