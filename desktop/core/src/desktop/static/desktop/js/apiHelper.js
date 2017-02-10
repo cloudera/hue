054e32420adafbeb9a02fda7378e3a056ee42a38
Johan Ahlen <johan@johanahlen.com>
2017-02-10 08:56:10 -0800

    HUE-5823 [editor] Cancel running doc search requests when the query has changed

--- desktop/core/src/desktop/static/desktop/js/apiHelper.js
+++ desktop/core/src/desktop/static/desktop/js/apiHelper.js
@@ -239,6 +239,9 @@ var ApiHelper = (function () {
    */
   ApiHelper.prototype.assistErrorCallback = function (options) {
     return function (errorResponse) {
+      if (typeof errorResponse.statusText !== 'undefined' && errorResponse.statusText === 'abort') {
+        return;
+      }
       var errorMessage = 'Unknown error occurred';
       if (errorResponse !== 'undefined') {
         if (typeof errorResponse.responseText !== 'undefined') {
@@ -611,7 +614,7 @@ var ApiHelper = (function () {
    */
   ApiHelper.prototype.searchDocuments = function (options) {
     var self = this;
-    $.ajax({
+    return $.ajax({
       url: DOCUMENTS_SEARCH_API,
       data: {
         uuid: options.uuid,
