80e9ea594009497dbce81e288fc95a4bd8062f1d
Enrico Berti <hello@enricoberti.com>
2017-02-16 18:15:09 +0100

    HUE-5851 [fb] Support the new S3 regions

--- desktop/libs/aws/src/aws/s3/s3fs.py
+++ desktop/libs/aws/src/aws/s3/s3fs.py
@@ -137,8 +137,10 @@ class S3FileSystem(object):
         raise S3FileSystemException(e.message or e.reason)
 
   def _get_location(self):
-    if get_default_region() in (Location.EU, Location.EUCentral1, Location.USWest, Location.USWest2, Location.SAEast,
-                                Location.APNortheast, Location.APSoutheast, Location.APSoutheast2, Location.CNNorth1):
+    if get_default_region() in (Location.EU, Location.EUCentral, Location.EUWest, Location.EUWest2,
+                                Location.CACentral, Location.USEast, Location.USEast2, Location.USWest,
+                                Location.USWest2, Location.SAEast, Location.APNortheast, Location.APNortheast2,
+                                Location.APSoutheast, Location.APSoutheast2, Location.APSouth):
       return get_default_region()
     else:
       return Location.DEFAULT
