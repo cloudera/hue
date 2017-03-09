a9f4349c2e2d8dc0492f7996529f548a528fcd37
Johan Ahlen <johan@johanahlen.com>
2017-03-09 15:21:44 +0100

    HUE-6050 [autocomplete] Add support for aggregate functions as column names in the autocompleter

--- desktop/core/src/desktop/static/desktop/js/aceSqlWorker.js
+++ desktop/core/src/desktop/static/desktop/js/aceSqlWorker.js
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var version = 13;
+var version = 14;
 importScripts('/static/desktop/js/autocomplete/sql.js?version=' + version);
 importScripts('/static/desktop/js/sqlFunctions.js?version=' + version);
 
