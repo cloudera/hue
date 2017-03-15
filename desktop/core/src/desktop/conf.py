9642ae98bcdf42d8979bf9970ead178c9436797e
Enrico Berti <hello@enricoberti.com>
2017-03-15 15:04:38 +0100

    HUE-6077 [frontend] Added Vimeo as valid security exception
    (cherry picked from commit ea8b20f90bc4e033497918664915aee16769e988)

--- desktop/core/src/desktop/conf.py
+++ desktop/core/src/desktop/conf.py
@@ -232,7 +232,7 @@ SECURE_CONTENT_SECURITY_POLICY = Config(
           "img-src 'self' *.google-analytics.com *.doubleclick.net http://*.tile.osm.org *.tile.osm.org *.gstatic.com data:;"+
           "style-src 'self' 'unsafe-inline' fonts.googleapis.com;"+
           "connect-src 'self';"+
-          "child-src 'self' data:;"+
+          "child-src 'self' data: *.vimeo.com;"+
           "object-src 'none'")
 
 SECURE_SSL_REDIRECT = Config(
