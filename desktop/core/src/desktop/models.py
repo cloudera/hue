10a47061679cecf170f2c13d01e1b094ff7a8974
Jenny Kim <jennykim@cloudera.com>
2017-03-23 17:24:58 -0700

    HUE-6115 [core] Fix document paths for names with unicode characters

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -32,7 +32,6 @@ from django.core.urlresolvers import reverse, NoReverseMatch
 from django.db import connection, models, transaction
 from django.db.models import Q
 from django.db.models.query import QuerySet
-from django.template.defaultfilters import urlencode
 from django.utils.translation import ugettext as _, ugettext_lazy as _t
 
 from desktop import appmanager
@@ -1051,7 +1050,7 @@ class Document2(models.Model):
 
   @property
   def path(self):
-    quoted_name = urllib.quote(self.name)
+    quoted_name = urllib.quote(self.name.encode('utf-8'))
     if self.parent_directory:
       return '%s/%s' % (self.parent_directory.path, quoted_name)
     else:
@@ -1123,7 +1122,7 @@ class Document2(models.Model):
     return {
       'owner': self.owner.username,
       'name': self.name,
-      'path': urlencode(self.path or '/'),
+      'path': self.path or '/',
       'description': self.description,
       'uuid': self.uuid,
       'id': self.id,
