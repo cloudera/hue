d9fd745b632949665ee76ff2354a7d61ddb0155c
Jenny Kim <jennykim@cloudera.com>
2017-05-01 14:40:03 -0700

    HUE-6262 [core] Converter should separate history docs from saved docs
    (cherry picked from commit 6a9243935c22dee0794de8198b12efca34991227)

--- desktop/core/src/desktop/converters.py
+++ desktop/core/src/desktop/converters.py
@@ -58,9 +58,6 @@ class DocumentConverter(object):
             notebook = import_saved_beeswax_query(doc.content_object)
             data = notebook.get_data()
 
-            if doc.is_historic():
-              data['isSaved'] = False
-
             doc2 = self._create_doc2(
                 document=doc,
                 doctype=data['type'],
@@ -68,9 +65,6 @@ class DocumentConverter(object):
                 description=data['description'],
                 data=notebook.get_json()
             )
-
-            if doc.is_historic():
-              doc2.is_history = False
 
             self.imported_docs.append(doc2)
         except Exception, e:
@@ -83,7 +77,7 @@ class DocumentConverter(object):
     try:
       from beeswax.models import SavedQuery, HQL, IMPALA, RDBMS
 
-      docs = self._get_unconverted_docs(SavedQuery, with_history=True).filter(extra__in=[HQL, IMPALA, RDBMS]).order_by('-last_modified')
+      docs = self._get_unconverted_docs(SavedQuery, only_history=True).filter(extra__in=[HQL, IMPALA, RDBMS]).order_by('-last_modified')
 
       for doc in docs:
         try:
@@ -179,17 +173,19 @@ class DocumentConverter(object):
       LOG.error('Failed to import %d document(s) for user: %s - %s' % (len(self.failed_docs), self.user.username, ([doc.id for doc in self.failed_docs])))
 
 
-  def _get_unconverted_docs(self, content_type, with_history=False):
+  def _get_unconverted_docs(self, content_type, only_history=False):
     docs = Document.objects.get_docs(self.user, content_type).filter(owner=self.user)
 
     tags = [
-      DocumentTag.objects.get_trash_tag(user=self.user), # No trashed docs
-      DocumentTag.objects.get_example_tag(user=self.user), # No examples
-      self.imported_tag # No already imported docs
+      DocumentTag.objects.get_trash_tag(user=self.user),  # No trashed docs
+      DocumentTag.objects.get_example_tag(user=self.user),  # No examples
+      self.imported_tag  # No already imported docs
     ]
 
-    if not with_history:
-      tags.append(DocumentTag.objects.get_history_tag(user=self.user)) # No history yet
+    if only_history:
+      docs = docs.filter(tags__in=[DocumentTag.objects.get_history_tag(user=self.user)])
+    else:  # Exclude history docs by default
+      tags.append(DocumentTag.objects.get_history_tag(user=self.user))
 
     return docs.exclude(tags__in=tags)
 
