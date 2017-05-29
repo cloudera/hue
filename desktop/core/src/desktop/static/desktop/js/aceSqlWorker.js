5a1880fd2f63db189243baa57751fe6960d7b2af
Johan Ahlen <johan@johanahlen.com>
2017-05-29 11:31:07 +0200

    HUE-6491 [autocomplete] Prevent logging of unknown chars to the console
    (cherry picked from commit 26c89c58f1efb19d3ed41801dbc58254b2c01875)

--- desktop/core/src/desktop/static/desktop/js/aceSqlWorker.js
+++ desktop/core/src/desktop/static/desktop/js/aceSqlWorker.js
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var version = 18;
+var version = 19;
 importScripts('/static/desktop/js/autocomplete/sqlParseSupport.js?version=' + version);
 importScripts('/static/desktop/js/autocomplete/sql.js?version=' + version);
 importScripts('/static/desktop/js/sqlFunctions.js?version=' + version);
