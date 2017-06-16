ab359856eae93c3831783cb61d6954418c219d3f
Romain Rigaux <romain@cloudera.com>
2017-06-16 11:16:54 -0700

    HUE-5304 [assist] List collections via the new API
    (cherry picked from commit 5e0b573638d977b5e7f99e7d1ad5d19e7cdd5e66)

--- desktop/core/src/desktop/static/desktop/js/apiHelper.js
+++ desktop/core/src/desktop/static/desktop/js/apiHelper.js
@@ -62,7 +62,7 @@ var ApiHelper = (function () {
   var IMPALA_INVALIDATE_API = '/impala/api/invalidate';
   var CONFIG_SAVE_API = '/desktop/api/configurations/save/';
   var CONFIG_APPS_API = '/desktop/api/configurations';
-  var SOLR_COLLECTIONS_API = '/indexer/api/collections/';
+  var SOLR_COLLECTIONS_API = '/indexer/api/collections/list/';
   var HBASE_API_PREFIX = '/hbase/api/';
   var SAVE_TO_FILE = '/filebrowser/save';
 
@@ -541,7 +541,7 @@ var ApiHelper = (function () {
    */
   ApiHelper.prototype.fetchSolrCollections = function (options) {
     var self = this;
-    var url = SOLR_COLLECTIONS_API + '?format=json';
+    var url = SOLR_COLLECTIONS_API;
     var fetchFunction = function (storeInCache) {
       if (options.timeout === 0) {
         self.assistErrorCallback(options)({ status: -1 });
@@ -550,6 +550,7 @@ var ApiHelper = (function () {
       $.ajax({
         dataType: "json",
         url: url,
+        type: 'POST',
         timeout: options.timeout,
         success: function (data) {
           if (!data.error && !self.successResponseIsError(data) && typeof data.collections !== 'undefined' && data.collections !== null) {
