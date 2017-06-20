ed6b6c156f81f228e629336d670b7006bfc05d54
Romain Rigaux <romain@cloudera.com>
2017-06-20 13:18:04 -0700

    HUE-5304 [search] Move list of collections to use the new API
    (cherry picked from commit adfd5066af7fc970f9d130d0fd010ede7dd6c14c)

--- apps/search/src/search/dashboard_api.py
+++ apps/search/src/search/dashboard_api.py
@@ -20,9 +20,9 @@ import logging
 from dashboard.dashboard_api import DashboardApi
 from dashboard.models import augment_solr_response
 from libsolr.api import SolrApi
+from indexer.solr_client import SolrClient
 
 from search.conf import SOLR_URL
-from search.controller import SearchController
 
 
 LOG = logging.getLogger(__name__)
@@ -39,7 +39,8 @@ class SearchApi(DashboardApi):
     return augment_solr_response(response, collection, query)
 
   def datasets(self, show_all=False):
-    return SearchController(self.user).get_all_indexes(show_all=show_all)
+    client = SolrClient(user=self.user)
+    return [index['name'] for index in client.get_indexes(include_cores=show_all)]
 
   def fields(self, collection):
     return self.api.fields(collection)
