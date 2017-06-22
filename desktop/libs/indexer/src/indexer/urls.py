a5ca83c790e8de63f601e72aa0f746ab28029dd5
Romain Rigaux <romain@cloudera.com>
2017-06-21 21:18:44 -0700

    HUE-5304 [indexer] Update name when switching output format
    (cherry picked from commit 35b89d5546e1793fe70c2eda7c16fdbf20166f5c)

--- desktop/libs/indexer/src/indexer/urls.py
+++ desktop/libs/indexer/src/indexer/urls.py
@@ -47,6 +47,7 @@ urlpatterns += patterns('indexer.api',
 urlpatterns += patterns('indexer.solr_api',
   # V2
   url(r'^api/aliases/create/$', 'create_alias', name='create_alias'),
+  url(r'^api/configs/list/$', 'list_configs', name='list_configs'),
   url(r'^api/indexes/list/$', 'list_indexes', name='list_indexes'),
   url(r'^api/indexes/create/$', 'create_index', name='create_index'),
   url(r'^api/indexes/delete/$', 'delete_indexes', name='delete_indexes'),
