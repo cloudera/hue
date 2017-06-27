f8c892ac4996861f6560ac9464657bc23f82c989
Romain Rigaux <romain@cloudera.com>
2017-06-27 15:20:28 -0700

    HUE-6791 [search] Protect against pivot facets conflicting with nested facets

--- desktop/libs/dashboard/src/dashboard/static/dashboard/js/search.ko.js
+++ desktop/libs/dashboard/src/dashboard/static/dashboard/js/search.ko.js
@@ -718,7 +718,7 @@ var Collection = function (vm, collection) {
     }
     if (typeof facet.properties.facets != 'undefined') {
       $.each(facet.properties.facets(), function (index, pivotFacet) {
-        if (pivotFacet.aggregate) {
+        if (pivotFacet.aggregate && typeof facet.properties.aggregate == 'function') {
           pivotFacet.aggregate.function.subscribe(function () {
             vm.search();
           });
