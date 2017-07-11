723c57096254d0644d4f98d4db99046eb24f270b
Enrico Berti <hello@enricoberti.com>
2017-07-11 12:12:13 +0200

    HUE-6844 [frontend] Make all custom apps embeddable
    (cherry picked from commit f7f487fa04f3ffc2dbd12092b6891144d43653c4)

--- desktop/core/src/desktop/models.py
+++ desktop/core/src/desktop/models.py
@@ -1824,7 +1824,7 @@ class ClusterConfig():
 
     for other in other_apps:
       interpreters.append({
-        'type': other.nice_name,
+        'type': other.display_name,
         'displayName': other.nice_name,
         'buttonName': other.nice_name,
         'tooltip': other.nice_name,
