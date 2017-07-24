99b25b35be814dd97496073e3c0dafb2dcd20ca4
Romain Rigaux <romain@cloudera.com>
2017-07-23 20:54:50 -0700

    HUE-6908 [spark] Add get sessions API
    (cherry picked from commit 0210fdaa6e62346e391e9451575873111a04d8bf)

--- apps/spark/src/spark/job_server_api.py
+++ apps/spark/src/spark/job_server_api.py
@@ -104,6 +104,9 @@ class JobServerApi(object):
     properties['proxyUser'] = self.user
     return self._root.post('sessions', data=json.dumps(properties), contenttype=_JSON_CONTENT_TYPE)
 
+  def get_sessions(self):
+    return self._root.get('sessions')
+
   def get_session(self, uuid):
     return self._root.get('sessions/%s' % uuid)
 
