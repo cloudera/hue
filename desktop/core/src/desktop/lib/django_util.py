3f70205aa8ccdb12049c1fe2d28cbaeae45e169e
chiwen <today.vc@gmail.com>
2017-07-25 17:39:08 +0800

    update JsonResponse: support set json_dumps_params

--- desktop/core/src/desktop/lib/django_util.py
+++ desktop/core/src/desktop/lib/django_util.py
@@ -468,10 +468,13 @@ class JsonResponse(HttpResponse):
       to ``True``.
     """
 
-    def __init__(self, data, encoder=DjangoJSONEncoder, safe=True, indent=None, **kwargs):
+    def __init__(self, data, encoder=DjangoJSONEncoder, safe=True,
+                 json_dumps_params=None, **kwargs):
         if safe and not isinstance(data, dict):
             raise TypeError('In order to allow non-dict objects to be '
                 'serialized set the safe parameter to False')
+        if json_dumps_params is None:
+            json_dumps_params = {}
         kwargs.setdefault('content_type', 'application/json')
-        data = json.dumps(data, cls=encoder, indent=indent)
+        data = json.dumps(data, cls=encoder, **json_dumps_params)
         super(JsonResponse, self).__init__(content=data, **kwargs)
