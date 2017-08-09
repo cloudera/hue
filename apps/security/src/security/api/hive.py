c61cd44f9fc7148b490f48cc3caaf307f87e74fc
Romain Rigaux <romain@cloudera.com>
2017-08-09 16:37:35 -0700

    HUE-7090 [security] Protect from missing groupName in the list_sentry_roles_by_group API
    (cherry picked from commit 74b6fab52b7e4c6b728d4eff04adfd2cc80b6c3b)

--- apps/security/src/security/api/hive.py
+++ apps/security/src/security/api/hive.py
@@ -62,7 +62,7 @@ def list_sentry_roles_by_group(request):
   result = {'status': -1, 'message': 'Error'}
 
   try:
-    if request.POST['groupName']:
+    if request.POST.get('groupName'):
       groupName = request.POST['groupName']
     else:
       # Admins can see everything, other only the groups they belong too
