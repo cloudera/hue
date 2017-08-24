6422e21eff21a238b40352a325a25954ef5c6703
jdesjean <jgauthier@cloudera.com>
2017-08-24 16:11:50 -0700

    HUE-7165 [aws] hue s3 browser should provide accessibility for all buckets in v2 regions (cherry picked from commit 457158cab4c5e1e49c7cec5edec23443001d5382)

--- desktop/libs/aws/src/aws/client.py
+++ desktop/libs/aws/src/aws/client.py
@@ -23,7 +23,7 @@ import boto.s3
 import boto.s3.connection
 import boto.utils
 
-from aws.conf import get_default_region, has_iam_metadata, DEFAULT_CALLING_FORMAT
+from aws.conf import get_default_region, has_iam_metadata, DEFAULT_CALLING_FORMAT, AWS_ACCOUNT_REGION_DEFAULT
 from aws.s3.s3fs import S3FileSystemException
 
 
@@ -34,13 +34,13 @@ HTTP_SOCKET_TIMEOUT_S = 60
 
 
 class Client(object):
-  def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, aws_security_token=None, region=None,
+  def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, aws_security_token=None, region=AWS_ACCOUNT_REGION_DEFAULT,
                timeout=HTTP_SOCKET_TIMEOUT_S, host=None, proxy_address=None, proxy_port=None, proxy_user=None,
                proxy_pass=None, calling_format=None, is_secure=True):
     self._access_key_id = aws_access_key_id
     self._secret_access_key = aws_secret_access_key
     self._security_token = aws_security_token
-    self._region = region.lower() if region is not None else None
+    self._region = region.lower()
     self._timeout = timeout
     self._host = host
     self._proxy_address = proxy_address
