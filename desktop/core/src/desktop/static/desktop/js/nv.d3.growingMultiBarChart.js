f4809888fafd96bf271d8f3e88c757ca1440fabf
Enrico Berti <hello@enricoberti.com>
2017-09-12 12:15:45 +0200

    HUE-7261 [editor] Prevent enormous chart legend labels to take over the chart
    (cherry picked from commit 8cdb597b6d23732004fa9e832dd6eefcf45f26d8)

--- desktop/core/src/desktop/static/desktop/js/nv.d3.growingMultiBarChart.js
+++ desktop/core/src/desktop/static/desktop/js/nv.d3.growingMultiBarChart.js
@@ -187,10 +187,16 @@ nv.models.growingMultiBarChart = function() {
             .datum(data)
             .call(legend);
 
-        if ( margin.top != legend.height()) {
-          margin.top = legend.height();
-          availableHeight = (height || parseInt(container.style('height')) || 400)
-                             - margin.top - margin.bottom;
+        if (legend.height() > 50) {
+          g.select('.nv-legendWrap').style('visibility', 'hidden');
+        }
+        else {
+          g.select('.nv-legendWrap').style('visibility', 'visible');
+          if (margin.top != legend.height()) {
+            margin.top = legend.height();
+            availableHeight = (height || parseInt(container.style('height')) || 400)
+              - margin.top - margin.bottom;
+          }
         }
 
         g.select('.nv-legendWrap')
