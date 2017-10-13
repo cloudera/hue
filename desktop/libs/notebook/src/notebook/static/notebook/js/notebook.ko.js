dbd3435a39c60cd4a4a8e3cae0661f000a6043cb
Johan Ahlen <johan@johanahlen.com>
2017-10-13 12:27:37 +0200

    HUE-7470 [editor] Only check for risks when a snippet has focus
    (cherry picked from commit 66c75d26f89e2dead9508260eac11ca380ddea5d)

--- desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
+++ desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
@@ -1001,7 +1001,7 @@ var EditorViewModel = (function() {
       });
 
       self.checkComplexity = function () {
-        if (lastCheckedComplexityStatement === self.statement()) {
+        if (!self.inFocus() || lastCheckedComplexityStatement === self.statement()) {
           return;
         }
 
