1a0a1213e92f93a42a6bdae5972d2452e947d5af
Johan Ahlen <johan@johanahlen.com>
2017-11-08 11:19:30 +0100

    HUE-2451 [core] Split parser generation in the makefile

--- tools/jison/hue-sql-syntax.sh
+++ tools/jison/hue-sql-syntax.sh
@@ -21,18 +21,6 @@ echo "Generating parser..."
 
 pushd ../../desktop/core/src/desktop/static/desktop/js/autocomplete/jison
 
-# === Autocomplete parser ===
-# For quick version of just SELECT statements without value expressions
-# cat autocomplete_header.jison sql_main.jison autocomplete_footer.jison > sqlAutocompleteParser.jison
-cat autocomplete_header.jison sql_main.jison sql_valueExpression.jison sql_error.jison sql_alter.jison sql_analyze.jison sql_create.jison sql_drop.jison sql_grant.jison sql_insert.jison sql_load.jison sql_set.jison sql_show.jison sql_update.jison sql_use.jison autocomplete_footer.jison > sqlAutocompleteParser.jison
-
-echo "Creating SQL autocomplete parser..."
-jison sqlAutocompleteParser.jison sql.jisonlex
-grunt uglify:sqlAutocompleteParser
-cat license.txt sqlAutocompleteParser.js > ../sqlAutocompleteParser.js
-rm sqlAutocompleteParser.jison
-rm sqlAutocompleteParser.js
-
 # === Syntax parser ===
 cat syntax_header.jison sql_main.jison sql_valueExpression.jison  sql_alter.jison sql_analyze.jison sql_create.jison sql_drop.jison sql_grant.jison sql_insert.jison sql_load.jison sql_set.jison sql_show.jison sql_update.jison sql_use.jison syntax_footer.jison > sqlSyntaxParser.jison
 
@@ -45,27 +33,6 @@ grunt uglify:sqlSyntaxParser
 cat license.txt sqlSyntaxParser.js > ../sqlSyntaxParser.js
 rm sqlSyntaxParser.jison
 rm sqlSyntaxParser.js
-
-# === Statement parser ===
-echo "Creating SQL statements parser..."
-jison sqlStatementsParser.jison
-grunt uglify:sqlStatementsParser
-cat license.txt sqlStatementsParser.js > ../sqlStatementsParser.js
-rm sqlStatementsParser.js
-
-# === Global Search parser ===
-echo "Creating Global Search parser..."
-jison globalSearchParser.jison
-grunt uglify:globalSearchParser
-cat license.txt globalSearchParser.js > ../globalSearchParser.js
-rm globalSearchParser.js
-
-# === SOLR Expression parser ===
-echo "Creating SOLR Expression parser..."
-jison solrExpressionParser.jison
-grunt uglify:solrExpressionParser
-cat license.txt solrExpressionParser.js > ../solrExpressionParser.js
-rm solrExpressionParser.js
 
 popd
 echo "Done!"
