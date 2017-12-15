d78734152c4ddf0102cf4329d87b2936491f80a8
Romain Rigaux <romain@cloudera.com>
2017-12-14 19:03:21 -0800

    HUE-7803 [jb] Remove old reference to hadoop gen-py lib

--- desktop/libs/hadoop/src/hadoop/__init__.py
+++ desktop/libs/hadoop/src/hadoop/__init__.py
@@ -13,6 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-
-__path__.append(os.path.join(os.path.dirname(__file__), '..', '..', 'gen-py', 'hadoop'))
