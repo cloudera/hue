09c40b7bf5b4f9b1a0e88cfbbc19c3d9690a121a
Ivan Dzikovsky <idzikovsky@protonmail.com>
2018-06-06 23:07:08 +0300

    HUE-8351 [editor] Execution time is no available for sync sources (#711)

--- desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
+++ desktop/libs/notebook/src/notebook/static/notebook/js/notebook.ko.js
@@ -1500,6 +1500,7 @@ var EditorViewModel = (function() {
             self.loadData(data.result, 100);
             self.status('available');
             self.progress(100);
+            self.result.endTime(new Date());
           } else {
             if (! notebook.unloaded()) {
               self.checkStatus();
