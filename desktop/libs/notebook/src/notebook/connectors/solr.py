41c69862c605504852f4ffdc27faf45f4ca168c6
Johan Ahlen <johan@johanahlen.com>
2018-09-17 14:10:37 +0200

    HUE-8570 [editor] Enable optional operation on the sample API endpoints
    This enables distinct sample queries
    
    (cherry picked from commit 03a64a02a43fd898b39823917e27dd14cf990670)

--- desktop/libs/notebook/src/notebook/connectors/solr.py
+++ desktop/libs/notebook/src/notebook/connectors/solr.py
@@ -159,7 +159,7 @@ class SolrApi(Api):
 
 
   @query_error_handler
-  def get_sample_data(self, snippet, database=None, table=None, column=None, async=False):
+  def get_sample_data(self, snippet, database=None, table=None, column=None, async=False, operation=None):
     from search.conf import SOLR_URL
     db = NativeSolrApi(SOLR_URL.get(), self.user)
 
