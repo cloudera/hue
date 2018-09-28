2b8d2ce3f5c01d86fb2efb9fa1fc20a6347e09d5
Romain Rigaux <romain@cloudera.com>
2018-09-27 20:50:01 -0700

    HUE-8586 [notebook] Add selected cluster to the create session call

--- desktop/libs/notebook/src/notebook/connectors/base.py
+++ desktop/libs/notebook/src/notebook/connectors/base.py
@@ -314,7 +314,7 @@ def get_api(request, snippet):
   interface = interpreter['interface']
 
   # Multi cluster
-  cluster = json.loads(request.POST.get('cluster', '""')) # Via Catalog autocomplete API
+  cluster = json.loads(request.POST.get('cluster', '""')) # Via Catalog autocomplete API or Notebook create sessions
   if cluster == 'undefined':
     cluster = None
   if not cluster and snippet.get('compute'): # Via notebook.ko.js
