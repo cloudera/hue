1b069c95a3095a917695411e9e6616fa89a95ecf
Romain Rigaux <romain@cloudera.com>
2018-09-29 16:22:55 -0700

    [importer] Saving pipeline API skeleton

--- desktop/libs/indexer/src/indexer/models.py
+++ desktop/libs/indexer/src/indexer/models.py
@@ -15,6 +15,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from desktop.models import Document2, Document
+
+
+def _save_pipeline(pipeline, user):
+  pipeline_type = pipeline.get('type', 'pipeline')
+  save_as = False
+
+  if pipeline.get('parentSavedQueryUuid'): # We save into the original saved query, not into the query history
+    pipeline_doc = Document2.objects.get_by_uuid(user=user, uuid=pipeline['parentSavedQueryUuid'])
+  elif pipeline.get('id'):
+    pipeline_doc = Document2.objects.get(id=pipeline['id'])
+  else:
+    pipeline_doc = Document2.objects.create(name=pipeline['name'], uuid=pipeline['uuid'], type=pipeline_type, owner=user)
+    Document.objects.link(pipeline_doc, owner=pipeline_doc.owner, name=pipeline_doc.name, description=pipeline_doc.description, extra=pipeline_type)
+    save_as = True
+
+    if pipeline.get('directoryUuid'):
+      pipeline_doc.parent_directory = Document2.objects.get_by_uuid(user=user, uuid=pipeline.get('directoryUuid'), perm_type='write')
+    else:
+      pipeline_doc.parent_directory = Document2.objects.get_home_directory(user)
+
+  pipeline['isSaved'] = True
+  pipeline['isHistory'] = False
+  pipeline['id'] = pipeline_doc.id
+  pipeline_doc1 = pipeline_doc._get_doc1(doc2_type=pipeline_type)
+  pipeline_doc.update_data(pipeline)
+  #pipeline_doc.search = _get_statement(pipeline)
+  pipeline_doc.name = pipeline_doc1.name = pipeline['name']
+  pipeline_doc.description = pipeline_doc1.description = pipeline['description']
+  pipeline_doc.save()
+  pipeline_doc1.save()
+
+  return pipeline_doc, save_as
+
+
 FIELD_TYPES = (
   "alphaOnlySort",
   "ancestor_path",
