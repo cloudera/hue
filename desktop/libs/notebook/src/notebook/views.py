d15edf6a9ae3bc401dd2cfbaa999bba2de5508ab
Johan Ahlen <johan@johanahlen.com>
2018-10-02 14:51:02 +0200

    HUE-8607 [tb] Include namespace when querying a table from the table browser

--- desktop/libs/notebook/src/notebook/views.py
+++ desktop/libs/notebook/src/notebook/views.py
@@ -154,12 +154,16 @@ def browse(request, database, table, partition_spec=None):
 
   statement = get_api(request, snippet).get_browse_query(snippet, database, table, partition_spec)
   editor_type = snippet['type']
+  namespace = request.POST.get('namespace', 'default')
+  compute = request.POST.get('compute', 'default')
 
   if request.method == 'POST':
-    notebook = make_notebook(name='Execute and watch', editor_type=editor_type, statement=statement, status='ready-execute', is_task=True)
+    notebook = make_notebook(name='Execute and watch', editor_type=editor_type, statement=statement, status='ready-execute',
+                             is_task=True, namespace=namespace, compute=compute)
     return JsonResponse(notebook.execute(request, batch=False))
   else:
-    editor = make_notebook(name='Browse', editor_type=editor_type, statement=statement, status='ready-execute')
+    editor = make_notebook(name='Browse', editor_type=editor_type, statement=statement, status='ready-execute',
+                           namespace=namespace, compute=compute)
 
     return render('editor.mako', request, {
         'notebooks_json': json.dumps([editor.get_data()]),
