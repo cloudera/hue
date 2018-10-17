6efb58996e0770407ff6de3b83256782748985b2
Romain Rigaux <romain@cloudera.com>
2018-10-17 08:44:30 -0700

    HUE-8330 [core] API should not check for remote cloud clusters if they are not configured
    (cherry picked from commit 3c85e44524d099a1b6c6b0d7f0c1bc66c987e55e)
    
    Conflicts:
    	desktop/core/src/desktop/api2.py
    
    Change-Id: I796d4dc202f1f0ae1e75e473cd3788929c34be9c

--- desktop/core/src/desktop/api2.py
+++ desktop/core/src/desktop/api2.py
@@ -138,6 +138,7 @@ def get_context_computes(request, interface):
   computes = []
 
   clusters = get_clusters(request.user).values()
+  has_altus_clusters = [cluster for cluster in clusters if 'altus' in cluster['type']]
 
   if interface == 'hive' or interface == 'impala' or interface == 'oozie' or interface == 'report':
     computes.extend([{
@@ -149,13 +150,13 @@ def get_context_computes(request, interface):
       } for cluster in clusters if cluster.get('type') == 'direct'
     ])
 
-  if interface == 'impala' or interface == 'report':
-    if IS_K8_ONLY.get():
-      dw_clusters = DataWarehouse2Api(request.user).list_clusters()['clusters']
-    else:
-      dw_clusters = AnalyticDbApi(request.user).list_clusters()['clusters']
+  if has_altus_clusters:
+    if interface == 'impala' or interface == 'report':
+      if IS_K8S_ONLY.get():
+        dw_clusters = DataWarehouse2Api(request.user).list_clusters()['clusters']
+      else:
+        dw_clusters = AnalyticDbApi(request.user).list_clusters()['clusters']
 
-    if [cluster for cluster in clusters if cluster['type'] == 'altus']:
       computes.extend([{
           'id': cluster.get('crn'),
           'name': cluster.get('clusterName'),
@@ -166,8 +167,7 @@ def get_context_computes(request, interface):
         } for cluster in dw_clusters if (cluster.get('status') == 'CREATED' and cluster.get('cdhVersion') >= 'CDH515') or (IS_K8_ONLY.get() and cluster['status'] == 'ONLINE')]
       )
 
-  if interface == 'oozie' or interface == 'spark2':
-    if [cluster for cluster in clusters if cluster['type'] == 'altus']:
+    if interface == 'oozie' or interface == 'spark2':
       computes.extend([{
           'id': cluster.get('crn'),
           'name': cluster.get('clusterName'),
