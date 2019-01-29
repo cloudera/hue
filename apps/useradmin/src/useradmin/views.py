e8cf3583041e26ce5846c2f40076727095cf42cc
Ying Chen <yingchen@cloudera.com>
2019-01-29 12:02:43 -0800

    HUE-8693 [useradmin] Fix Security app only display 100 user in impersonate list
    (cherry picked from commit c169794ddd5b4444e78bce48f1f075303d8c3288)
    
    Change-Id: Ia8cf4b4fe9fe2c60b5dc4a818b49d5fece29dd0a

--- apps/useradmin/src/useradmin/views.py
+++ apps/useradmin/src/useradmin/views.py
@@ -100,6 +100,8 @@ def list_configurations(request):
 def list_for_autocomplete(request):
   extended_user_object = request.GET.get('extend_user') == 'true'
   autocomplete_filter = request.GET.get('filter', "")
+  count = int(request.GET.get("count", 100))
+
   if is_admin(request.user):
     users = User.objects.filter(username__icontains=autocomplete_filter).order_by('username')
     groups = Group.objects.filter(name__icontains=autocomplete_filter).order_by('name')
@@ -115,8 +117,8 @@ def list_for_autocomplete(request):
   if not request.GET.get('include_myself'):
     users = users.exclude(pk=request.user.pk)
 
-  users = users[:100]
-  groups = groups[:100]
+  users = users[:count]
+  groups = groups[:count]
 
   response = {
     'users': massage_users_for_json(users, extended_user_object),
