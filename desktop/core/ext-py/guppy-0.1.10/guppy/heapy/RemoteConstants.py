e9eefdf13cde909649aa6ba86a366d8b5c473be0
Ying Chen <yingchen@cloudera.com>
2019-03-28 15:52:31 -0700

    [core] Remove guppy-0.1.10 for Python 3.5

--- desktop/core/ext-py/guppy-0.1.10/guppy/heapy/RemoteConstants.py
+++
@@ -1,9 +0,0 @@
-# Constants used in communication between RemoteAccess and Monitor
-
-READLINE = 'READLINE\n'
-KEYBOARDINTERRUPT = 'KEYBOARDINTERRUPT\n'
-DONE = 'DONE\n'
-LOCALHOST = '127.0.0.1'
-#HEAPYPORT = 8834
-HEAPYPORT = 3546
-
