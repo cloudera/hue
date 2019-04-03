83fed219f56078baa3ab2c4d51364e00bd317fed
Ying Chen <yingchen@cloudera.com>
2019-04-03 15:52:51 -0700

    [core] Futurize tools/app_reg for Python 3.5

--- tools/app_reg/registry.py
+++ tools/app_reg/registry.py
@@ -19,6 +19,8 @@
 Registry for the applications
 """
 
+from past.builtins import cmp
+from builtins import object
 import glob
 import logging
 import os
@@ -56,7 +58,7 @@ class AppRegistry(object):
   def _write(self, path):
     """Write out the registry to the given path"""
     outfile = file(path, 'w')
-    json.dump(self._apps.values(), outfile, cls=AppJsonEncoder, indent=2)
+    json.dump(list(self._apps.values()), outfile, cls=AppJsonEncoder, indent=2)
     outfile.close()
 
   def contains(self, app):
@@ -98,7 +100,7 @@ class AppRegistry(object):
 
   def get_all_apps(self):
     """get_all_apps() -> List of HueApp"""
-    return self._apps.values()
+    return list(self._apps.values())
 
   def save(self):
     """Save and write out the registry"""
@@ -189,7 +191,7 @@ class HueApp(object):
           if not os.path.exists(cur):
             os.unlink(link_name)
             LOG.warn("Removing broken link: %s" % (link_name,))
-        except OSError, ex:
+        except OSError as ex:
           LOG.warn("Error checking for existing link %s: %s" % (link_name, ex))
 
       # Actually install the link
@@ -197,12 +199,12 @@ class HueApp(object):
         os.symlink(target, link_name)
         LOG.info('Symlink config %s -> %s' % (link_name, target))
         installed.append(link_name)
-      except OSError, ex:
+      except OSError as ex:
         LOG.error("Failed to symlink %s to %s: %s" % (target, link_name, ex))
         for lnk in installed:
           try:
             os.unlink(lnk)
-          except OSError, ex2:
+          except OSError as ex2:
             LOG.error("Failed to cleanup link %s: %s" % (link_name, ex2))
         return False
     return True
@@ -225,7 +227,7 @@ class HueApp(object):
         try:
           os.unlink(path)
           LOG.info('Remove config symlink %s -> %s' % (path, target))
-        except OSError, ex:
+        except OSError as ex:
           LOG.error("Failed to remove configuration link %s: %s" % (path, ex))
           return False
     return True
