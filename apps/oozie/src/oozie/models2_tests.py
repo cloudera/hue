c8526299c7e2e2a242a559559d86ce1293678603
Ying Chen <yingchen@cloudera.com>
2019-04-29 11:23:28 -0700

    HUE-8825 [oozie] Fix unit test oozie.models2_tests:TestEditor.test_share_workflow

--- apps/oozie/src/oozie/models2_tests.py
+++ apps/oozie/src/oozie/models2_tests.py
@@ -473,7 +473,7 @@ LIMIT $limit"""))
 
       # other user cannot view document
       response = self.client_not_me.get(reverse('oozie:edit_workflow'), {'uuid': wf_doc.uuid})
-      assert_equal(response.status_code, 401)
+      assert_equal(response.status_code, 500) # 401 error is caught and raise PopupException (500)
 
       # Share write perm by user
       if USE_NEW_EDITOR.get():
