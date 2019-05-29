ae298ca240b0fee80ff1a1da62135a48252683d2
Johan Ahlen <johan@johanahlen.com>
2019-05-29 13:44:17 +0200

    HUE-8848 [catalog] Enable faceted classification search for Atlas
    This adds 'tags:...', 'tag:...' and 'classification:...' from the top search and it shows the classifications in read-only mode in the table browser and context popover.

--- desktop/core/src/desktop/js/ko/components/ko.globalSearch.js
+++ desktop/core/src/desktop/js/ko/components/ko.globalSearch.js
@@ -152,7 +152,7 @@ class GlobalSearch {
         placeHolder: I18n(window.HAS_CATALOG ? 'Search data and saved documents...' : 'Search saved documents...'),
         querySpec: self.querySpec,
         onClear: function () { self.selectedIndex(null); self.searchResultVisible(false); },
-        facets: window.HAS_CATALOG_TAGS ? ['type', 'tags', 'parentPath', 'originalName'] : ['type'],
+        facets: window.HAS_READ_ONLY_CATALOG ? ['type', 'tag', 'classification'] : ['type', 'tag', 'parentPath', 'originalName'],
         knownFacetValues: self.knownFacetValues,
         autocompleteFromEntries: self.autocompleteFromEntries,
         triggerObservable: self.searchResultCategories
@@ -339,7 +339,9 @@ class GlobalSearch {
     const self = this;
     dataCatalog.getAllNavigatorTags({ silenceErrors: true }).done(facets => {
       const facetValues = self.knownFacetValues();
+      facetValues['tag'] = facets;
       facetValues['tags'] = facets;
+      facetValues['classification'] = facets;
     });
   }
 
