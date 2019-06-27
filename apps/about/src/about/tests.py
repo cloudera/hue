068de915a3939ffd965a383ae5b9865bb201b043
Ying Chen <yingchen@cloudera.com>
2019-06-26 17:03:02 -0700

    HUE-8737 [core] Futurize apps/about/ for Python 3.5

--- apps/about/src/about/tests.py
+++ apps/about/src/about/tests.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from builtins import object
 import json
 
 from django.contrib.auth.models import User
