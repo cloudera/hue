ad6f8d92d07d14bca4b5420216cfcaa8a22061c1
Travis Le <travis.le@dhcp-10-16-0-133.pa.cloudera.com>
2019-07-17 14:43:11 -0700

    Added more test conditions to ABFS Fixed Bugs

--- desktop/libs/azure/src/azure/abfs/abfs_test.py
+++ desktop/libs/azure/src/azure/abfs/abfs_test.py
@@ -19,6 +19,7 @@ from __future__ import absolute_import
 import logging
 import unittest
 
+import time
 from azure.abfs.abfs import ABFS
 from azure.active_directory import ActiveDirectory
 from azure.conf import ABFS_CLUSTERS, is_abfs_enabled
@@ -38,28 +39,39 @@ class ABFSTestBase(unittest.TestCase):
     if not is_abfs_enabled():
       raise SkipTest
     self.client = ABFS.from_config(ABFS_CLUSTERS['default'], ActiveDirectory.from_config(None, version='v2.0'))
+    self.test_fs = 'abfss://testfs' + (str(int(time.time()) ))
+    LOG.debug("%s" %self.test_fs)
+    self.client.mkdir(self.test_fs)
 
   def tearDown(self):
-    pass
+    self.client.rmtree(self.test_fs)
+    
 
   def test_list(self):
     filesystems = self.client.listdir('abfss://')
     LOG.debug("%s" %filesystems)
-    assert_true(None not in filesystems, filesystems)
+    assert_true(filesystems is not None, filesystems)
     
     pathing = self.client.listdir('abfss://' + filesystems[0])
     LOG.debug("%s" %pathing)
-    assert_true(None not in pathing, pathing)
+    assert_true(pathing is not None, pathing)
     
     directory = self.client.listdir('abfss://' + filesystems[0] + '/' + pathing[0])
     LOG.debug("%s" %directory)
-    assert_true(None not in directory, directory)
+    assert_true(directory is not None, directory)
     
-    ok2 = self.client.stats('abfss://' + filesystems[0] + '/' + pathing[0])
+    directory = self.client.listdir(self.test_fs)
+    LOG.debug("%s" %directory)
+    assert_true(directory is not None, directory)
+  
+    
+  def test_stats(self):
+    ok2 = self.client.stats(self.test_fs)
     LOG.debug("%s" %ok2)
     
-    ok2 = self.client.listdir_stats('abfss://' + filesystems[0])
+    ok2 = self.client.listdir_stats(self.test_fs)
     LOG.debug("%s" %ok2)
+    
     
   def test_mkdir(self):
     filesystems = self.client.listdir('abfss://')
@@ -75,3 +87,30 @@ class ABFSTestBase(unittest.TestCase):
     
     pathing = self.client.listdir('abfss://' + filesystems[1])
     LOG.debug("%s" %pathing)
+    
+  def test_createandread(self):
+    test_fs = self.test_fs
+    test_file = test_fs + '/test.txt'
+    self.client.create(test_file)
+    self.client.read(test_file)
+    self.client.remove(test_file)
+  
+  def test_rename(self):
+    test_fs = self.test_fs
+    test_dir = test_fs + '/test'
+    test_file = test_fs + '/test.txt'
+    self.client.mkdir(test_dir)
+    ok2 = self.client.listdir_stats(self.test_fs)
+    LOG.debug("%s" %ok2)
+    
+    self.client.rename(test_dir, test_fs + '/' + 'help')
+    ok2 = self.client.listdir_stats(self.test_fs)
+    LOG.debug("%s" %ok2)
+    
+    self.client.create(test_file)
+    ok2 = self.client.listdir_stats(self.test_fs)
+    LOG.debug("%s" %ok2)
+    
+    self.client.rename(test_file, test_fs + '/' + 'help.txt')
+    ok2 = self.client.listdir_stats(self.test_fs)
+    LOG.debug("%s" %ok2)
