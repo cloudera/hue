6a90a8f4fbb0eab168263642ce2a49f456cad13b
Prakash Ranade <ranade@cloudera.com>
2019-07-19 19:08:26 -0700

    Adding Hue build information

--- tools/container/build.sh
+++ tools/container/build.sh
@@ -15,22 +15,40 @@ compile_hue() {
   PREFIX=$BUILD_DIR make install
   cd $BUILD_DIR/hue
   APPS=$(find apps -maxdepth 2 -name "src" -type d|cut -d"/" -f2|sort| sed 's/[^ ]* */apps\/&/g')
-  python tools/app_reg/app_reg.py --install $APPS --relative-paths
+  ./build/env/bin/python tools/app_reg/app_reg.py --install $APPS --relative-paths
   bash tools/relocatable.sh
+}
+
+find_git_state() {
+  cd $HUE_SRC
+  export GBRANCH=$(git ls-remote  --get-url)"/commits/"$(git rev-parse --abbrev-ref HEAD)
+  export GSHA=$(git ls-remote  --get-url)"/commit/"$(git rev-list --no-walk HEAD)
+  export VERSION=$(grep "VERSION=" VERSION | cut -d"=" -f2 | cut -d'"' -f2)
 }
 
 docker_hue_build() {
   cd $HUE_DIR
   cp -a $BUILD_DIR/hue $HUE_DIR
-  docker build -f $HUE_DIR/Dockerfile -t ${REGISTRY}/hue:$GBN .
+  docker build -f $HUE_DIR/Dockerfile -t ${REGISTRY}/hue:$GBN \
+    --build-arg GBN=$GBN \
+    --build-arg GSHA="$GSHA" \
+    --build-arg GBRANCH=$GBRANCH \
+    --build-arg VERSION=$VERSION \
+    .
 }
 
 docker_huelb_build() {
   cd $APACHE_DIR
   cp -a $BUILD_DIR/hue/build/static $APACHE_DIR
-  docker build -f $APACHE_DIR/Dockerfile -t ${REGISTRY}/huelb:$GBN .
+  docker build -f $APACHE_DIR/Dockerfile -t ${REGISTRY}/huelb:$GBN \
+    --build-arg GBN=$GBN \
+    --build-arg GSHA="$GSHA" \
+    --build-arg GBRANCH=$GBRANCH \
+    --build-arg VERSION=$VERSION \
+    .
 }
 
 compile_hue
+find_git_state
 docker_hue_build
 docker_huelb_build
