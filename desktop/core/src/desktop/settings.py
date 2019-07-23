3de66d8532a729c3189b889d41c135b524fa837a
Travis Le <travis.le@dhcp-10-16-0-133.pa.cloudera.com>
2019-07-23 14:48:53 -0700

    Added an upload class Added the upload test_case to ABFS_testcases

--- desktop/core/src/desktop/settings.py
+++ desktop/core/src/desktop/settings.py
@@ -36,6 +36,7 @@ from desktop.lib.paths import get_desktop_root
 from desktop.lib.python_util import force_dict_to_strings
 
 from aws.conf import is_enabled as is_s3_enabled
+from azure.conf import is_abfs_enabled
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', '..', '..'))
@@ -569,6 +570,8 @@ file_upload_handlers = [
 if is_s3_enabled():
   file_upload_handlers.insert(0, 'aws.s3.upload.S3FileUploadHandler')
 
+if is_abfs_enabled():
+  file_upload_handlers.insert(0, 'azure.abfs.upload.ABFSFileUploadHandler')
 FILE_UPLOAD_HANDLERS = tuple(file_upload_handlers)
 
 ############################################################
