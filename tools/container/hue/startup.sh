95b38411e2aa3c732fcce57d1e3bc5c2f2842b08
Prakash Ranade <ranade@cloudera.com>
2019-07-27 19:07:37 -0700

    Adding DWX related changes
    Change-Id: I8508ce892815f764a3e2bc44460f3cd995e0a0ce

--- tools/container/hue/startup.sh
+++ tools/container/hue/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export DESKTOP_LOG_DIR=${HUE_LOG_DIR:-"/var/log/hue"}
+export PYTHON_EGG_CACHE=$HUE_CONF_DIR/.python-eggs
+export SERVER_SOFTWARE="apache"
+
 $HUE_BIN/hue syncdb --noinput
 $HUE_BIN/hue migrate
-$HUE_BIN/hue runserver 0.0.0.0:8888
+$HUE_BIN/hue runcherrypyserver
