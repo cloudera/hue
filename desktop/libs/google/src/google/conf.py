8954726d2a279b58517116458ad754bb4019c553
Travis Le <travis.le@dhcp-10-16-0-133.pa.cloudera.com>
2019-08-15 11:44:45 -0700

    Added more methods to conf for googlefs

--- desktop/libs/google/src/google/conf.py
+++ desktop/libs/google/src/google/conf.py
@@ -23,7 +23,9 @@ from django.utils.translation import ugettext_lazy as _, ugettext as _t
 from desktop.lib.conf import Config, UnspecifiedConfigSection, ConfigSection, coerce_password_from_script
 from hadoop.core_site import get_google_access_key, get_google_secret_key, get_goggle_refresh_token, get_google_access_token
 
-def get_default_access_key_id():
+PERMISSION_ACTION_GOOGLE = "google_access"
+
+def get_default_client_id():
   """
   Attempt to set AWS access key ID from script, else core-site, else None
   """
@@ -35,7 +37,7 @@ def get_default_secret_key():
   """
   Attempt to set google secret key from script, else core-site, else None
   """
-  secret_access_key_script = GOOGLE_ACCOUNTS['default'].SECRET_ACCESS_KEY_SCRIPT.get()
+  secret_access_key_script = GOOGLE_ACCOUNTS['default'].SECRET_KEY_SCRIPT.get()
   return secret_access_key_script or get_google_secret_key()
 
 
@@ -52,24 +54,24 @@ GOOGLE_ACCOUNTS = UnspecifiedConfigSection(
   each=ConfigSection(
     help=_('Information about single Google account'),
     members=dict(
-      ACCESS_KEY_ID=Config(
+      CLIENT_ID=Config(
         key='access_key_id',
         type=str,
         dynamic_default=get_default_access_key_id
       ),
-      ACCESS_KEY_ID_SCRIPT=Config(
+      CLIENT_ID_SCRIPT=Config(
         key='access_key_id_script',
         default=None,
         private=True,
         type=coerce_password_from_script,
         help=_("Execute this script to produce the AWS access key ID.")),
-      SECRET_ACCESS_KEY=Config(
+      SECRET_KEY=Config(
         key='secret_access_key',
         type=str,
         private=True,
         dynamic_default=get_default_secret_key
       ),
-      SECRET_ACCESS_KEY_SCRIPT=Config(
+      SECRET_KEY_SCRIPT=Config(
         key='secret_access_key_script',
         default=None,
         private=True,
@@ -79,3 +81,10 @@ GOOGLE_ACCOUNTS = UnspecifiedConfigSection(
     )
   )
 )
+
+def is_googlefs_enabled():
+  return ('default' in list(GOOGLE_ACCOUNTS_ACCOUNTS.keys()) and GOOGLE_ACCOUNTS['default'].get_raw() and GOOGLE_ACCOUNTS['default'].CLIENT_ID.get() is not None )
+
+def has_googlefs_access(user):
+  from desktop.auth.backend import is_admin
+  return user.is_authenticated() and user.is_active and (is_admin(user) or user.has_hue_permission(action="google_access", app="filebrowser"))
