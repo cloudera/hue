04c13bd3a66ee3c03e11f34a058b06adbac824d2
Romain <romain.rigaux@gmail.com>
2019-08-22 09:43:27 -0700

    HUE-8758 [connectors] Ordered intrepreter is not valid anymore with connectors
    (cherry picked from commit 3ea767b31b992945fdc6f3a0ab059c9a2c753510)
    
    Change-Id: I01280f02703d88fbfe69bc4215779f404aca7ea1

--- desktop/libs/notebook/src/notebook/tasks.py
+++ desktop/libs/notebook/src/notebook/tasks.py
@@ -154,6 +154,7 @@ def run_sync_query(doc_id, user):
   if type(user) is str:
     lookup = {'email' if ENABLE_ORGANIZATIONS.get() else 'username': user}
     user = User.objects.get(**lookup)
+    user = rewrite_user(user)
 
   query_document = Document2.objects.get_by_uuid(user=user, uuid=doc_id)
   notebook = Notebook(document=query_document).get_data()
