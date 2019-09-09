ae019d799727bafb8b656f1040f8530e6bfdd6b8
Prakash Ranade <ranade@cloudera.com>
2019-09-09 16:15:56 -0700

    HUE-8993 [core] Add supervisor based Hue process management inside docker container
    Change-Id: I1a1e5bd661c072e52f3dcec395d87e17cc84891a

--- tools/container/hue/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export DESKTOP_LOG_DIR="/var/log/hue"
-export PYTHON_EGG_CACHE=$HUE_CONF_DIR/.python-eggs
-export SERVER_SOFTWARE="apache"
-
-$HUE_BIN/hue syncdb --noinput
-$HUE_BIN/hue migrate
-$HUE_BIN/hue runcherrypyserver
