907a340973dc167a3d7be93581a40ae9c8133bba
Romain <romain.rigaux@gmail.com>
2019-10-30 12:05:33 +0100

    HUE-9077 [ksql] Support SHOW QUERIES
    (cherry picked from commit 221554ac21fc8a3a82066e3bde3cfaee9a272ceb)
    
    Change-Id: I93c039f01ffc90bce390b7f75c25ce82b68b8bea

--- desktop/libs/kafka/src/kafka/ksql_client.py
+++ desktop/libs/kafka/src/kafka/ksql_client.py
@@ -104,16 +104,17 @@ class KSqlApi(object):
 
     if result['@type'] == 'statement_error':
       raise KSqlApiException(result['message'])
-    elif result['@type'] in ('kafka_topics', 'streams', 'tables'):
+    elif result['@type'] in ('kafka_topics', 'streams', 'tables', 'queries'):
       result_type = 'topics' if result['@type'] == 'kafka_topics' else result['@type']
-      columns = result[result_type][0].keys()
-      for line in result[result_type]:
-        row = []
-        for column in columns:
-          row.append(
-            json.dumps(line[column])
-          )
-        data.append(row)
+      if result[result_type]:
+        columns = result[result_type][0].keys()
+        for line in result[result_type]:
+          row = []
+          for column in columns:
+            row.append(
+              json.dumps(line[column])
+            )
+          data.append(row)
     elif result['@type'] == 'sourceDescription':
       columns = ['name', 'description']
       for key in result['sourceDescription']:
