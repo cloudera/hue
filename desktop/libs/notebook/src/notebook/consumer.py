44a2972dced2f5d6c4bcf46f7a34ae287fa3f343
Romain <romain.rigaux@gmail.com>
2019-10-31 15:25:26 +0100

    HUE-9064 [ws] Parameterize channels config to be setup
    For py3 only. No change in py2 and off by default.
    Skeleton only so non documented currently.
    Next step is TaskServer / Editor integration. These commits will enable
    making progress on it.
    
    (cherry picked from commit 1383cc848fc49c1533e32c919729ca09333f9d8d)
    
    Change-Id: Ic3b67a75f940e25ec73bcb615bfca031a02a6523

--- desktop/libs/notebook/src/notebook/consumer.py
+++ desktop/libs/notebook/src/notebook/consumer.py
@@ -1,28 +1,50 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+#!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
-class ChatConsumer(AsyncWebsocketConsumer):
+from desktop.conf import has_channels
 
-  async def connect(self):
-    await self.accept()
-
-    await self.send(text_data=json.dumps({
-      'type': 'channel_name',
-      'data': self.channel_name,
-      'accept': True
-    }))
+if has_channels():
+  from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-  async def task_progress(self, event):
-    await self.send(text_data=json.dumps({
-      'type': 'task_progress',
-      'data': event["data"]
-    }))
+  class EditorConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+      await self.accept()
+
+      await self.send(text_data=json.dumps({
+        'type': 'channel_name',
+        'data': self.channel_name,
+        'accept': True
+      }))
 
 
-def _send_to_channel(channel_name, message_type, message_data):
-  channel_layer = get_channel_layer()
-  async_to_sync(channel_layer.send)(channel_name, {
-      "type": message_type,
-      "data": message_data,
-  })
+    async def task_progress(self, event):
+      await self.send(text_data=json.dumps({
+        'type': 'task_progress',
+        'data': event["data"]
+      }))
+
+
+  def _send_to_channel(channel_name, message_type, message_data):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.send)(channel_name, {
+        "type": message_type,
+        "data": message_data,
+    })
