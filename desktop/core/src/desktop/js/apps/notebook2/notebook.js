e637b31ff26ecd5583758404a917f0a00e028c32
Johan Ahlen <johan@johanahlen.com>
2019-11-13 15:12:54 +0100

    HUE-8900 [editor] Make sure the notebook has a name set in editor v2

--- desktop/core/src/desktop/js/apps/notebook2/notebook.js
+++ desktop/core/src/desktop/js/apps/notebook2/notebook.js
@@ -22,6 +22,7 @@ import apiHelper from 'api/apiHelper';
 import hueAnalytics from 'utils/hueAnalytics';
 import huePubSub from 'utils/huePubSub';
 import hueUtils from 'utils/hueUtils';
+import I18n from 'utils/i18n';
 import sessionManager from 'apps/notebook2/execution/sessionManager';
 
 import Snippet, { STATUS as SNIPPET_STATUS } from 'apps/notebook2/snippet';
@@ -31,7 +32,7 @@ export default class Notebook {
     this.parentVm = vm;
     this.id = ko.observable(notebook.id);
     this.uuid = ko.observable(notebook.uuid || hueUtils.UUID());
-    this.name = ko.observable(notebook.name || '');
+    this.name = ko.observable(notebook.name || I18n('My Notebook'));
     this.description = ko.observable(notebook.description || '');
     this.type = ko.observable(notebook.type || 'notebook');
     this.initialType = this.type().replace('query-', '');
