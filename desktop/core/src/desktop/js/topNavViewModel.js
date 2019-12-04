8d0087ab6f87c6cb96c0570ca1dcb35cb92bb306
Shin So <zzeng@cloudera.com>
2019-12-05 00:41:23 +0900

    PR1007 [core] Fix the URL for "Add more" button . Issue #1006 (#1007)

--- desktop/core/src/desktop/js/topNavViewModel.js
+++ desktop/core/src/desktop/js/topNavViewModel.js
@@ -104,7 +104,7 @@ class TopNavViewModel {
             interpreters.push({
               displayName: I18n('Add more...'),
               dividerAbove: true,
-              href: 'http://gethue.com/sql-editor/'
+              href: 'http://gethue.com/new-sql-editor/'
             });
           }
 
