2db6468b729e9896c1bf70a3fe0900eba551f739
Romain <romain.rigaux@gmail.com>
2020-01-17 17:24:02 -0600

    HUE-8530 [organization] Backward compatibility for User.objects.values_list
    (cherry picked from commit ffbe9b2b47028405b6b3072b71cb89f7031510a0)

--- apps/useradmin/src/useradmin/models2_tests.py
+++ apps/useradmin/src/useradmin/models2_tests.py
@@ -129,5 +129,7 @@ class TestOrganizationSingleUser(unittest.TestCase):
 
     User.objects.filter(groups__in=Group.objects.all()).order_by('username')
 
+    User.objects.values_list('username', flat=True)
+
     self.client2.get('/useradmin/groups/edit/default')
 
