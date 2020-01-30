c4d60b34e00cf65340375760e36fcc9681855165
Romain <romain.rigaux@gmail.com>
2020-01-30 13:30:31 -0800

    HUE-8530 [organization] Adding crequest module dependency
    (cherry picked from commit e9e813e9ea629999c97d966baa5a594af11d3e2b)
    
    Change-Id: I1c63c7be6e913678fb92c77254013a77f1e9f411

---
+++ desktop/core/ext-py/django-crequest-2018.5.11/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-crequest',
+    version=__import__('crequest').__version__,
+    description='Middleware that makes the current request available from anywhere.',
+    long_description=open('README.rst').read(),
+    license=open('LICENSE').read(),
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
+    url='https://github.com/Alir3z4/django-crequest',
+    packages=find_packages(exclude=['django_crequest']),
+    install_requires=['Django>=1.2'],
+    keywords=[
+        'django',
+        'request',
+        'web'
+    ],
+    platforms='OS Independent',
+    provides=['crequest',],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
