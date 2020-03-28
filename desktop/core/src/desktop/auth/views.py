4bc14351e2686e6973d38f98c1e9ce3cecd1ebbd
Ying Chen <yingchen@cloudera.com>
2020-03-27 17:09:17 -0700

    HUE-9212 [core] Fix missing login-modal causes auto logout failed
    (cherry picked from commit a037d0746d75bf89a3c4e5d9f3cbd0e184c738c0)
    
    Change-Id: Ib94ae4d08e4429787f6b51961f3a0aafe0811fd5

--- desktop/core/src/desktop/auth/views.py
+++ desktop/core/src/desktop/auth/views.py
@@ -186,7 +186,7 @@ def dt_login(request, from_modal=False):
         ensure_home_directory(request.fs, request.user)
       except (IOError, WebHdfsException) as e:
         LOG.error('Could not create home directory for %s user %s.' % ('OIDC' if 'OIDCBackend' in backend_names else 'SAML', request.user))
-    if request.user.is_authenticated():
+    if request.user.is_authenticated() and not from_modal:
       return HttpResponseRedirect(redirect_to)
 
   if is_active_directory and not is_ldap_option_selected and \
