62f025aca356a682908703e6a5997d09a3fcc967
Johan Ahlen <johan@johanahlen.com>
2020-04-06 14:12:59 +0200

    HUE-9236 [frontend] Remove Ace build related packages from the main package.json
    This also takes care of a bug when building the ace editor on a fresh install
    
    (cherry picked from commit 32a5638a26ee77091b856ad7ceb441cad8b25910)
    
    Change-Id: I7819a7c82e8307be99ee1bb9e31e992775476103

--- desktop/core/src/desktop/static/desktop/js/ace/snippets/druid.js
+++ desktop/core/src/desktop/static/desktop/js/ace/snippets/druid.js
@@ -1,1 +1,1 @@
-ace.define("ace/snippets/druid",["require","exports","module"],function(e,t,n){"use strict";t.snippetText="",t.scope="druid"})
+ace.define("ace/snippets/druid",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="druid"})
