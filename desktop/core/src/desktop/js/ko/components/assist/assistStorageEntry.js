a8161404d47cde6e284e1284ae0c8ac68f8550f0
Johan Ahlen <johan@johanahlen.com>
2020-04-16 16:33:11 +0200

    HUE-9256 [editor] Fix js exception in the file preview context popover

--- desktop/core/src/desktop/js/ko/components/assist/assistStorageEntry.js
+++ desktop/core/src/desktop/js/ko/components/assist/assistStorageEntry.js
@@ -350,8 +350,8 @@ class AssistStorageEntry {
     type = type.replace(/abfs.*/i, 'abfs');
 
     huePubSub.publish(GET_KNOWN_CONFIG_EVENT, config => {
-      if (config && config.app_config && config.app_config.browsers) {
-        const source = config.app_config.browsers.interpreters.find(
+      if (config && config.app_config && config.app_config.browser) {
+        const source = config.app_config.browser.interpreters.find(
           interpreter => interpreter.type === type
         );
         if (source) {
