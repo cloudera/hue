dbc4f4d4f9671345504fd8352ab4ad832ffc1967
Johan Ahlen <johan@johanahlen.com>
2020-04-27 14:15:53 +0200

    HUE-9191 [ui] Show foreign key details in a tooltip for entries in the DB assist
    (cherry picked from commit 5b36ae0ba573156d0ab2f165e58f95205489b507)
    
    Change-Id: Iee923ef708fb64b1ce9b9eb26ed77f5cb7ce504a

---
+++ desktop/core/src/desktop/js/ko/components/assist/ko.assistKey.js
@@ -0,0 +1,76 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ko from 'knockout';
+
+import componentUtils from 'ko/components/componentUtils';
+import huePubSub from 'utils/huePubSub';
+import I18n from 'utils/i18n';
+import { ASSIST_DB_HIGHLIGHT_EVENT } from './events';
+
+import 'ko/bindings/ko.tooltip';
+
+export const NAME = 'hue-assist-key';
+
+const TEMPLATE = `
+  <!-- ko if: entry.definition.foreignKey -->
+    <!-- ko if: entry.definition.foreignKey.to -->
+      <a class="assist-field-link" style="padding: 0 2px;" href="javascript: void(0);" data-bind="
+          tooltip: {
+            title: foreignKeyTooltipHtml,
+            html: true,
+            placement: 'bottom'
+          },
+          click: highlight
+        "><i class="fa fa-key"></i></a>
+    <!-- /ko -->
+  <!-- /ko -->
+  <!-- ko if: entry.definition.primaryKey -->
+    <i class="fa fa-key" style="padding: 0 2px;" data-bind="
+        tooltip: {
+          title: '${I18n('Primary Key')}',
+          placement: 'bottom'
+        }"></i>
+  <!-- /ko -->
+  <!-- ko ifnot: entry.definition.foreignKey || entry.definition.primaryKey -->
+  <i class="fa fa-key"></i>
+  <!-- /ko -->
+`;
+
+class AssistKey {
+  constructor(entry) {
+    this.entry = entry;
+
+    this.foreignKeyTooltipHtml = ko.pureComputed(
+      () => `<i style="margin-right: 4px" class="fa fa-arrow-right"></i>
+    ${entry.definition.foreignKey && entry.definition.foreignKey.to}`
+    );
+  }
+
+  highlight(entry, event) {
+    this.entry.dataCatalog
+      .getEntry({
+        path: this.entry.definition.foreignKey.to,
+        namespace: this.entry.namespace,
+        compute: this.entry.compute
+      })
+      .then(entry => {
+        huePubSub.publish(ASSIST_DB_HIGHLIGHT_EVENT, entry);
+      });
+  }
+}
+
+componentUtils.registerComponent(NAME, AssistKey, TEMPLATE);
