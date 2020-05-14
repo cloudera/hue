50abf47fcace14585b4f087680b12b8f8303e4cd
Romain <romain.rigaux@gmail.com>
2020-05-14 13:19:32 -0700

    [connector] Support connector to same dialect but different interface
    Could have introduced a connector type id instead, but adding the
    interface name is more explicit and not really an overhead.

--- desktop/core/src/desktop/lib/connectors/urls.py
+++ desktop/core/src/desktop/lib/connectors/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
   url(r'^api/types/?$', api.get_connector_types, name='connectors.api.get_connector_types'),
   url(r'^api/instances/?$', api.get_connectors_instances, name='connectors.api.get_connectors_instances'),
 
-  url(r'^api/instance/new/(?P<dialect>[\w\-]+)$', api.new_connector, name='connectors.api.new_connector'),
+  url(r'^api/instance/new/(?P<dialect>[\w\-]+)/(?P<interface>[\w\-]+)$', api.new_connector, name='connectors.api.new_connector'),
   url(r'^api/instance/get/(?P<id>\d+)$', api.get_connector, name='connectors.api.get_connector'),
   url(r'^api/instance/delete/?$', api.delete_connector, name='connectors.api.delete_connector'),
   url(r'^api/instance/update/?$', api.update_connector, name='connectors.api.update_connector'),
