9cf1c48565f0cdf57e82fb941b25436003f399dc
Johan Ahlen <johan@johanahlen.com>
2020-06-16 12:35:16 +0200

    HUE-9377 [ui] Upgrade jest and linters to latest versions

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'desktop/core/src/desktop/js'],
   modulePaths: ['desktop/core/src/desktop/js'],
   testMatch: ['<rootDir>/desktop/core/src/desktop/js/**/*.test.(js|jsx|ts|tsx)'],
-  testEnvironment: 'jest-environment-jsdom-fourteen',
+  testEnvironment: 'jest-environment-jsdom-sixteen',
   testURL: 'https://www.gethue.com/hue',
   setupFilesAfterEnv: ['<rootDir>/desktop/core/src/desktop/js/jest/jest.init.js'],
   watchPathIgnorePatterns: ['<rootDir>/desktop/core/src/desktop/static'],
