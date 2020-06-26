c3d0a6fa63bea5770193bc38588654ca13ba62d6
Johan Ahlen <johan@johanahlen.com>
2020-06-26 14:40:25 +0200

    HUE-9392 [ui] Adjust Vue indentation linter rules

--- desktop/core/src/desktop/js/apps/jobBrowser/components/hiveQueryPlan.vue
+++ desktop/core/src/desktop/js/apps/jobBrowser/components/hiveQueryPlan.vue
@@ -5,22 +5,22 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { wrap } from 'vue/webComponentWrapper';
-import Component from 'vue-class-component';
+  import Vue from 'vue';
+  import { wrap } from 'vue/webComponentWrapper';
+  import Component from 'vue-class-component';
 
-export const HIVE_QUERY_PLAN_COMPONENT = 'hive-query-plan';
+  export const HIVE_QUERY_PLAN_COMPONENT = 'hive-query-plan';
 
-@Component
-export default class HiveQueryPlan extends Vue {
-  title: string = 'Web Components FTW.';
-}
+  @Component
+  export default class HiveQueryPlan extends Vue {
+    title: string = 'Web Components FTW.';
+  }
 
-wrap(HIVE_QUERY_PLAN_COMPONENT, HiveQueryPlan);
+  wrap(HIVE_QUERY_PLAN_COMPONENT, HiveQueryPlan);
 </script>
 
 <style lang="scss" scoped>
-.query-plan {
-  color: #0a78a3;
-}
+  .query-plan {
+    color: #0a78a3;
+  }
 </style>
