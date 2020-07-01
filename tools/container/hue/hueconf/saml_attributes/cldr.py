b281eee6cc12763d1bf32cf0ee3a60d803c84b6a
Prakash Ranade <ranade@cloudera.com>
2020-06-30 17:23:05 -0700

    HUE-9398 [core] Add attribute_map_dir saml configuration. (#1196)
    * HUE-9396 [core] Implement SAML groups check and redirect them to 403 page if not permitted.
    
    Testing Done:
    - Tested on private setup
    
    * HUE-9398 [core] Add attribute_map_dir saml configuration.
    
    "attribute_map_dir": "attribute-maps" Points to a directory which has the attribute maps in Python modules.

---
+++ tools/container/hue/hueconf/saml_attributes/cldr.py
@@ -0,0 +1,17 @@
+MAP = {
+  "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified",
+  "fro": {
+    'uid': 'uid',
+    'https://cdp.cloudera.com/SAML/Attributes/groups': 'groups',
+    'urn:oid:2.5.4.4': 'last_name',
+    'urn:oid:2.5.4.42': 'first_name',
+    'urn:oid:0.9.2342.19200300.100.1.3': 'email'
+  },
+  "to": {
+    'uid': 'uid',
+    'groups': 'https://cdp.cloudera.com/SAML/Attributes/groups',
+    'last_name': 'urn:oid:2.5.4.4',
+    'first_name': 'urn:oid:2.5.4.42',
+    'email': 'urn:oid:0.9.2342.19200300.100.1.3'
+  }
+}
