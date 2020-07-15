3d8c15f4d91b9063327947bfd4d6701959d44f89
sreenaths <sree@apache.org>
2020-07-16 03:28:15 +0530

    HUE-9412. [ui] Add tests for ERD component (sree)
    (cherry picked from commit 0502e4caab94def31dd9670781596fc4ef1c0647)

--- desktop/core/src/desktop/js/components/er-diagram/er-diagram.test.ts
+++ desktop/core/src/desktop/js/components/er-diagram/er-diagram.test.ts
@@ -17,9 +17,13 @@
  */
 
 import ERDiagram from './index.vue';
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
+import { Table } from './lib/entities';
+import { createTables } from './test/utils';
 
-describe('ERDiagram', () => {
+import ERDData from './test/data.json';
+
+describe('ERDiagram UTs', () => {
   test('Empty instance created', () => {
     const wrapper = shallowMount(ERDiagram, {
       propsData: {
@@ -28,14 +32,147 @@ describe('ERDiagram', () => {
       }
     });
 
-    expect(wrapper.exists()).toBe(true);
-    expect(wrapper.find('[title="Toggle Fullscreen"]').exists()).toBe(true);
-    expect(wrapper.find('.erd-scroll-panel').exists()).toBe(true);
-    expect(wrapper.find('svg.erd-relations').exists()).toBe(true);
+    expect(wrapper.props('entities')).toHaveLength(0);
+    expect(wrapper.props('relations')).toHaveLength(0);
 
-    expect(wrapper.classes('er-diagram')).toBe(true);
+    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find('[title="Toggle Fullscreen"]').exists()).toBeTruthy();
+    expect(wrapper.find('.erd-scroll-panel').exists()).toBeTruthy();
+    expect(wrapper.find('svg.erd-relations').exists()).toBeTruthy();
 
-    expect(wrapper.props('entities').length).toBe(0);
-    expect(wrapper.props('relations').length).toBe(0);
+    expect(wrapper.classes('er-diagram')).toBeTruthy();
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(0);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(0);
+  });
+
+  test('Single entity', () => {
+    const wrapper = shallowMount(ERDiagram, {
+      propsData: {
+        entities: [new Table('db-name', 'table-name', [])],
+        relations: []
+      }
+    });
+
+    expect(wrapper.props('entities')).toHaveLength(1);
+    expect(wrapper.props('relations')).toHaveLength(0);
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(1);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(0);
+  });
+
+  test('Multiple unrelated entities (t0, t1, t2, t3, t4)', () => {
+    const tableCount = 5;
+
+    const wrapper = shallowMount(ERDiagram, {
+      propsData: {
+        entities: createTables(tableCount, 0),
+        relations: []
+      }
+    });
+
+    expect(wrapper.props('entities')).toHaveLength(tableCount);
+    expect(wrapper.props('relations')).toHaveLength(0);
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(tableCount);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(0);
+  });
+
+  test('Related entities - 3 levels (t0-t1, t0-t2, t0-t3, t3-t4, t3-t5)', () => {
+    const tableCount = 6;
+
+    const tables: Array<Table> = createTables(tableCount, 4);
+    const wrapper = shallowMount(ERDiagram, {
+      propsData: {
+        entities: tables,
+        relations: [
+          {
+            desc: '',
+            left: tables[0].columns[1],
+            right: tables[1].columns[0]
+          },
+          {
+            desc: '',
+            left: tables[0].columns[2],
+            right: tables[2].columns[0]
+          },
+          {
+            desc: '',
+            left: tables[0].columns[3],
+            right: tables[3].columns[0]
+          },
+          {
+            desc: '',
+            left: tables[3].columns[3],
+            right: tables[4].columns[0]
+          },
+          {
+            desc: '',
+            left: tables[3].columns[3],
+            right: tables[5].columns[0]
+          }
+        ]
+      }
+    });
+
+    expect(wrapper.props('entities')).toHaveLength(tableCount);
+    expect(wrapper.props('relations')).toHaveLength(5);
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(tableCount);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(5);
+
+    expect(wrapper.findAll('.entity-group')).toHaveLength(3);
+  });
+});
+
+describe('ERDiagram integration tests', () => {
+  test('Check plotting of relation paths (t0-t1, t0-t2)', () => {
+    const tableCount = 3;
+
+    const tables: Array<Table> = createTables(tableCount, 3);
+    const wrapper = mount(ERDiagram, {
+      propsData: {
+        entities: tables,
+        relations: [
+          {
+            desc: '',
+            left: tables[0].columns[1],
+            right: tables[1].columns[0]
+          },
+          {
+            desc: '',
+            left: tables[0].columns[2],
+            right: tables[2].columns[0]
+          }
+        ]
+      }
+    });
+
+    expect(wrapper.props('entities')).toHaveLength(tableCount);
+    expect(wrapper.props('relations')).toHaveLength(2);
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(tableCount);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(2);
+
+    expect(wrapper.findAll('.relation-path').at(0).attributes('d')).toBe('M 0,1 C 40,1 -40,1 0,1');
+    expect(wrapper.findAll('.relation-path').at(1).attributes('d')).toBe('M 0,1 C 40,1 -40,1 0,1');
+  });
+
+  test('Real data test', () => {
+    const wrapper = mount(ERDiagram, {
+      propsData: ERDData
+    });
+
+    expect(wrapper.props('entities')).toHaveLength(3);
+    expect(wrapper.props('relations')).toHaveLength(3);
+
+    expect(wrapper.findAll('.entity-container')).toHaveLength(3);
+    expect(wrapper.findAll('.relation-path')).toHaveLength(3);
+
+    expect(wrapper.findAll('.entity-group')).toHaveLength(2);
+
+    expect(wrapper.findAll('.relation-path').at(0).attributes('d')).toBe('M 0,1 C 40,1 -40,1 0,1');
+    expect(wrapper.findAll('.relation-path').at(1).attributes('d')).toBe('M 0,1 C 40,1 -40,1 0,1');
+    expect(wrapper.findAll('.relation-path').at(2).attributes('d')).toBe('M 0,1 C 40,1 -40,1 0,1');
   });
 });
