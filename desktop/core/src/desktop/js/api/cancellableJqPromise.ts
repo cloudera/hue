259136abbaf539f168358aed4fc1b64e3c5aa5fb
Johan Ahlen <johan@johanahlen.com>
2020-08-28 16:14:30 +0200

    HUE-9456 [editor] Implement variable substitution in editor V2
    This moves all the variable logic from the snippet to a separate class and the substitution is now triggered on execute as opposed to on any change as before.

--- desktop/core/src/desktop/js/api/cancellableJqPromise.ts
+++ desktop/core/src/desktop/js/api/cancellableJqPromise.ts
@@ -14,10 +14,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { Cancellable } from 'api/cancellablePromise';
 import $ from 'jquery';
 import { cancelActiveRequest } from './apiUtils';
 
-export default class CancellableJqPromise<T> {
+export default class CancellableJqPromise<T> implements Cancellable {
   cancelCallbacks: (() => void)[] = [];
   deferred: JQuery.Deferred<T>;
   request?: JQuery.jqXHR;
