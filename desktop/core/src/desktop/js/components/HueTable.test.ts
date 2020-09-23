b704a14b9402e08dce1bdf728f2334b64d4e1104
Johan Ahlen <johan@johanahlen.com>
2020-09-23 13:37:40 +0200

    HUE-9485 [frontend] Add a Vue table component

---
+++ desktop/core/src/desktop/js/components/HueTable.test.ts
@@ -0,0 +1,41 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// 'License'); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { shallowMount } from '@vue/test-utils'
+import { Column, Row } from './HueTable';
+import HueTable from './HueTable.vue'
+
+describe('HueTable.vue', () => {
+  it('should render a table', () => {
+    const wrapper = shallowMount(HueTable, {
+      propsData: {
+        columns: <Column[]>[
+          { key: 'a', label: 'A' },
+          { key: 'd', label: 'D' },
+          { key: 'c', label: 'C' },
+          { key: 'b', label: 'B' }
+        ],
+        rows: <Row[]>[
+          { a: '1', b: 5, c: false, d: undefined },
+          { a: '2', b: 6, c: true, d: null },
+          { a: '3', b: 7, c: false },
+          { a: '4', b: 8, c: true },
+        ]
+      }
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  })
+})
