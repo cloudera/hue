13a34b7cdd629e39e01b5a3cdfd3e40e7203fb2c
Johan Ahlen <johan@johanahlen.com>
2020-09-23 13:37:40 +0200

    HUE-9485 [frontend] Add a Vue table component
    (cherry picked from commit b704a14b9402e08dce1bdf728f2334b64d4e1104)

---
+++ desktop/core/src/desktop/js/components/HueTable.vue
@@ -0,0 +1,54 @@
+<!--
+  Licensed to Cloudera, Inc. under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  Cloudera, Inc. licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+<!--
+  TODO: Decide whether we should have our own or replace with vue-good-table, vue-tables-2, bootstrap-vue etc.
+        More at https://awesome-vue.js.org/components-and-libraries/ui-components.html
+-->
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(column, colIndex) in columns" :key="colIndex">{{ column.label }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+        <td v-for="(column, colIndex) in columns" :key="colIndex">{{ row[column.key] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+  import { Column, Row } from './HueTable';
+
+  @Component
+  export default class HueTable extends Vue {
+    @Prop({ required: false, default: () => [] })
+    rows?: Row[];
+    @Prop({ required: false, default: () => [] })
+    columns?: Column[];
+  }
+</script>
+
+<style lang="scss" scoped></style>
