d1c445f587f016e1bbf8bb4745798be4b80285fe
Johan Ahlen <johan@johanahlen.com>
2020-09-30 16:41:26 +0200

    HUE-9485 [frontend] Add a Vue StatusIndicator component
    (cherry picked from commit aa2cc30c79f279082754a2043b4a6d8e0c9c2457)

---
+++ desktop/core/src/desktop/js/components/StatusIndicator.vue
@@ -0,0 +1,51 @@
+<!--
+  Licensed to Cloudera, Inc. under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  Cloudera, Inc. licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+<template>
+  <hue-icon v-if="icon" :type="icon" />
+</template>
+
+<script lang="ts">
+  import HueIcon from './HueIcon.vue';
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+
+  // TODO: Support additional statuses
+  // TODO: Support spinner
+  const ICON_MAPPING = {
+    success: 'hi-status-success',
+    error: 'hi-status-error',
+    warning: 'hi-status-warning',
+    stopped: 'hi-status-stopped'
+  };
+
+  @Component({
+    components: { HueIcon }
+  })
+  export default class StatusIndicator extends Vue {
+    @Prop({ required: true, default: 'unknown' })
+    value: string;
+
+    get icon(): string | null {
+      return ICON_MAPPING[this.value.toLowerCase()];
+    }
+  }
+</script>
+
+<style lang="scss" scoped></style>
