d259de786d94842b6988bf3c914da4b203c7081b
Romain <romain.rigaux@gmail.com>
2020-10-18 05:27:52 +0200

    [libs] Fix the pydruid lib to compile with Python 2

--- desktop/core/ext-py/pydruid-0.5.11/pydruid/db/api.py
+++ desktop/core/ext-py/pydruid-0.5.11/pydruid/db/api.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 import itertools
 import json
 from six import string_types
@@ -20,7 +20,19 @@ class Type(object):
     BOOLEAN = 3
 
 
-def connect(host='localhost', port=8082, path='/druid/v2/sql/', scheme='http'):
+def connect(
+    host="localhost",
+    port=8082,
+    path="/druid/v2/sql/",
+    scheme="http",
+    user=None,
+    password=None,
+    context=None,
+    header=False,
+    ssl_verify_cert=True,
+    ssl_client_cert=None,
+    proxies=None,
+):  # noqa: E125
     """
     Constructor for creating a connection to the database.
 
@@ -28,7 +40,21 @@ def connect(host='localhost', port=8082, path='/druid/v2/sql/', scheme='http'):
         >>> curs = conn.cursor()
 
     """
-    return Connection(host, port, path, scheme)
+    context = context or {}
+
+    return Connection(
+        host,
+        port,
+        path,
+        scheme,
+        user,
+        password,
+        context,
+        header,
+        ssl_verify_cert,
+        ssl_client_cert,
+        proxies,
+    )
 
 
 def check_closed(f):
@@ -37,8 +63,10 @@ def check_closed(f):
     def g(self, *args, **kwargs):
         if self.closed:
             raise exceptions.Error(
-                '{klass} already closed'.format(klass=self.__class__.__name__))
+                "{klass} already closed".format(klass=self.__class__.__name__)
+            )
         return f(self, *args, **kwargs)
+
     return g
 
 
@@ -47,8 +75,9 @@ def check_result(f):
 
     def g(self, *args, **kwargs):
         if self._results is None:
-            raise exceptions.Error('Called before `execute`')
+            raise exceptions.Error("Called before `execute`")
         return f(self, *args, **kwargs)
+
     return g
 
 
@@ -62,12 +91,12 @@ def get_description_from_row(row):
     """
     return [
         (
-            name,                            # name
-            get_type(value),                 # type_code
-            None,                            # [display_size]
-            None,                            # [internal_size]
-            None,                            # [precision]
-            None,                            # [scale]
+            name,  # name
+            get_type(value),  # type_code
+            None,  # [display_size]
+            None,  # [internal_size]
+            None,  # [precision]
+            None,  # [scale]
             get_type(value) == Type.STRING,  # [null_ok]
         )
         for name, value in row.items()
@@ -75,34 +104,50 @@ def get_description_from_row(row):
 
 
 def get_type(value):
-    """Infer type from value."""
+    """
+    Infer type from value.
+
+    Note that bool is a subclass of int so order of statements matter.
+    """
+
     if isinstance(value, string_types) or value is None:
         return Type.STRING
-    elif isinstance(value, (int, float)):
-        return Type.NUMBER
     elif isinstance(value, bool):
         return Type.BOOLEAN
+    elif isinstance(value, (int, float)):
+        return Type.NUMBER
 
-    raise exceptions.Error(
-        'Value of unknown type: {value}'.format(value=value))
+    raise exceptions.Error("Value of unknown type: {value}".format(value=value))
 
 
 class Connection(object):
-
     """Connection to a Druid database."""
 
     def __init__(
         self,
-        host='localhost',
+        host="localhost",
         port=8082,
-        path='/druid/v2/sql/',
-        scheme='http',
+        path="/druid/v2/sql/",
+        scheme="http",
+        user=None,
+        password=None,
+        context=None,
+        header=False,
+        ssl_verify_cert=True,
+        ssl_client_cert=None,
+        proxies=None,
     ):
-        netloc = '{host}:{port}'.format(host=host, port=port)
-        self.url = parse.urlunparse(
-            (scheme, netloc, path, None, None, None))
+        netloc = "{host}:{port}".format(host=host, port=port)
+        self.url = parse.urlunparse((scheme, netloc, path, None, None, None))
+        self.context = context or {}
         self.closed = False
         self.cursors = []
+        self.header = header
+        self.user = user
+        self.password = password
+        self.ssl_verify_cert = ssl_verify_cert
+        self.ssl_client_cert = ssl_client_cert
+        self.proxies = proxies
 
     @check_closed
     def close(self):
@@ -126,7 +171,18 @@ class Connection(object):
     @check_closed
     def cursor(self):
         """Return a new Cursor Object using the connection."""
-        cursor = Cursor(self.url)
+
+        cursor = Cursor(
+            self.url,
+            self.user,
+            self.password,
+            self.context,
+            self.header,
+            self.ssl_verify_cert,
+            self.ssl_client_cert,
+            self.proxies,
+        )
+
         self.cursors.append(cursor)
 
         return cursor
@@ -144,11 +200,27 @@ class Connection(object):
 
 
 class Cursor(object):
-
     """Connection cursor."""
 
-    def __init__(self, url):
+    def __init__(
+        self,
+        url,
+        user=None,
+        password=None,
+        context=None,
+        header=False,
+        ssl_verify_cert=True,
+        proxies=None,
+        ssl_client_cert=None,
+    ):
         self.url = url
+        self.context = context or {}
+        self.header = header
+        self.user = user
+        self.password = password
+        self.ssl_verify_cert = ssl_verify_cert
+        self.ssl_client_cert = ssl_client_cert
+        self.proxies = proxies
 
         # This read/write attribute specifies the number of rows to fetch at a
         # time with .fetchmany(). It defaults to 1 meaning to fetch a single
@@ -180,15 +252,17 @@ class Cursor(object):
 
     @check_closed
     def execute(self, operation, parameters=None):
-        query = apply_parameters(operation, parameters or {})
-
-        # `_stream_query` returns a generator that produces the rows; we need
-        # to consume the first row so that `description` is properly set, so
-        # let's consume it and insert it back.
+        query = apply_parameters(operation, parameters)
         results = self._stream_query(query)
+
+        # `_stream_query` returns a generator that produces the rows; we need to
+        # consume the first row so that `description` is properly set, so let's
+        # consume it and insert it back if it is not the header.
         try:
             first_row = next(results)
-            self._results = itertools.chain([first_row], results)
+            self._results = (
+                results if self.header else itertools.chain([first_row], results)
+            )
         except StopIteration:
             self._results = iter([])
 
@@ -197,7 +271,8 @@ class Cursor(object):
     @check_closed
     def executemany(self, operation, seq_of_parameters=None):
         raise exceptions.NotSupportedError(
-            '`executemany` is not supported, use `execute` instead')
+            "`executemany` is not supported, use `execute` instead"
+        )
 
     @check_result
     @check_closed
@@ -220,7 +295,7 @@ class Cursor(object):
         no more rows are available.
         """
         size = size or self.arraysize
-        return list(itertools.islice(self, size))
+        return list(itertools.islice(self._results, size))
 
     @check_result
     @check_closed
@@ -230,7 +305,7 @@ class Cursor(object):
         sequence of sequences (e.g. a list of tuples). Note that the cursor's
         arraysize attribute can affect the performance of this operation.
         """
-        return list(self)
+        return list(self._results)
 
     @check_closed
     def setinputsizes(self, sizes):
@@ -261,18 +336,36 @@ class Cursor(object):
         """
         self.description = None
 
-        headers = {'Content-Type': 'application/json'}
-        payload = {'query': query}
-        r = requests.post(self.url, stream=True, headers=headers, json=payload)
-        if r.encoding is None:
-            r.encoding = 'utf-8'
+        headers = {"Content-Type": "application/json"}
 
+        payload = {"query": query, "context": self.context, "header": self.header}
+
+        auth = (
+            requests.auth.HTTPBasicAuth(self.user, self.password) if self.user else None
+        )
+        r = requests.post(
+            self.url,
+            stream=True,
+            headers=headers,
+            json=payload,
+            auth=auth,
+            verify=self.ssl_verify_cert,
+            cert=self.ssl_client_cert,
+            proxies=self.proxies,
+        )
+        if r.encoding is None:
+            r.encoding = "utf-8"
         # raise any error messages
         if r.status_code != 200:
-            payload = r.json()
-            msg = (
-                '{error} ({errorClass}): {errorMessage}'.format(**payload)
-            )
+            try:
+                payload = r.json()
+            except Exception:
+                payload = {
+                    "error": "Unknown error",
+                    "errorClass": "Unknown",
+                    "errorMessage": r.text,
+                }
+            msg = "{error} ({errorClass}): {errorMessage}".format(**payload)
             raise exceptions.ProgrammingError(msg)
 
         # Druid will stream the data in chunks of 8k bytes, splitting the JSON
@@ -283,11 +376,13 @@ class Cursor(object):
         for row in rows_from_chunks(chunks):
             # update description
             if self.description is None:
-                self.description = get_description_from_row(row)
+                self.description = (
+                    list(row.items()) if self.header else get_description_from_row(row)
+                )
 
             # return row in namedtuple
             if Row is None:
-                Row = namedtuple('Row', row.keys(), rename=True)
+                Row = namedtuple("Row", row.keys(), rename=True)
             yield Row(*row.values())
 
 
@@ -299,10 +394,10 @@ def rows_from_chunks(chunks):
     JSON objects. This function will parse all complete rows inside each chunk,
     yielding them as soon as possible.
     """
-    body = ''
+    body = ""
     for chunk in chunks:
         if chunk:
-            body = ''.join((body, chunk))
+            body = "".join((body, chunk))
 
         # find last complete row
         boundary = 0
@@ -312,41 +407,50 @@ def rows_from_chunks(chunks):
             if char == '"':
                 if not in_string:
                     in_string = True
-                elif body[i - 1] != '\\':
+                elif body[i - 1] != "\\":
                     in_string = False
 
             if in_string:
                 continue
 
-            if char == '{':
+            if char == "{":
                 brackets += 1
-            elif char == '}':
+            elif char == "}":
                 brackets -= 1
                 if brackets == 0 and i > boundary:
                     boundary = i + 1
 
-        rows = body[:boundary].lstrip('[,')
+        rows = body[:boundary].lstrip("[,")
         body = body[boundary:]
 
-        for row in json.loads('[{rows}]'.format(rows=rows)):
+        for row in json.loads(
+            "[{rows}]".format(rows=rows), object_pairs_hook=OrderedDict
+        ):
             yield row
 
 
 def apply_parameters(operation, parameters):
-    escaped_parameters = {
-        key: escape(value) for key, value in parameters.items()
-    }
+    if not parameters:
+        return operation
+
+    escaped_parameters = {key: escape(value) for key, value in parameters.items()}
     return operation % escaped_parameters
 
 
 def escape(value):
-    if value == '*':
+    """
+    Escape the parameter value.
+
+    Note that bool is a subclass of int so order of statements matter.
+    """
+
+    if value == "*":
         return value
     elif isinstance(value, string_types):
         return "'{}'".format(value.replace("'", "''"))
+    elif isinstance(value, bool):
+        return "TRUE" if value else "FALSE"
     elif isinstance(value, (int, float)):
         return value
-    elif isinstance(value, bool):
-        return 'TRUE' if value else 'FALSE'
     elif isinstance(value, (list, tuple)):
-        return ', '.join(escape(element) for element in value)
+        return ", ".join(escape(element) for element in value)
--- desktop/core/ext-py/pydruid-0.5.11/pydruid/utils/dimensions.py
+++ desktop/core/ext-py/pydruid-0.5.11/pydruid/utils/dimensions.py
@@ -6,9 +6,9 @@ def build_dimension(dim):
 
 
 class DimensionSpec(object):
-
-    def __init__(self, dimension, output_name,
-                 extraction_function=None, filter_spec=None):
+    def __init__(
+        self, dimension, output_name, extraction_function=None, filter_spec=None
+    ):
         self._dimension = dimension
         self._output_name = output_name
         self._extraction_function = extraction_function
@@ -16,14 +16,14 @@ class DimensionSpec(object):
 
     def build(self):
         dimension_spec = {
-            'type': 'default',
-            'dimension': self._dimension,
-            'outputName': self._output_name
+            "type": "default",
+            "dimension": self._dimension,
+            "outputName": self._output_name,
         }
 
         if self._extraction_function is not None:
-            dimension_spec['type'] = 'extraction'
-            dimension_spec['extractionFn'] = self._extraction_function.build()
+            dimension_spec["type"] = "extraction"
+            dimension_spec["extractionFn"] = self._extraction_function.build()
 
         if self._filter_spec is not None:
             dimension_spec = self._filter_spec.build(dimension_spec)
@@ -36,16 +36,13 @@ class FilteredSpec(object):
     filter_type = None
 
     def build(self, delegate):
-        dimension_spec = {
-            'type': self.filter_type,
-            'delegate': delegate,
-        }
+        dimension_spec = {"type": self.filter_type, "delegate": delegate}
         return dimension_spec
 
 
 class ListFilteredSpec(FilteredSpec):
 
-    filter_type = 'listFiltered'
+    filter_type = "listFiltered"
 
     def __init__(self, values, is_whitelist=True):
         self._values = values
@@ -53,24 +50,24 @@ class ListFilteredSpec(FilteredSpec):
 
     def build(self, dimension_spec):
         filtered_dimension_spec = super(ListFilteredSpec, self).build(dimension_spec)
-        filtered_dimension_spec['values'] = self._values
+        filtered_dimension_spec["values"] = self._values
 
         if not self._is_whitelist:
-            filtered_dimension_spec['isWhitelist'] = False
+            filtered_dimension_spec["isWhitelist"] = False
 
         return filtered_dimension_spec
 
 
 class RegexFilteredSpec(FilteredSpec):
 
-    filter_type = 'regexFiltered'
+    filter_type = "regexFiltered"
 
     def __init__(self, pattern):
         self._pattern = pattern
 
     def build(self, dimension_spec):
         filtered_dimension_spec = super(RegexFilteredSpec, self).build(dimension_spec)
-        filtered_dimension_spec['pattern'] = self._pattern
+        filtered_dimension_spec["pattern"] = self._pattern
 
         return filtered_dimension_spec
 
@@ -80,35 +77,34 @@ class ExtractionFunction(object):
     extraction_type = None
 
     def build(self):
-        return {'type': self.extraction_type}
+        return {"type": self.extraction_type}
 
 
 class BaseRegexExtraction(ExtractionFunction):
-
     def __init__(self, expr):
         super(BaseRegexExtraction, self).__init__()
         self._expr = expr
 
     def build(self):
         extractor = super(BaseRegexExtraction, self).build()
-        extractor['expr'] = self._expr
+        extractor["expr"] = self._expr
 
         return extractor
 
 
 class RegexExtraction(BaseRegexExtraction):
 
-    extraction_type = 'regex'
+    extraction_type = "regex"
 
 
 class PartialExtraction(BaseRegexExtraction):
 
-    extraction_type = 'partial'
+    extraction_type = "partial"
 
 
 class JavascriptExtraction(ExtractionFunction):
 
-    extraction_type = 'javascript'
+    extraction_type = "javascript"
 
     def __init__(self, func, injective=False):
         super(JavascriptExtraction, self).__init__()
@@ -117,15 +113,15 @@ class JavascriptExtraction(ExtractionFunction):
 
     def build(self):
         extractor = super(JavascriptExtraction, self).build()
-        extractor['function'] = self._func
-        extractor['injective'] = self._injective
+        extractor["function"] = self._func
+        extractor["injective"] = self._injective
 
         return extractor
 
 
 class TimeFormatExtraction(ExtractionFunction):
 
-    extraction_type = 'timeFormat'
+    extraction_type = "timeFormat"
 
     def __init__(self, format, locale=None, time_zone=None):
         super(TimeFormatExtraction, self).__init__()
@@ -135,22 +131,23 @@ class TimeFormatExtraction(ExtractionFunction):
 
     def build(self):
         extractor = super(TimeFormatExtraction, self).build()
-        extractor['format'] = self._format
+        extractor["format"] = self._format
         if self._locale:
-            extractor['locale'] = self._locale
+            extractor["locale"] = self._locale
         if self._time_zone:
-            extractor['timeZone'] = self._time_zone
+            extractor["timeZone"] = self._time_zone
 
         return extractor
 
 
 class LookupExtraction(ExtractionFunction):
 
-    extraction_type = 'lookup'
+    extraction_type = "lookup"
     lookup_type = None
 
-    def __init__(self, retain_missing_values=False,
-                 replace_missing_values=None, injective=False):
+    def __init__(
+        self, retain_missing_values=False, replace_missing_values=None, injective=False
+    ):
         super(LookupExtraction, self).__init__()
         self._retain_missing_values = retain_missing_values
         self._replace_missing_values = replace_missing_values
@@ -158,20 +155,20 @@ class LookupExtraction(ExtractionFunction):
 
     def build(self):
         extractor = super(LookupExtraction, self).build()
-        extractor['lookup'] = self.build_lookup()
-        extractor['retainMissingValue'] = self._retain_missing_values
-        extractor['replaceMissingValueWith'] = self._replace_missing_values
-        extractor['injective'] = self._injective
+        extractor["lookup"] = self.build_lookup()
+        extractor["retainMissingValue"] = self._retain_missing_values
+        extractor["replaceMissingValueWith"] = self._replace_missing_values
+        extractor["injective"] = self._injective
 
         return extractor
 
     def build_lookup(self):
-        return {'type': self.lookup_type}
+        return {"type": self.lookup_type}
 
 
 class MapLookupExtraction(LookupExtraction):
 
-    lookup_type = 'map'
+    lookup_type = "map"
 
     def __init__(self, mapping, **kwargs):
         super(MapLookupExtraction, self).__init__(**kwargs)
@@ -179,14 +176,14 @@ class MapLookupExtraction(LookupExtraction):
 
     def build_lookup(self):
         lookup = super(MapLookupExtraction, self).build_lookup()
-        lookup['map'] = self._mapping
+        lookup["map"] = self._mapping
 
         return lookup
 
 
 class NamespaceLookupExtraction(LookupExtraction):
 
-    lookup_type = 'namespace'
+    lookup_type = "namespace"
 
     def __init__(self, namespace, **kwargs):
         super(NamespaceLookupExtraction, self).__init__(**kwargs)
@@ -194,6 +191,18 @@ class NamespaceLookupExtraction(LookupExtraction):
 
     def build_lookup(self):
         lookup = super(NamespaceLookupExtraction, self).build_lookup()
-        lookup['namespace'] = self._namespace
+        lookup["namespace"] = self._namespace
 
         return lookup
+
+
+class RegisteredLookupExtraction(LookupExtraction):
+
+    extraction_type = "registeredLookup"
+
+    def __init__(self, reglookup, **kwargs):
+        super(RegisteredLookupExtraction, self).__init__(**kwargs)
+        self._lookup = reglookup
+
+    def build_lookup(self):
+        return self._lookup
