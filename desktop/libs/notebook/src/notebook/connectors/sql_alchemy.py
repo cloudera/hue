4e9f40426d25d2c4b3e8bac83986d1f0c231ed53
ayush.goyal <ayush.goyal@cloudera.com>
2020-11-12 17:13:58 +0530

    HUE-9561 [SQLAlchemy] Default selected DB does not seem to be used

--- desktop/libs/notebook/src/notebook/connectors/sql_alchemy.py
+++ desktop/libs/notebook/src/notebook/connectors/sql_alchemy.py
@@ -227,6 +227,9 @@ class SqlAlchemyApi(Api):
         self.interpreter.get('dialect_properties') and self.interpreter['dialect_properties']['trim_statement_semicolon']:
       statement = statement.strip().rstrip(';')
 
+    if 'database' in snippet and snippet['database']:
+      connection.execute('USE ' + snippet['database'])
+
     result = connection.execute(statement)
 
     cache = {
