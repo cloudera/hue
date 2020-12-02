c3de5579581dd3835ce8545e98a6a35f17e6c036
Johan Ahlen <johan@johanahlen.com>
2020-12-02 15:54:52 +0100

    [frontend] Add global web component attribute for hue base URL config
    For now just for axios requests

--- desktop/core/src/desktop/js/vue/webComponentWrapper.ts
+++ desktop/core/src/desktop/js/vue/webComponentWrapper.ts
@@ -1,8 +1,21 @@
-import Vue from 'vue';
+import axios from 'axios';
+import Vue, { ComponentOptions } from 'vue';
 import vueCustomElement from 'vue-custom-element';
 
 Vue.use(vueCustomElement);
 
+export interface HueComponentOptions<T extends Vue> extends ComponentOptions<T> {
+  hueBaseUrl?: string;
+}
+
 export const wrap = <T extends Vue>(tag: string, component: { new (): T }): void => {
-  Vue.customElement(tag, new component().$options);
+  Vue.customElement(tag, new component().$options, {
+    connectedCallback() {
+      const element = <HTMLElement>this;
+      const hueBaseUrl = element.getAttribute('hue-base-url');
+      if (hueBaseUrl) {
+        axios.defaults.baseURL = hueBaseUrl;
+      }
+    }
+  });
 };
