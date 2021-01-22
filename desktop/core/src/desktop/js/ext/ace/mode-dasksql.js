6bd2c5f3f87faf578026144848c9b0a32e53a376
Akhil Naik <asnaik@cloudera.com>
2021-01-22 08:15:10 +0530

    [flink] adding TUMBLE,HOP,SESSION to keywords and addding associated builtinFunctions - review comments

--- desktop/core/src/desktop/js/ext/ace/mode-dasksql.js
+++ desktop/core/src/desktop/js/ext/ace/mode-dasksql.js
@@ -100,7 +100,7 @@ var FoldMode = exports.FoldMode = function(commentRegex) {
 oop.inherits(FoldMode, BaseFoldMode);
 
 (function() {
-
+    
     this.foldingStartMarker = /(\{|\[)[^\}\]]*$|^\s*(\/\*)/;
     this.foldingStopMarker = /^[^\[\{]*(\}|\])|^[\s\*]*(\*\/)/;
     this.singleLineBlockCommentRe= /^\s*(\/\*).*\*\/\s*$/;
@@ -109,42 +109,42 @@ oop.inherits(FoldMode, BaseFoldMode);
     this._getFoldWidgetBase = this.getFoldWidget;
     this.getFoldWidget = function(session, foldStyle, row) {
         var line = session.getLine(row);
-
+    
         if (this.singleLineBlockCommentRe.test(line)) {
             if (!this.startRegionRe.test(line) && !this.tripleStarBlockCommentRe.test(line))
                 return "";
         }
-
+    
         var fw = this._getFoldWidgetBase(session, foldStyle, row);
-
+    
         if (!fw && this.startRegionRe.test(line))
             return "start"; // lineCommentRegionStart
-
+    
         return fw;
     };
 
     this.getFoldWidgetRange = function(session, foldStyle, row, forceMultiline) {
         var line = session.getLine(row);
-
+        
         if (this.startRegionRe.test(line))
             return this.getCommentRegionBlock(session, line, row);
-
+        
         var match = line.match(this.foldingStartMarker);
         if (match) {
             var i = match.index;
 
             if (match[1])
                 return this.openingBracketBlock(session, match[1], row, i);
-
+                
             var range = session.getCommentFoldRange(row, i + match[0].length, 1);
-
+            
             if (range && !range.isMultiLine()) {
                 if (forceMultiline) {
                     range = this.getSectionRange(session, row);
                 } else if (foldStyle != "all")
                     range = null;
             }
-
+            
             return range;
         }
 
@@ -161,7 +161,7 @@ oop.inherits(FoldMode, BaseFoldMode);
             return session.getCommentFoldRange(row, i, -1);
         }
     };
-
+    
     this.getSectionRange = function(session, row) {
         var line = session.getLine(row);
         var startIndent = line.search(/\S/);
@@ -178,7 +178,7 @@ oop.inherits(FoldMode, BaseFoldMode);
             if  (startIndent > indent)
                 break;
             var subRange = this.getFoldWidgetRange(session, "all", row);
-
+            
             if (subRange) {
                 if (subRange.start.row <= startRow) {
                     break;
@@ -190,14 +190,14 @@ oop.inherits(FoldMode, BaseFoldMode);
             }
             endRow = row;
         }
-
+        
         return new Range(startRow, startColumn, endRow, session.getLine(endRow).length);
     };
     this.getCommentRegionBlock = function(session, line, row) {
         var startColumn = line.search(/\s*$/);
         var maxRow = session.getLength();
         var startRow = row;
-
+        
         var re = /^\s*(?:\/\*|\/\/|--)#?(end)?region\b/;
         var depth = 1;
         while (++row < maxRow) {
@@ -232,20 +232,20 @@ var FoldMode = exports.FoldMode = function() {};
 oop.inherits(FoldMode, BaseFoldMode);
 
 (function() {
-
+    
     this.foldingStartMarker = /(\bCASE\b|\bBEGIN\b)|^\s*(\/\*)/i;
     this.startRegionRe = /^\s*(\/\*|--)#?region\b/;
-
+    
     this.getFoldWidgetRange = function(session, foldStyle, row, forceMultiline) {
         var line = session.getLine(row);
-
+    
         if (this.startRegionRe.test(line)) return this.getCommentRegionBlock(session, line, row);
-
+    
         var match = line.match(this.foldingStartMarker);
         if (match) {
             var i = match.index;
             if (match[1]) return this.getBeginEndBlock(session, row, i, match[1]);
-
+    
             var range = session.getCommentFoldRange(row, i + match[0].length, 1);
             if (range && !range.isMultiLine()) {
                 if (forceMultiline) {
@@ -253,10 +253,10 @@ oop.inherits(FoldMode, BaseFoldMode);
                 }
                 else if (foldStyle != "all") range = null;
             }
-
+    
             return range;
         }
-
+    
         if (foldStyle === "markbegin") return;
         return;
     };
@@ -267,7 +267,7 @@ oop.inherits(FoldMode, BaseFoldMode);
         };
         var maxRow = session.getLength();
         var line;
-
+    
         var depth = 1;
         var re = /(\bCASE\b|\bBEGIN\b)|(\bEND\b)/i;
         while (++row < maxRow) {
@@ -276,7 +276,7 @@ oop.inherits(FoldMode, BaseFoldMode);
             if (!m) continue;
             if (m[1]) depth++;
             else depth--;
-
+    
             if (!depth) break;
         }
         var endRow = row;
