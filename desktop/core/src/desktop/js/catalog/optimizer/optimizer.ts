62dd96c1c20018bdfc6692adfc13a38b7f2e656b
Johan Ahlen <johan@johanahlen.com>
2021-01-27 13:23:23 +0100

    [editor] Combine the local and api strategies into a new SqlAnalyzer
    With this change it will now always call the api to retrieve top joins.

--- desktop/core/src/desktop/js/catalog/optimizer/optimizer.ts
+++ desktop/core/src/desktop/js/catalog/optimizer/optimizer.ts
@@ -14,9 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import ApiStrategy from './ApiStrategy';
-import LocalStrategy from './LocalStrategy';
-import BaseStrategy from './BaseStrategy';
+import NoopSqlAnalyzer from './NoopSqlAnalyzer';
+import SqlAnalyzer from 'catalog/optimizer/SqlAnalyzer';
 import { CancellablePromise } from 'api/cancellablePromise';
 import { OptimizerResponse } from 'catalog/dataCatalog';
 import { OptimizerMeta } from 'catalog/DataCatalogEntry';
@@ -90,13 +89,13 @@ export const API_STRATEGY = 'api';
 
 const createOptimizer = (connector: Connector): Optimizer => {
   // TODO: Remove window.OPTIMIZER_MODE and hardcoded { optimizer: 'api' } when 'connector.optimizer_mode' works.
-  if ((<hueWindow>window).OPTIMIZER_MODE === LOCAL_STRATEGY) {
-    return new LocalStrategy(connector);
+  if (
+    (<hueWindow>window).OPTIMIZER_MODE === LOCAL_STRATEGY ||
+    (<hueWindow>window).OPTIMIZER_MODE === API_STRATEGY
+  ) {
+    return new SqlAnalyzer(connector);
   }
-  if ((<hueWindow>window).OPTIMIZER_MODE === API_STRATEGY) {
-    return new ApiStrategy(connector);
-  }
-  return new BaseStrategy();
+  return new NoopSqlAnalyzer();
 };
 
 export const getOptimizer = (connector: Connector): Optimizer => {
