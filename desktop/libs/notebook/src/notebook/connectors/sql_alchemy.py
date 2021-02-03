990261090fd92674bc7633e019655a0ef760c5c5
ayush.goyal <ayush.goyal@cloudera.com>
2021-02-03 19:54:57 +0530

    added type = 'view' for views
    (cherry picked from commit e3dd9c67a8327ee7a6e93f1b6bbbb88c9d3c0967)

--- desktop/libs/notebook/src/notebook/connectors/sql_alchemy.py
+++ desktop/libs/notebook/src/notebook/connectors/sql_alchemy.py
@@ -438,9 +438,12 @@ class SqlAlchemyApi(Api):
       response['databases'] = [db or '' for db in assist.get_databases()]
     elif table is None:
       tables_meta = []
-      for t in assist.get_tables(database):
+      for t in assist.get_table_names(database):
         t = self._fix_bigquery_db_prefixes(t)
         tables_meta.append({'name': t, 'type': 'Table', 'comment': ''})
+      for t in assist.get_view_names(database):
+        t = self._fix_bigquery_db_prefixes(t)
+        tables_meta.append({'name': t, 'type': 'View', 'comment': ''})
       response['tables_meta'] = tables_meta
     elif column is None:
       columns = assist.get_columns(database, table)
