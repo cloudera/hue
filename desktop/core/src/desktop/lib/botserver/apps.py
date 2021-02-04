1f267f4c1e1daa5600f7a1833e076007a65cfc1e
Harshg999 <harshg999@gmail.com>
2021-02-04 21:48:46 +0530

    Fix pylint issues

--- desktop/core/src/desktop/lib/botserver/apps.py
+++ desktop/core/src/desktop/lib/botserver/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class BotserverConfig(AppConfig):
-    name = 'botserver'
+  name = 'botserver'
--- desktop/core/src/desktop/lib/botserver/urls.py
+++ desktop/core/src/desktop/lib/botserver/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^events/', views.home, name = 'home')
+  url(r'^events/', views.home, name='home')
 ]
