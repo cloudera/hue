467faffbc40f6868876e743b2999de11b78ba18f
Harshg999 <harshg999@gmail.com>
2021-02-05 15:08:40 +0530

    Add config flag to globally turn on/off the Slack Integration

--- desktop/core/src/desktop/lib/botserver/apps.py
+++ desktop/core/src/desktop/lib/botserver/apps.py
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class BotserverConfig(AppConfig):
-  name = 'botserver'
+# class BotserverConfig(AppConfig):
+#   name = 'botserver'
