837c352628e3a03cc9edc35489275189904fea90
sreenaths <somarajapuram@gmail.com>
2021-02-11 02:13:36 +0530

    [ui] Vue 3 - Migrated InlineAlert component

--- desktop/core/src/desktop/js/components/InlineAlert.vue
+++ desktop/core/src/desktop/js/components/InlineAlert.vue
@@ -46,9 +46,8 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
-  import { Prop } from 'vue-property-decorator';
+  import { defineComponent, PropType } from 'vue';
+
   import I18n from 'utils/i18n';
 
   export enum AlertType {
@@ -80,29 +79,38 @@
     [AlertType.Unknown, UNKNOWN_DEF]
   ]);
 
-  @Component({
+  export default defineComponent({
+    props: {
+      type: {
+        type: Object as PropType<AlertType>,
+        required: true,
+        default: AlertType.Unknown
+      },
+
+      message: String,
+      details: String,
+
+      showClose: Boolean
+    },
+
+    setup(): {
+      showDetails: Boolean,
+    } {
+      return {
+        showDetails: false
+      };
+    },
+
     methods: {
       I18n
+    },
+
+    computed: {
+      definition(): AlertDef {
+        return TYPE_DEFS.get(this.type) || UNKNOWN_DEF;
+      }
     }
   })
-  export default class InlineAlert extends Vue {
-    @Prop({ required: true, default: AlertType.Unknown })
-    type!: AlertType;
-
-    @Prop()
-    message!: string;
-    @Prop()
-    details!: string;
-
-    @Prop({ default: false })
-    showClose!: boolean;
-
-    showDetails = false;
-
-    get definition(): AlertDef {
-      return TYPE_DEFS.get(this.type) || UNKNOWN_DEF;
-    }
-  }
 </script>
 
 <style lang="scss" scoped>
