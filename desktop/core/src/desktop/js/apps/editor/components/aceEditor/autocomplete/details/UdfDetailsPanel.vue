7a9ba9aefea7e7cb9c2485eef2a3020cf9bdc57e
sreenaths <somarajapuram@gmail.com>
2021-02-11 18:43:07 +0530

    [ui] Vue 3 - Migrated AceAutocomplete components
    (cherry picked from commit 36dd6d03d0c5cfc920a67bcd4febc657c88b8c0d)

--- desktop/core/src/desktop/js/apps/editor/components/aceEditor/autocomplete/details/UdfDetailsPanel.vue
+++ desktop/core/src/desktop/js/apps/editor/components/aceEditor/autocomplete/details/UdfDetailsPanel.vue
@@ -32,24 +32,27 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
-  import { Prop } from 'vue-property-decorator';
+  import { defineComponent, PropType } from 'vue';
 
   import { UdfDetails } from 'sql/reference/types';
   import { Suggestion } from '../AutocompleteResults';
 
-  @Component
-  export default class UdfDetailsPanel extends Vue {
-    @Prop({ required: true })
-    suggestion!: Suggestion;
+  export default defineComponent({
+    props: {
+      suggestion: {
+        type: Object as PropType<Suggestion>,
+        required: true
+      }
+    },
 
-    get details(): UdfDetails {
-      return <UdfDetails>this.suggestion.details;
-    }
+    computed: {
+      details(): UdfDetails {
+        return <UdfDetails>this.suggestion.details;
+      },
 
-    get udfName(): string {
-      return this.details.signature.substring(0, this.details.signature.indexOf('('));
+      udfName(): string {
+        return this.details.signature.substring(0, this.details.signature.indexOf('('));
+      }
     }
-  }
+  });
 </script>
