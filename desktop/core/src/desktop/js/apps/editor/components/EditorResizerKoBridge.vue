991d953b9fb09a630a70d32430c7ec769232df53
sreenaths <somarajapuram@gmail.com>
2021-02-11 19:27:32 +0530

    [ui] Vue 3 - Migrated EditorResizer components

--- desktop/core/src/desktop/js/apps/editor/components/EditorResizerKoBridge.vue
+++ desktop/core/src/desktop/js/apps/editor/components/EditorResizerKoBridge.vue
@@ -21,25 +21,38 @@
 </template>
 
 <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+
   import { Ace } from 'ext/ace';
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
-  import { Prop } from 'vue-property-decorator';
+
   import { wrap } from 'vue/webComponentWrapper';
 
   import EditorResizer from 'apps/editor/components/EditorResizer.vue';
   import SubscriptionTracker from 'components/utils/SubscriptionTracker';
 
-  @Component({
-    components: { EditorResizer }
-  })
-  export default class EditorResizerKoBridge extends Vue {
-    @Prop()
-    editorObservable?: KnockoutObservable<Ace.Editor | undefined>;
+  const EditorResizerKoBridge = defineComponent({
+    components: {
+      EditorResizer
+    },
 
-    editor: Ace.Editor | null = null;
-    subTracker = new SubscriptionTracker();
-    initialized = false;
+    props: {
+      editorObservable: {
+        type: Object as PropType<KnockoutObservable<Ace.Editor | undefined>>,
+        required: true
+      }
+    },
+
+    setup(): {
+      editor: Ace.Editor | null,
+      subTracker: SubscriptionTracker,
+      initialized: boolean
+    } {
+      return {
+        editor: null,
+        subTracker: new SubscriptionTracker(),
+        initialized: false
+      };
+    },
 
     updated(): void {
       if (!this.initialized && this.editorObservable) {
@@ -49,12 +62,12 @@
         });
         this.initialized = true;
       }
-    }
+    },
 
     destroyed(): void {
       this.subTracker.dispose();
     }
-  }
+  });
 
   export const COMPONENT_NAME = 'editor-resizer-ko-bridge';
   wrap(COMPONENT_NAME, EditorResizerKoBridge);
