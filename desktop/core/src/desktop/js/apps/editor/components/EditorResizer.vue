2e24ea200431f7848ba13264b74f947840d7f675
sreenaths <somarajapuram@gmail.com>
2021-02-11 19:27:32 +0530

    [ui] Vue 3 - Migrated EditorResizer components

--- desktop/core/src/desktop/js/apps/editor/components/EditorResizer.vue
+++ desktop/core/src/desktop/js/apps/editor/components/EditorResizer.vue
@@ -17,13 +17,13 @@
 -->
 
 <template>
-  <div class="snippet-code-resizer" @mousedown="start"><i class="fa fa-ellipsis-h" /></div>
+  <div ref="root" class="snippet-code-resizer" @mousedown="start">
+    <i class="fa fa-ellipsis-h" />
+  </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
-  import { Prop } from 'vue-property-decorator';
+  import { defineComponent, PropType } from 'vue';
 
   import { HIDE_FIXED_HEADERS_EVENT, REDRAW_FIXED_HEADERS_EVENT } from 'apps/editor/events';
   import SubscriptionTracker from 'components/utils/SubscriptionTracker';
@@ -34,77 +34,103 @@
   const TARGET_ELEMENT_SELECTOR = '.ace-editor-component';
   const CONTENT_PANEL_SELECTOR = '.content-panel';
 
-  @Component
-  export default class EditorResizer extends Vue {
-    @Prop({ required: false })
-    editor?: Ace.Editor | null = null;
+  export default defineComponent({
+    props: {
+      editor: {
+        type: Object as PropType<Ace.Editor | null>,
+        required: false,
+        default: null
+      }
+    },
 
-    @Prop({ required: false })
-    panelSelector?: string;
+    setup(): {
+      subTracker: SubscriptionTracker;
+    } {
+      return {
+        subTracker: new SubscriptionTracker()
+      };
+    },
 
-    subTracker = new SubscriptionTracker();
+    data(
+      thisComp
+    ): {
+      newHeight: number;
+      startHeight: number;
+      startY: number;
 
-    newHeight = 0;
-    startHeight = 0;
-    startY = 0;
+      targetElement: HTMLElement | null;
 
-    targetElement: HTMLElement | null = null;
+      onMouseMove: (event: Event) => void;
+      onMouseUp: (event: Event) => void;
+    } {
+      return {
+        newHeight: 0,
+        startHeight: 0,
+        startY: 0,
 
-    onMouseMove = this.drag.bind(this);
-    onMouseUp = this.stop.bind(this);
+        targetElement: null,
 
-    destroyed(): void {
+        onMouseMove: thisComp.drag.bind(this),
+        onMouseUp: thisComp.stop.bind(this)
+      };
+    },
+
+    unmounted(): void {
       document.removeEventListener('mousemove', this.onMouseMove);
       document.removeEventListener('mouseup', this.onMouseUp);
       this.subTracker.dispose();
-    }
+    },
 
-    drag(event: MouseEvent): void {
-      if (!this.targetElement || !this.editor) {
-        return;
-      }
-      const diff = event.clientY - this.startY;
-
-      this.newHeight = Math.max(this.startHeight + diff, 128);
-      this.targetElement.style.height = this.newHeight + 'px';
-      this.editor.resize();
-    }
-
-    findTarget(): void {
-      const contentPanel = this.$el.closest<HTMLElement>(CONTENT_PANEL_SELECTOR);
-      if (contentPanel) {
-        this.targetElement = contentPanel.querySelector<HTMLElement>(TARGET_ELEMENT_SELECTOR);
-      }
-      if (!this.targetElement) {
-        console.warn(
-          `EditorResizer could not find the target element '${TARGET_ELEMENT_SELECTOR}'.`
-        );
-        return;
-      }
-    }
-
-    start(event: MouseEvent): void {
-      if (!this.targetElement) {
-        this.findTarget();
-        if (!this.targetElement) {
+    methods: {
+      drag(event: MouseEvent): void {
+        if (!this.targetElement || !this.editor) {
           return;
         }
-      }
-      this.startY = event.clientY;
-      this.startHeight = this.targetElement.offsetHeight;
-      document.addEventListener('mousemove', this.onMouseMove);
-      document.addEventListener('mouseup', this.onMouseUp);
-      huePubSub.publish(HIDE_FIXED_HEADERS_EVENT);
-    }
+        const diff = event.clientY - this.startY;
 
-    stop(): void {
-      huePubSub.publish(REDRAW_FIXED_HEADERS_EVENT);
-      document.dispatchEvent(new Event('editorSizeChanged')); // TODO: Who listens?
-      document.removeEventListener('mousemove', this.onMouseMove);
-      document.removeEventListener('mouseup', this.onMouseUp);
-      localStorage.setItem(CUSTOM_HEIGHT_STORAGE_KEY, String(this.newHeight));
+        this.newHeight = Math.max(this.startHeight + diff, 128);
+        this.targetElement.style.height = this.newHeight + 'px';
+        this.editor.resize();
+      },
+
+      findTarget(): void {
+        const contentPanel = (this.$refs.root as HTMLElement).closest<HTMLElement>(
+          CONTENT_PANEL_SELECTOR
+        );
+        if (contentPanel) {
+          this.targetElement = contentPanel.querySelector<HTMLElement>(TARGET_ELEMENT_SELECTOR);
+        }
+        if (!this.targetElement) {
+          console.warn(
+            `EditorResizer could not find the target element '${TARGET_ELEMENT_SELECTOR}'.`
+          );
+          return;
+        }
+      },
+
+      start(event: MouseEvent): void {
+        if (!this.targetElement) {
+          this.findTarget();
+          if (!this.targetElement) {
+            return;
+          }
+        }
+        this.startY = event.clientY;
+        this.startHeight = this.targetElement.offsetHeight;
+        document.addEventListener('mousemove', this.onMouseMove);
+        document.addEventListener('mouseup', this.onMouseUp);
+        huePubSub.publish(HIDE_FIXED_HEADERS_EVENT);
+      },
+
+      stop(): void {
+        huePubSub.publish(REDRAW_FIXED_HEADERS_EVENT);
+        document.dispatchEvent(new Event('editorSizeChanged')); // TODO: Who listens?
+        document.removeEventListener('mousemove', this.onMouseMove);
+        document.removeEventListener('mouseup', this.onMouseUp);
+        localStorage.setItem(CUSTOM_HEIGHT_STORAGE_KEY, String(this.newHeight));
+      }
     }
-  }
+  });
 </script>
 
 <style lang="scss" scoped>
