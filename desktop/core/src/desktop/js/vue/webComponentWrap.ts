735af35bab568531815267e753b5347579adbf4e
sreenaths <somarajapuram@gmail.com>
2021-02-12 16:32:00 +0530

    [ui] Vue 3 - Improved webComponentWrap to prevent re-registering of web-components

--- desktop/core/src/desktop/js/vue/webComponentWrap.ts
+++ desktop/core/src/desktop/js/vue/webComponentWrap.ts
@@ -1,15 +1,12 @@
-import { createApp, ComponentOptions, Component } from 'vue';
-// import wrapper from './wrapper/index';
-
-// Dummy till wrapper is fixed ---
-import { CreateAppFunction } from '@vue/runtime-core';
-function wrapper(createApp: CreateAppFunction<Element>, component: Component): CustomElementConstructor {
-  return null;
-}
-// Dummy till wrapper is fixed ---
+import { ComponentOptions, Component } from 'vue';
+import wrapper from './wrapper/index';
 
 export interface HueComponentOptions<T extends Component> extends ComponentOptions<T> {
   hueBaseUrl?: string;
+}
+
+const isRegistered = function(tag: string): boolean {
+  return document.createElement(tag).constructor !== HTMLElement;
 }
 
 export const wrap = <T extends Component>(
@@ -17,6 +14,8 @@ export const wrap = <T extends Component>(
   component: { new (): T },
   options?: ElementDefinitionOptions
 ): void => {
-  const customElement: CustomElementConstructor = wrapper(createApp, component);
-  window.customElements.define(tag, customElement, options);
+  if(!isRegistered(tag)) {
+    const customElement: CustomElementConstructor = wrapper(component);
+    window.customElements.define(tag, customElement, options);
+  }
 };
