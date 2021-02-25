b01f7fe8c1f8d1115982bcf952ad0a8cbc9134b8
Harshg999 <harshg999@gmail.com>
2021-02-25 12:34:35 +0530

    [slack] Initial link-unfurl event

--- desktop/core/src/desktop/lib/botserver/views.py
+++ desktop/core/src/desktop/lib/botserver/views.py
@@ -17,12 +17,15 @@
 
 import logging
 import json
+import urllib.parse
 from pprint import pprint
 
 from desktop import conf
-from django.http import HttpResponse
 from desktop.lib.django_util import login_notrequired, JsonResponse
 from desktop.lib.exceptions_renderable import PopupException
+from desktop.models import Document2
+
+from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import csrf_exempt
 
@@ -31,11 +34,10 @@ LOG = logging.getLogger(__name__)
 SLACK_VERIFICATION_TOKEN = conf.SLACK.SLACK_VERIFICATION_TOKEN.get()
 SLACK_BOT_USER_TOKEN = conf.SLACK.SLACK_BOT_USER_TOKEN.get()
 
-slack_client, appname = None, None
+slack_client = None
 if conf.SLACK.IS_ENABLED.get():
   from slack_sdk import WebClient
   slack_client = WebClient(token=SLACK_BOT_USER_TOKEN)
-  appname = "hue_bot"
 
 
 @login_notrequired
@@ -57,43 +59,100 @@ def slack_events(request):
       parse_events(event_message)
   except ValueError as e:
     raise PopupException(_("Response content is not valid JSON"), detail=e)
-  
+
   return HttpResponse(status=200)
 
 
-def parse_events(event_message):
-  user_id = event_message.get('user')
-  text = event_message.get('text')
-  channel = event_message.get('channel')
+def parse_events(event):
+  """
+  Parses the event according to its 'type'.
+  """
 
-  BOT_ID = None
-  if appname is not None:
-    BOT_ID = get_bot_id(appname)
+  # To-do: Creating function for every type to de-congest
+  if event.get('type') == 'message':
+    user_id = event.get('user')
+    text = event.get('text')
+    channel = event.get('channel')
 
-  # ignore bot's own message
-  if BOT_ID == user_id:
-    return HttpResponse(status=200)
-  
-  if slack_client is not None:
-    if 'hello hue' in text.lower():
-      response = say_hi_user(channel, user_id)
-      if response['ok']:
-        return HttpResponse(status=200)
-      else:
-        raise PopupException(response["error"])
+    # ignore bot's own message since that will cause an infinite loop of messages if we respond.
+    if event.get('bot_id'):
+      return HttpResponse(status=200)
+    
+    if slack_client:
+      if text and 'hello hue' in text.lower():
+        response = say_hi_user(channel, user_id)
+        if response['ok']:
+          return HttpResponse(status=200)
+        else:
+          raise PopupException(response["error"])
 
-  
+  if event.get('type') == 'link_shared':
+    channel_id = event.get('channel')
+    message_ts = event.get('message_ts')
+    links = event.get('links')
+
+    for item in links:
+      path = urllib.parse.urlsplit(item['url'])[2]
+      queryid_or_uuid = urllib.parse.urlsplit(item['url'])[3]  # if /hue/editor/ then query_id else if /hue/gist then uuid
+
+      if path == '/hue/editor':
+        query_id = queryid_or_uuid.split('=')[1]
+        doc2 = Document2.objects.get(id=query_id)
+        doc2_data = json.loads(doc2.data)
+
+        query_statement = doc2_data['snippets'][0]['statement_raw']
+        dialect = doc2_data['dialect']
+        database = doc2_data['snippets'][0]['database']
+
+        payload = {
+          'unfurls': {
+            item['url']: {
+              "attachments": [
+                {
+                  "blocks": [
+                    {
+                      "type": "section",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": "\n*<{}|Hue - SQL Editor>*".format((item['url']))
+                      }
+                    },
+                    {
+                      "type": "divider"
+                    },
+                    {
+                      "type": "section",
+                      "text": {
+                        "type": "mrkdwn",
+                        "text": query_statement
+                      }
+                    },
+                    {
+                      "type": "section",
+                      "fields": [
+                        {
+                          "type": "mrkdwn",
+                          "text": "*Dialect:*\n{}".format(dialect)
+                        },
+                        {
+                          "type": "mrkdwn",
+                          "text": "*Database:*\n{}".format(database)
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+
+        response = slack_client.chat_unfurl(channel=channel_id, ts=message_ts, unfurls=payload)
+
 def say_hi_user(channel, user_id):
-  """Bot sends Hi<username> message in a specific channel"""
-
+  """
+  Sends Hi<user_id> message in a specific channel.
+  """
+ 
   bot_message = 'Hi <@{}> :wave:'.format(user_id)
   return slack_client.api_call(api_method='chat.postMessage', json={'channel': channel, 'text': bot_message})
-
-def get_bot_id(botusername):
-  """Takes in bot username, Returns the bot id"""
-  
-  response = slack_client.api_call('users.list')
-  users = response['members']
-  for user in users:
-    if botusername in user.get('name', '') and not user.get('deleted'):
-      return user.get('id')
