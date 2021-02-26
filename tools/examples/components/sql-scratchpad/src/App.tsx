7d9b4b743a8d74a0c2baf83330ce08e554501c0b
Johan Ahlen <johan@johanahlen.com>
2021-02-26 14:05:52 +0100

    [frontend] Add the query editor component to the SQL Scratchpad example

--- tools/examples/components/sql-scratchpad/src/App.tsx
+++ tools/examples/components/sql-scratchpad/src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Executor from "gethue/lib/execution/executor";
+import { QueryEditor } from "./components/QueryEditor";
+import { ExecuteActions } from "./components/ExecuteActions";
+import { ExecuteProgress } from "./components/ExecuteProgress";
+import { ResultTable } from "./components/ResultTable";
+
+const executor = new Executor({
+  compute: (() => ({ id: 'default' })) as KnockoutObservable<any>,
+  connector: (() => ({
+    dialect: 'hive',
+    id: 'hive',
+    is_sql: true
+  })) as KnockoutObservable<any>,
+  database: (() => 'default') as KnockoutObservable<any>,
+  namespace: (() => ({ id: 'default' })) as KnockoutObservable<any>,
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          SQL Scratchpad Example
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="ace-editor">
+        <QueryEditor executor={ executor } />
+      </div>
+      <div className="executable-progress-bar">
+        <ExecuteProgress />
+      </div>
+      <div className="executable-actions">
+        <ExecuteActions />
+      </div>
+      <div className="result-table">
+        <ResultTable />
+      </div>
     </div>
   );
 }
