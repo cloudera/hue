187b98bb81aee3e008346027627b618800f6f7f3
Johan Ahlen <johan@johanahlen.com>
2021-03-15 14:00:49 +0100

    [editor] Add a QueryHistoryTable Vue component

--- desktop/core/src/desktop/js/apps/editor/api.ts
+++ desktop/core/src/desktop/js/apps/editor/api.ts
@@ -14,14 +14,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import axios from 'axios';
-
-type FormatSqlApiResponse = {
-  formatted_statements?: string;
-  status: number;
-};
+import { ExecutionStatus } from './execution/executable';
+import { CancellablePromise } from 'api/cancellablePromise';
+import { get, post } from 'api/utils';
 
 const FORMAT_SQL_API_URL = '/notebook/api/format';
+const HISTORY_API_URL = '/notebook/api/get_history';
+
+interface FormatSqlApiResponse {
+  formatted_statements?: string;
+  status: number;
+}
 
 export const formatSql = async (options: {
   statements: string;
@@ -30,10 +33,10 @@ export const formatSql = async (options: {
   try {
     const params = new URLSearchParams();
     params.append('statements', options.statements);
-    const response = await axios.post<FormatSqlApiResponse>(FORMAT_SQL_API_URL, params);
+    const response = await post<FormatSqlApiResponse>(FORMAT_SQL_API_URL, params);
 
-    if (response.data.status !== -1 && response.data.formatted_statements) {
-      return response.data.formatted_statements;
+    if (response.status !== -1 && response.formatted_statements) {
+      return response.formatted_statements;
     }
   } catch (err) {
     if (!options.silenceErrors) {
@@ -41,4 +44,43 @@ export const formatSql = async (options: {
     }
   }
   return options.statements;
+};
+
+export interface FetchHistoryOptions {
+  type: string;
+  page?: number;
+  limit?: number;
+  docFilter?: string;
+  isNotificationManager?: boolean;
+}
+
+export interface FetchHistoryResponse {
+  count: number;
+  history: {
+    absoluteUrl: string;
+    data: {
+      lastExecuted: number;
+      parentSavedQueryUuid: string;
+      statement: string;
+      status: ExecutionStatus;
+    };
+    id: number;
+    name: string;
+    type: string;
+    uuid: string;
+  }[];
+  message: string;
+  status: number;
+}
+
+export const fetchHistory = (
+  options: FetchHistoryOptions
+): CancellablePromise<FetchHistoryResponse> => {
+  return get<FetchHistoryResponse>(HISTORY_API_URL, {
+    doc_type: options.type,
+    limit: options.limit || 50,
+    page: options.page || 1,
+    doc_text: options.docFilter,
+    is_notification_manager: options.isNotificationManager
+  });
 };
---
+++ desktop/core/src/desktop/js/apps/editor/components/QueryHistoryTable.vue
@@ -0,0 +1,283 @@
+<!--
+  Licensed to Cloudera, Inc. under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  Cloudera, Inc. licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
+<template>
+  <div class="query-history-table">
+    <ClearQueryHistoryModal
+      v-model="clearHistoryModalOpen"
+      :connector="connector"
+      @history-cleared="refresh"
+    />
+    <ImportDocumentsModal
+      v-model="importHistoryModalOpen"
+      :connector="connector"
+      :header="I18n('Import Query History')"
+      @documents-imported="refresh"
+    />
+    <div class="query-history-top-bar">
+      <div class="query-history-filter">
+        <SearchInput v-model="searchFilter" :show-magnify="false" :small="true" />
+      </div>
+      <div class="query-history-actions">
+        <HueButton :small="true" :disabled="!history.length" @click="onClearClick">
+          <i class="fa fa-fw fa-calendar-times-o" /> {{ I18n('Clear') }}
+        </HueButton>
+        <HueButton :small="true" :disabled="!history.length" @click="onExportClick">
+          <i
+            class="fa fa-fw"
+            :class="{ 'fa-download': !exportingHistory, 'fa-spinner fa-spin': exportingHistory }"
+          />
+          {{ I18n('Export') }}
+        </HueButton>
+        <HueButton :small="true" @click="onImportClick">
+          <i class="fa fa-fw fa-upload" /> {{ I18n('import') }}
+        </HueButton>
+      </div>
+    </div>
+    <div class="query-history-table-container">
+      <Spinner
+        :spin="loadingHistory"
+        :center="true"
+        :size="'large'"
+        :overlay="true"
+        :label="I18n('Loading...')"
+      />
+      <div class="query-history-table-scrollable">
+        <HueTable
+          :clickable-rows="true"
+          :columns="columns"
+          :rows="history"
+          :show-header="false"
+          @row-clicked="$emit('history-entry-clicked', $event)"
+        >
+          <template #cell-lastExecuted="historyEntry">
+            <TimeAgo class="query-history-last-executed" :value="historyEntry.lastExecuted" />
+          </template>
+          <template #cell-status="historyEntry">
+            <ExecutionStatusIcon class="query-history-status" :status="historyEntry.status" />
+          </template>
+          <template #cell-query="historyEntry">
+            <SqlText :value="historyEntry.query" :dialect="connector?.dialect" />
+          </template>
+        </HueTable>
+        <div v-if="!loadingHistory && !totalCount" class="no-history-entries">
+          <span v-if="!searchFilter">{{ I18n('Query History is empty') }}</span>
+          <span v-else>{{ I18n('No queries found for: ') }}{{ searchFilter }}</span>
+        </div>
+        <Paginator
+          v-show="!loadingHistory && totalCount"
+          :total-entries="totalCount"
+          @page-changed="onPageChange"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { debounce } from 'lodash';
+  import { defineComponent, PropType, ref, toRefs, watch } from 'vue';
+
+  import ClearQueryHistoryModal from './ClearQueryHistoryModal.vue';
+  import ExecutionStatusIcon from './ExecutionStatusIcon.vue';
+  import './QueryHistoryTable.scss';
+  import { fetchHistory, FetchHistoryResponse } from '../api';
+  import { ExecutionStatus } from '../execution/executable';
+  import { CancellablePromise } from 'api/cancellablePromise';
+  import { Connector } from 'config/types';
+  import HueButton from 'components/HueButton.vue';
+  import { Column } from 'components/HueTable';
+  import HueTable from 'components/HueTable.vue';
+  import ImportDocumentsModal from 'components/ImportDocumentsModal.vue';
+  import Paginator from 'components/Paginator.vue';
+  import { Page } from 'components/Paginator';
+  import SearchInput from 'components/SearchInput.vue';
+  import Spinner from 'components/Spinner.vue';
+  import SqlText from 'components/SqlText.vue';
+  import TimeAgo from 'components/TimeAgo.vue';
+  import SubscriptionTracker from 'components/utils/SubscriptionTracker';
+  import { hueWindow } from 'types/types';
+  import huePubSub from 'utils/huePubSub';
+  import I18n from 'utils/i18n';
+
+  interface HistoryEntry {
+    absoluteUrl?: string;
+    query: string;
+    lastExecuted: number;
+    status: ExecutionStatus;
+    name: string;
+    uuid: string;
+  }
+
+  const ADD_TO_HISTORY_EVENT = 'query.history.add';
+  const IGNORE_NEXT_UNLOAD_EVENT = 'ignore.next.unload';
+
+  const trimEllipsis = (str: string): string =>
+    `${str.substring(0, 1000)}${str.length > 1000 ? '...' : ''}`;
+
+  export default defineComponent({
+    name: 'QueryHistoryTable',
+    components: {
+      ExecutionStatusIcon,
+      ClearQueryHistoryModal,
+      HueButton,
+      HueTable,
+      ImportDocumentsModal,
+      Paginator,
+      SearchInput,
+      Spinner,
+      SqlText,
+      TimeAgo
+    },
+    props: {
+      connector: {
+        type: Object as PropType<Connector | undefined>,
+        default: undefined
+      }
+    },
+    emits: ['history-entry-clicked'],
+    setup(props) {
+      const { connector } = toRefs(props);
+      const subTracker = new SubscriptionTracker();
+      const clearHistoryModalOpen = ref(false);
+      const exportingHistory = ref(false);
+      const history = ref<HistoryEntry[]>([]);
+      const importHistoryModalOpen = ref(false);
+      const loadingHistory = ref(true);
+      const searchFilter = ref('');
+      const totalCount = ref(0);
+
+      const columns: Column<HistoryEntry>[] = [
+        { key: 'lastExecuted' },
+        { key: 'status' },
+        { key: 'name' },
+        { key: 'query' }
+      ];
+
+      let runningPromise: CancellablePromise<FetchHistoryResponse> | undefined = undefined;
+
+      const updateHistory = async (page?: Page) => {
+        if (runningPromise) {
+          runningPromise.cancel();
+        }
+        if (connector.value && connector.value.dialect) {
+          loadingHistory.value = true;
+          runningPromise = fetchHistory({
+            type: connector.value.dialect,
+            page: page?.pageNumber,
+            limit: page?.limit,
+            docFilter: searchFilter.value
+          });
+
+          try {
+            const response = await runningPromise;
+            totalCount.value = response.count;
+            history.value = response.history.map(({ name, data, uuid, absoluteUrl }) => ({
+              name,
+              query: trimEllipsis(data.statement),
+              lastExecuted: data.lastExecuted,
+              status: data.status,
+              uuid,
+              absoluteUrl
+            }));
+          } catch (err) {}
+          runningPromise = undefined;
+          loadingHistory.value = false;
+        }
+      };
+
+      const onClearClick = (): void => {
+        clearHistoryModalOpen.value = true;
+      };
+
+      const onExportClick = async (): Promise<void> => {
+        const dialect = connector.value?.dialect;
+        if (!dialect || exportingHistory.value) {
+          return;
+        }
+        try {
+          exportingHistory.value = true;
+          const historyResponse = await fetchHistory({ type: dialect, page: 1, limit: 500 });
+
+          huePubSub.publish(IGNORE_NEXT_UNLOAD_EVENT);
+
+          if (historyResponse && historyResponse.history) {
+            window.location.href = `${
+              (window as hueWindow).HUE_BASE_URL
+            }/desktop/api2/doc/export?history=true&documents=[${historyResponse.history
+              .map(historyDoc => historyDoc.id)
+              .join(',')}]`;
+          }
+        } catch (err) {}
+        exportingHistory.value = false;
+      };
+
+      const onImportClick = (): void => {
+        importHistoryModalOpen.value = true;
+      };
+
+      const debouncedUpdate = debounce(updateHistory, 300);
+      watch(connector, () => debouncedUpdate(), { immediate: true });
+      watch(searchFilter, () => debouncedUpdate());
+
+      subTracker.subscribe(
+        ADD_TO_HISTORY_EVENT,
+        (details: {
+          absoluteUrl?: string;
+          lastExecuted: number;
+          name: string;
+          statement: string;
+          status: ExecutionStatus;
+          uuid: string;
+        }) => {
+          if (!history.value.some(entry => entry.uuid === details.uuid)) {
+            history.value = [
+              {
+                absoluteUrl: details.absoluteUrl,
+                lastExecuted: details.lastExecuted,
+                name: details.name,
+                query: details.statement,
+                status: details.status,
+                uuid: details.uuid
+              },
+              ...history.value
+            ];
+            totalCount.value++;
+          }
+        }
+      );
+
+      return {
+        I18n,
+        clearHistoryModalOpen,
+        columns,
+        exportingHistory,
+        history,
+        importHistoryModalOpen,
+        loadingHistory,
+        onClearClick,
+        onExportClick,
+        onImportClick,
+        onPageChange: updateHistory,
+        refresh: debouncedUpdate,
+        searchFilter,
+        totalCount
+      };
+    }
+  });
+</script>
