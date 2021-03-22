99b3b1c1943f541e85847f7d2738764ce7b09ce9
ayush.goyal <ayush.goyal@cloudera.com>
2021-03-22 17:44:43 +0530

    [Django4_warning_force_str] RemovedInDjango40Warning: force_text() is deprecated in favor of force_str().

--- desktop/core/src/desktop/lib/i18n.py
+++ desktop/core/src/desktop/lib/i18n.py
@@ -75,8 +75,10 @@ def force_unicode(s, strings_only=False, errors='strict'):
   Wrapper around Django's version, while supplying our configured encoding.
   Decode char array to unicode.
   """
-  return django.utils.encoding.force_text(
-        s, get_site_encoding(), strings_only, errors)
+  if sys.version_info[0] > 2:
+    return django.utils.encoding.force_str(s, get_site_encoding(), strings_only, errors)
+  else:
+    return django.utils.encoding.force_unicode(s, get_site_encoding(), strings_only, errors)
 
 def smart_str(s, strings_only=False, errors='strict'):
   """
