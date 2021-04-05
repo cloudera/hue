5d8d2576bf86210fcf907422a2ba3ba32bee762c
Akhil Naik <asnaik@cloudere.com>
2021-04-05 10:48:43 +0530

    adding UT Cases to redaction test with unicode changes(format)

--- desktop/core/src/desktop/redaction/tests.py
+++ desktop/core/src/desktop/redaction/tests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: ascii -*-
 # Licensed to Cloudera, Inc. under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
