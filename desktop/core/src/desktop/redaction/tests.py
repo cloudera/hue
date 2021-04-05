3adffb7dda893f9e22cf7b9b1f4e4597302b2775
Akhil Naik <asnaik@cloudere.com>
2021-04-05 10:03:48 +0530

    adding UT Cases to redaction test with unicode changes(format)

--- desktop/core/src/desktop/redaction/tests.py
+++ desktop/core/src/desktop/redaction/tests.py
@@ -439,28 +439,27 @@ class TestRedactionLogFilter(object):
     assert_equal(errors, [])
 
 def byte_range(first, last):
-    return list(range(first, last+1))
+  return list(range(first, last+1))
 
 first_values = byte_range(0x00, 0x7F) + byte_range(0xC2, 0xF4)
 trailing_values = byte_range(0x80, 0xBF)
 
 def random_utf8_char():
-    first = random.choice(first_values)
-    if first <= 0x7F:
-        value = bytearray([first])
-    elif first <= 0xDF:
-        value = bytearray([first, random.choice(trailing_values)])
-    elif first == 0xE0:
-        value = bytearray([first, random.choice(byte_range(0xA0, 0xBF)), random.choice(trailing_values)])
-    elif first == 0xED:
-        value = bytearray([first, random.choice(byte_range(0x80, 0x9F)), random.choice(trailing_values)])
-    elif first <= 0xEF:
-        value = bytearray([first, random.choice(trailing_values), random.choice(trailing_values)])
-    elif first == 0xF0:
-        value = bytearray([first, random.choice(byte_range(0x90, 0xBF)), random.choice(trailing_values), random.choice(trailing_values)])
-    elif first <= 0xF3:
-        value = bytearray([first, random.choice(trailing_values), random.choice(trailing_values), random.choice(trailing_values)])
-    elif first == 0xF4:
-        value = bytearray([first, random.choice(byte_range(0x80, 0x8F)), random.choice(trailing_values), random.choice(trailing_values)])
-
-    return value.decode('utf8')
+  first = random.choice(first_values)
+  if first <= 0x7F:
+    value = bytearray([first])
+  elif first <= 0xDF:
+    value = bytearray([first, random.choice(trailing_values)])
+  elif first == 0xE0:
+    value = bytearray([first, random.choice(byte_range(0xA0, 0xBF)), random.choice(trailing_values)])
+  elif first == 0xED:
+    value = bytearray([first, random.choice(byte_range(0x80, 0x9F)), random.choice(trailing_values)])
+  elif first <= 0xEF:
+    value = bytearray([first, random.choice(trailing_values), random.choice(trailing_values)])
+  elif first == 0xF0:
+    value = bytearray([first, random.choice(byte_range(0x90, 0xBF)), random.choice(trailing_values), random.choice(trailing_values)])
+  elif first <= 0xF3:
+    value = bytearray([first, random.choice(trailing_values), random.choice(trailing_values), random.choice(trailing_values)])
+  elif first == 0xF4:
+    value = bytearray([first, random.choice(byte_range(0x80, 0x8F)), random.choice(trailing_values), random.choice(trailing_values)])
+  return value.decode('utf8')
