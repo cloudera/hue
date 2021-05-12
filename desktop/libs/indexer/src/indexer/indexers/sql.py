22948da0946e5961bc4d531408bed3ad1c9d646d
ayush.goyal <ayush.goyal@cloudera.com>
2021-05-12 23:37:43 +0530

    adding unit test for impala

--- desktop/libs/indexer/src/indexer/indexers/sql.py
+++ desktop/libs/indexer/src/indexer/indexers/sql.py
@@ -283,7 +283,7 @@ class SQLIndexer(object):
         is_task=True
     )
 
-  def bool_col_update(self, row, columns):
+  def nomalize_booleans(self, row, columns):
     for cnt, col in enumerate(columns):
       if col['type'] == 'boolean':
         if row[cnt] in ('T', 't', 'true', 'True', 'TRUE', '1'):
@@ -352,23 +352,21 @@ CONSTRAINT my_pk PRIMARY KEY (%(primary_keys)s));
     path = urllib_unquote(source['path'])
 
     if path:                                                  # data insertion
-
       with open(BASE_DIR + path, 'r') as local_file:
         reader = csv.reader(local_file)
-        csv_rows = []
+        _csv_rows = []
 
         for count, row in enumerate(reader):
           if source['format']['hasHeader'] and count == 0:
             continue
-          if editor_type == 'impala':                         # for the boolean col updating csv_val to (1,0)
-            row = self.bool_col_update(row, columns)
-          csv_rows.append(tuple(row))
+          if dialect == 'impala':                         # for the boolean col updating csv_val to (1,0)
+            row = self.nomalize_booleans(row, columns)
+          _csv_rows.append(tuple(row))
 
-        if csv_rows:
-          csv_rows = str(csv_rows)
-          csv_rows = csv_rows[1:-1]
+        if _csv_rows:
+          csv_rows = str(_csv_rows)[1:-1]
 
-          if editor_type in ('hive', 'mysql'):
+          if dialect in ('hive', 'mysql'):
             sql += '''\nINSERT INTO %(database)s.%(table_name)s VALUES %(csv_rows)s;
             '''% {
                     'database': database,
@@ -381,36 +379,24 @@ CONSTRAINT my_pk PRIMARY KEY (%(primary_keys)s));
                 else '{0}'.format(col_val) for count, col_val in enumerate(csv_row)])
 
               sql += '''\nUPSERT INTO %(database)s.%(table_name)s VALUES (%(csv_row)s);
-              ''' % {
-                      'database': database,
-                      'table_name': table_name,
-                      'csv_row': _sql
-                    }
-          elif editor_type == 'impala':
-            sql += '''\nINSERT INTO %(database)s.%(table_name)s_tmp VALUES %(csv_rows)s;\n\nCREATE TABLE IF NOT EXISTS %(database)s.%(table_name)s
-AS SELECT'''% {
+            ''' % {
                     'database': database,
                     'table_name': table_name,
-                    'csv_rows': csv_rows
+                    'csv_row': _sql
                   }
+          elif dialect == 'impala':
+             # casting from string to boolean is not allowed in impala so string -> int -> bool
+            sql_ = ',\n'.join([
+              '  CAST ( `%(name)s` AS %(type)s ) `%(name)s`' % col if col['type'] != 'boolean' \
+              else '  CAST ( CAST ( `%(name)s` AS TINYINT ) AS boolean ) `%(name)s`' % col for col in columns
+            ])
 
-            for count, col in enumerate(columns):
-              if col['type'] == 'boolean':                # casting from string to boolean is not allowed in impala so string -> int -> bool
-                sql += '''\n  CAST ( CAST ( `%(col_name)s` AS TINYINT ) AS boolean ) `%(col_name)s`'''%{
-                'col_name': col['name']
-              }
-              else:
-                sql += '''\n  CAST ( `%(col_name)s` AS %(col_type)s ) `%(col_name)s`'''%{
-                  'col_name': col['name'],
-                  'col_type': col['type']
-                }
-              if count != len(columns)-1:
-                sql += ','
-          
-            sql += '''\nFROM  %(database)s.%(table_name)s_tmp;\n\nDROP TABLE IF EXISTS %(database)s.%(table_name)s_tmp;
-            '''% {
+            sql += '''\nINSERT INTO %(database)s.%(table_name)s_tmp VALUES %(csv_rows)s;\n\nCREATE TABLE IF NOT EXISTS %(database)s.%(table_name)s
+AS SELECT\n%(sql_)s\nFROM  %(database)s.%(table_name)s_tmp;\n\nDROP TABLE IF EXISTS %(database)s.%(table_name)s_tmp;'''% {
                     'database': database,
                     'table_name': table_name,
+                    'csv_rows': csv_rows,
+                    'sql_': sql_
                   }
 
     on_success_url = reverse('metastore:describe_table', kwargs={'database': database, 'table': final_table_name}) + \
