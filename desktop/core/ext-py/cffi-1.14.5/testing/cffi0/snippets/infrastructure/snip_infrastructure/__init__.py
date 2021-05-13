2d198850cbcbf61deb294070a90f8233da26249a
Romain Rigaux <romain.rigaux@gmail.com>
2021-05-13 10:38:27 -0700

    [libs] Delete ext-py/cffi-1.14.5

--- desktop/core/ext-py/cffi-1.14.5/testing/cffi0/snippets/infrastructure/snip_infrastructure/__init__.py
+++
@@ -1,3 +0,0 @@
-
-def func():
-    return 42
--- desktop/core/ext-py/cffi-1.14.5/testing/cffi0/snippets/setuptools_module/setup.py
+++
@@ -1,8 +0,0 @@
-
-from setuptools import setup
-import snip_setuptools_verify
-
-setup(
-    zip_safe=False,
-    py_modules=['snip_setuptools_verify'],
-    ext_modules=[snip_setuptools_verify.ffi.verifier.get_extension()])
