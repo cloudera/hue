3a73c65d4b62fbabf69d9aa7a936942ac89db315
Jason Killian <1400151+jkillian@users.noreply.github.com>
2021-05-14 21:25:15 -0400

    Update more fields

--- desktop/core/src/desktop/js/parse/types.ts
+++ desktop/core/src/desktop/js/parse/types.ts
@@ -125,12 +125,12 @@ export interface AutocompleteParseResult {
     types?: string[];
     udfRef?: string;
   };
-  suggestCommonTableExpressions: {
+  suggestCommonTableExpressions?: {
     name: string;
     prependFrom: boolean;
     prependQuestionMark: boolean;
   }[];
-  suggestDatabases: {
+  suggestDatabases?: {
     appendDot?: boolean;
     prependFrom?: boolean;
     prependQuestionMark?: boolean;
@@ -151,7 +151,7 @@ export interface AutocompleteParseResult {
     prependJoin?: boolean;
     tables: ParsedTable[];
   };
-  suggestJoinConditions: {
+  suggestJoinConditions?: {
     prependOn?: boolean;
     tables: ParsedTable[];
   };
@@ -177,7 +177,7 @@ export interface AutocompleteParseResult {
     missingEndQuote?: boolean;
     partialQuote?: boolean;
   };
-  udfArgument: {
+  udfArgument?: {
     name: string;
     position: number;
   };
