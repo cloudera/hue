53bf6506c8676c4d02136048dcf91db2a28150a2
Romain Rigaux <romain.rigaux@gmail.com>
2021-05-23 07:28:29 -0700

    [libs] Adding djangorestframework-simplejwt 3.3
    (cherry picked from commit 70a77e491521bddc4c15dde799d73987ce81de89)

---
+++ desktop/core/ext-py/djangorestframework_simplejwt-3.3/rest_framework_simplejwt/state.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+
+from .backends import TokenBackend
+from .settings import api_settings
+
+User = get_user_model()
+token_backend = TokenBackend(api_settings.ALGORITHM, api_settings.SIGNING_KEY,
+                             api_settings.VERIFYING_KEY)
