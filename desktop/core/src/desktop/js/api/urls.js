ab9551da1e484393ae05d3955ef20e0a308a9be8
Romain Rigaux <romain.rigaux@gmail.com>
2021-05-25 06:21:33 -0700

    [api] Have get_config into both private and public URLs

--- desktop/core/src/desktop/js/api/urls.js
+++ desktop/core/src/desktop/js/api/urls.js
@@ -20,7 +20,7 @@ export const EXECUTE_API_PREFIX = '/notebook/api/execute/';
 export const DOCUMENTS_API = '/desktop/api2/doc/';
 export const DOCUMENTS_SEARCH_API = '/desktop/api2/docs/';
 export const GET_HUE_CONFIG_API = '/desktop/api2/get_hue_config';
-// export const FETCH_CONFIG_API = '/desktop/api2/get_config/';
+export const FETCH_CONFIG_PRIVATE_API = '/desktop/api2/get_config/';
 export const FETCH_CONFIG_API = '/api/iam/get_config/';
 export const HDFS_API_PREFIX = '/filebrowser/view=' + encodeURIComponent('/');
 export const ADLS_API_PREFIX = '/filebrowser/view=' + encodeURIComponent('adl:/');
