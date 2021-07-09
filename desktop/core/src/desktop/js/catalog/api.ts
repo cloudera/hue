b5349387ceabea007b120665201130fe71790bc3
Romain Rigaux <romain.rigaux@gmail.com>
2021-07-09 16:07:37 +0200

    [sqls] Avoid JSON.bigdataParse is not a function on result display
    TypeError: JSON.bigdataParse is not a function

--- desktop/core/src/desktop/js/catalog/api.ts
+++ desktop/core/src/desktop/js/catalog/api.ts
@@ -35,6 +35,7 @@ import DataCatalogEntry, {
 } from 'catalog/DataCatalogEntry';
 import { Cluster, Compute, Connector, Namespace } from 'config/types';
 import { hueWindow } from 'types/types';
+import 'utils/json.bigDataParse';
 import sleep from 'utils/timing/sleep';
 import UUID from 'utils/string/UUID';
 
