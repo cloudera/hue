c5e8f0b7c40883a8afa39b314e6c5e24887a0afb
Romain Rigaux <romain.rigaux@gmail.com>
2021-07-26 11:12:50 -0700

    [catalog] Pick-up according dialect when opening Table from popover
    (cherry picked from commit 9fda4bab1306e85a8a746fe9164a32ea8aa3ce8f)

--- apps/metastore/src/metastore/views.py
+++ apps/metastore/src/metastore/views.py
@@ -316,7 +316,8 @@ def get_table_metadata(request, database, table):
 def describe_table(request, database, table):
   app_name = get_app_name(request)
   cluster = json.loads(request.POST.get('cluster', '{}'))
-  source_type = request.POST.get('source_type', request.GET.get('source_type', 'hive'))
+  source_type = request.POST.get('source_type', request.GET.get('connector_id', request.GET.get('source_type', 'hive')))
+
   db = _get_db(user=request.user, source_type=source_type, cluster=cluster)
 
   try:
