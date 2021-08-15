4486c0b2b5e476f949eba6dda20dbd04ddc2e9e1
michael-brine <mbrine0@gmail.com>
2021-08-15 13:51:27 -0700

    hardcoded input

--- desktop/libs/indexer/src/indexer/api3_tests.py
+++ desktop/libs/indexer/src/indexer/api3_tests.py
@@ -1,19 +1,51 @@
 import tempfile
 import uuid
+from desktop.lib.paths import get_desktop_root
 from django.http import HttpRequest, QueryDict
 from indexer.api3 import guess_field_types
 from nose.tools import assert_equal, assert_true
 
 
 def test_local_file_header_reformatting_with_header():
-    with open('apps/beeswax/data/tables/testbook1.csv') as upload_file:
-        username = 'test_user'
-        filename = "%s_%s:%s;" % (username, uuid.uuid4(), upload_file.name)
-        temp_file = tempfile.NamedTemporaryFile(suffix='.csv', delete=False)
-        temp_file.write(bytes(upload_file.read(), encoding='utf-8'))
-        local_file_url = temp_file.name
-        temp_file.close()
-
+    username = 'test_user'
+    filename = "%s_%s:testbook1.csv;" % (username, uuid.uuid4())
+    temp_file = tempfile.NamedTemporaryFile(suffix='.csv', delete=False)
+    temp_file.write(bytes('''test 1,test.2,test_3,test_4
+10/10/10,1:00:00 AM,$30.00,
+"Sunday, October 10, 2021",3:00, 1/8,
+2010-10-10,9:00 AM,99.00%,
+10/10,4:00:00,500,
+10/10/10,9:00:00 AM,2.98E+04,
+10/10/10,00:00.0,50      ,
+10-Oct,988639:00:00,,
+10-Oct-10,10/11/12 3:00 AM,,
+10-Oct-10,10/11/12 9:00,,
+Oct-10,10/10/22 2:00 PM,,
+October-10,10/10/21 15:00,,
+"October 10, 2010",,,
+O,,,
+O-10,scattered,,
+10/10/2010,,scattered,
+10-Oct-2010,,,
+,,,
+,,,
+scattered,,,
+,,scattered,
+,,,
+,,,
+,scattered,,
+,,,
+,,,
+,,,
+scattered,,,scattered
+,,,
+,,,
+,,,
+,,,
+,scattered,,
+''', encoding='utf-8'))
+    local_file_url = temp_file.name
+    temp_file.close()
     data = {'fileFormat':'{"inputFormat":"localfile","path": "%s","format":{"hasHeader":true}}' % local_file_url}
     request = HttpRequest()
     q = QueryDict(mutable=True)
