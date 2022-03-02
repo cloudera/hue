59a410c2942a9040b714015086b60c45dd8c0b8d
Prakash Ranade <ranade@cloudera.com>
2022-03-02 15:24:16 -0800

    CDPD-35379 Hue [py3] Enable HTTP_X_FORWARDED header detection (#2732)
    (cherry picked from commit ddb87f3c2abae02a1d28d14e0c933b4ff4fa97cb)
    (cherry picked from commit aad29b8ec6271c2cb9b4be1ec8c20599b3e53463)
    Change-Id: If9c692a5a47b19fa36791ae9386781fe8403c647
    (cherry picked from commit 37dc1c9f25f2235a40ba9af046bf2b76c2e00901)

--- desktop/core/src/desktop/middleware.py
+++ desktop/core/src/desktop/middleware.py
@@ -923,3 +923,25 @@ class MimeTypeJSFileFixStreamingMiddleware(MiddlewareMixin):
       response['Content-Type'] = "application/javascript"
 
     return response
+
+class MultipleProxyMiddleware:
+  FORWARDED_FOR_FIELDS = [
+    'HTTP_X_FORWARDED_FOR',
+    'HTTP_X_FORWARDED_HOST',
+    'HTTP_X_FORWARDED_SERVER',
+  ]
+
+  def __init__(self, get_response):
+    self.get_response = get_response
+
+  def __call__(self, request):
+    """
+    Rewrites the proxy headers so that only the most
+    recent proxy is used.
+    """
+    for field in self.FORWARDED_FOR_FIELDS:
+      if field in request.META:
+        if ',' in request.META[field]:
+          parts = request.META[field].split(',')
+          request.META[field] = parts[-1].strip()
+    return self.get_response(request)
