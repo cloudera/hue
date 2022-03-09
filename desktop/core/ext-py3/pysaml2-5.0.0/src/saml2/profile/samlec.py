bcd0192ff4f761724ed15550699b3e04d087c1f9
Prakash Ranade <ranade@cloudera.com>
2022-03-08 16:51:11 -0800

    Hue [py3] Adding pysaml2-5.0.0 python library

---
+++ desktop/core/ext-py3/pysaml2-5.0.0/src/saml2/profile/samlec.py
@@ -0,0 +1,14 @@
+from saml2 import SamlBase
+
+
+NAMESPACE = 'urn:ietf:params:xml:ns:samlec'
+
+
+class GeneratedKey(SamlBase):
+    c_tag = 'GeneratedKey'
+    c_namespace = NAMESPACE
+
+
+ELEMENT_BY_TAG = {
+    'GeneratedKey': GeneratedKey,
+}
