58f86cc5b62846dfde805fb61a7a9e873b2df7f9
Prakash Ranade <ranade@cloudera.com>
2022-03-08 16:45:22 -0800

    Hue [py3] Remove chardet dependency in requests

--- desktop/core/ext-py3/requests-2.27.1/setup.py
+++ desktop/core/ext-py3/requests-2.27.1/setup.py
@@ -42,7 +42,6 @@ packages = ['requests']
 
 requires = [
     'charset_normalizer~=2.0.0; python_version >= "3"',
-    'chardet>=3.0.2,<5; python_version < "3"',
     'idna>=2.5,<3; python_version < "3"',
     'idna>=2.5,<4; python_version >= "3"',
     'urllib3>=1.21.1,<1.27',
@@ -108,8 +107,7 @@ setup(
     extras_require={
         'security': [],
         'socks': ['PySocks>=1.5.6, !=1.5.7'],
-        'socks:sys_platform == "win32" and python_version == "2.7"': ['win_inet_pton'],
-        'use_chardet_on_py3': ['chardet>=3.0.2,<5']
+        'socks:sys_platform == "win32" and python_version == "2.7"': ['win_inet_pton']
     },
     project_urls={
         'Documentation': 'https://requests.readthedocs.io',
--- desktop/core/ext-py3/requests-2.27.1/tests/test_packages.py
+++ desktop/core/ext-py3/requests-2.27.1/tests/test_packages.py
@@ -8,6 +8,3 @@ def test_can_access_urllib3_attribute():
 def test_can_access_idna_attribute():
     requests.packages.idna
 
-
-def test_can_access_chardet_attribute():
-    requests.packages.chardet
