982021a49e0ad9708a317e264e381dd305758cfc
Bjorn Alm <balm@cloudera.com>
2022-09-14 14:18:48 +0200

    CDPD-44544: [frontend] - unmount react root on DOM node disposal (#2982)
    * [frontend] - unmount react root on DOM node disposal
    
    (cherry picked from commit 8dac801691c3de737759180a68224630ae5a699d)
    (cherry picked from commit 1899ec28d9b4182fcf34b51969a4f3c8f65bfeed)
    Change-Id: Id344cbd6a18573064d702d1000a6370dc6d5122e

--- desktop/core/src/desktop/js/ko/bindings/ko.reactWrapper.js
+++ desktop/core/src/desktop/js/ko/bindings/ko.reactWrapper.js
@@ -36,12 +36,17 @@ ko.bindingHandlers.reactWrapper = (() => {
       const props = getProps(allBindings);
 
       // The component's react root should only be created once per DOM
-      // load so we pass it along via the bindingContext to be reused in the
+      // load so we pass it along via the bindingContext to be reused in the KO update call.
       const reactRoot = createRoot(el);
       el.__KO_React_root = reactRoot;
       loadComponent(componentName).then(Component => {
         reactRoot.render(createElement(Component, props));
       });
+
+      // Since the react component is a root component we need to handle the
+      // unmounting explicitly if the dom node is disposed by Knockout, e.g. via "ko if:"
+      ko.utils.domNodeDisposal.addDisposeCallback(el, () => reactRoot.unmount());
+
       // Tell Knockout that it does not need to update the children
       // of this component, since that is now handled by React
       return { controlsDescendantBindings: true };
