ecdfabe67c1e413e3066d3651bf988f5a2cffe82
Björn Alm <balm@cloudera.com>
2022-09-23 11:13:49 +0200

    [frontend] add modern i18n support

--- desktop/core/src/desktop/js/apps/editor/components/result/reactExample/ReactExample.tsx
+++ desktop/core/src/desktop/js/apps/editor/components/result/reactExample/ReactExample.tsx
@@ -1,7 +1,10 @@
 'use strict';
 
 import React, { FunctionComponent, useState } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Skeleton } from 'antd';
+
+// Provides a i18n translation hook
+import { i18nReact } from '../../../../../utils/i18nReact';
 
 // tsx-files don't have the same baseUrl as other js files so
 // we are using a relative path when importing
@@ -15,7 +18,7 @@ import SqlExecutable from '../../../execution/sqlExecutable';
 import './ReactExample.scss';
 
 export interface ReactExampleProps {
-  title: string;
+  title?: string;
   // This example component recieves the "activeExecutable" used in the result page but
   // the props in general can of course be of any type
   activeExecutable?: SqlExecutable;
@@ -27,18 +30,27 @@ interface EditorCursor {
   position: Ace.Position;
 }
 
-const defaultProps = { title: 'Default result title' };
-
 // Using the FunctionComponent generic is optional. Alternatively you can explicitly
 // define the children prop like in the ReactExampleGlobal component.
-const ReactExample: FunctionComponent<ReactExampleProps> = ({ title, activeExecutable }) => {
+const ReactExample: FunctionComponent<ReactExampleProps> = ({ activeExecutable, ...i18n }) => {
+  // We use the translation hook WITHOUT the built in suspence, meaning that our
+  // component should handle the rendering of the loading state. This gives us the
+  // possibilty to render a loader or placeholder while waiting.
+  const { t, ready: i18nReady } = i18nReact.useTranslation(undefined, { useSuspense: false });
+
+  // This is one way we can do i18n on default values for strings passed as props.
+  // Use the prop value if present, if not we use a default value/key passed through i18n
+  const { title = t('Schedule') } = i18n;
+
   // Example of having the react component rerender based on changes from useHuePubSub.
   // Use with caution and preferrably only at the top level component in your component tree.
   const editorCursor = useHuePubSub<EditorCursor>({ topic: CURSOR_POSITION_CHANGED_EVENT });
 
   const id = activeExecutable?.id;
   const position =
-    editorCursor?.position !== undefined ? JSON.stringify(editorCursor.position) : 'not available';
+    editorCursor?.position !== undefined
+      ? JSON.stringify(editorCursor.position)
+      : t('Not available');
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -54,41 +66,45 @@ const ReactExample: FunctionComponent<ReactExampleProps> = ({ title, activeExecu
     setIsModalOpen(false);
   };
 
-  return (
+  return !i18nReady ? (
+    <div className="antd">
+      <Skeleton />
+    </div>
+  ) : (
     // The 'antd' class is added to the root element since we want it to apply the correct
     // "global" styling to its antd sub components, e.g. the antd Button.
     // Also make sure that the component specific Antd style is imported in the file
     // 'root-wrapped-antd.less'.
-    <div className="react-example antd">
-      <h1 className="react-example__title">{title}</h1>
-      <Button type="primary" onClick={showModal}>
-        Open Modal
-      </Button>
+    <React.StrictMode>
+      <div className="react-example antd">
+        <h1 className="react-example__title">{title}</h1>
+        <Button type="primary" onClick={showModal}>
+          {t('Open')}
+        </Button>
 
-      <p className="react-example__description">
-        I'm an Editor specific react component containing subcomponents. The dynamic id that I'm
-        getting from a Knockout observable is {id}.
-      </p>
-      <p className="react-example__description">
-        {`I'm also geting a cursor position from hue huePubSub using the hook useHuePubSub which is
+        <p className="react-example__description">
+          I'm an Editor specific react component containing subcomponents. The dynamic id that I'm
+          getting from a Knockout observable is {id}.
+        </p>
+        <p className="react-example__description">
+          {`I'm also geting a cursor position from hue huePubSub using the hook useHuePubSub which is
         updated on each 'editor.cursor.position.changed'. Cursor position is
         ${position}`}
-      </p>
-      <ReactExampleGlobal className="react-example__react-example-global-component">
-        I'm a button from the application global component set
-      </ReactExampleGlobal>
-      <Button type="primary" onClick={() => console.info('clicked')}>
-        I'm an Antd button
-      </Button>
-      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </div>
+        </p>
+        <ReactExampleGlobal className="react-example__react-example-global-component">
+          I'm a button from the application global component set
+        </ReactExampleGlobal>
+        <Button type="primary" onClick={() => console.info('clicked')}>
+          I'm an Antd button
+        </Button>
+
+        {/* The title and content of the Modal is internationlized using the t-function */}
+        <Modal title={t('Modify')} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <p>{t('Settings')}.</p>
+        </Modal>
+      </div>
+    </React.StrictMode>
   );
 };
-
-ReactExample.defaultProps = defaultProps;
 
 export default ReactExample;
