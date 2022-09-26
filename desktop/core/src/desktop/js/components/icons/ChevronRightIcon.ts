d84ea225674c9f3c4cd726fc894b19bb3e4e1834
Johan Åhlén <johan@johanahlen.com>
2022-09-26 14:00:41 +0200

    [frontend] Convert some icons to React

--- desktop/core/src/desktop/js/components/icons/ChevronRightIcon.ts
+++
@@ -1,6 +0,0 @@
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'ChevronRightIcon',
-  template: '<svg class="hi hi-fw"><use xlink:href="#hueChevronRightSymbol" /></svg>'
-});
