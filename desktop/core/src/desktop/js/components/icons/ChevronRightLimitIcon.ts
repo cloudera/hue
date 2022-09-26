77605d2ff338af5653c8c7894c04ae3df04c7d81
Johan Åhlén <johan@johanahlen.com>
2022-09-26 14:00:41 +0200

    CDPD-45112: [frontend] Convert some icons to React (#3011)
    (cherry picked from commit 2d3c8aefa15b16630b19b6436b9065fbcaf52c92)
    (cherry picked from commit ffe8a8feec78ba831cd56c031dc576671b84c4b0)
    Change-Id: I935d7e814907c8c54d434a60ef045936c4d2df5e
    (cherry picked from commit 020ca84a71ccbdf5e6e32d31c433e73728a0a2f9)

--- desktop/core/src/desktop/js/components/icons/ChevronRightLimitIcon.ts
+++
@@ -1,6 +0,0 @@
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'ChevronRightLimitIcon',
-  template: '<svg class="hi hi-fw"><use xlink:href="#hueChevronRightLimitSymbol" /></svg>'
-});
