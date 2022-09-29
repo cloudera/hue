7716ce03d89df05034b7ef3934b2a6d2fed35cb4
Johan Åhlén <johan@johanahlen.com>
2022-09-29 16:00:03 +0200

    [frontend] Regenerate all the parsers
    In some cases the generated parsers were out of sync with their jison definitions, likely caused by not generating after jison version bump.
    
    For Hive, Impala and the generic parsers the only change is the introduction of jsdoc on the parse function.
    
    (cherry picked from commit dbc763174386a6f28ca1ff1f9bda055eca4957ef)

--- desktop/core/src/desktop/js/parse/sql/dasksql/dasksqlSyntaxParser.js
+++ desktop/core/src/desktop/js/parse/sql/dasksql/dasksqlSyntaxParser.js
@@ -2527,6 +2527,11 @@ parseError: function parseError (str, hash) {
         throw error;
     }
 },
+/**
+ * @param {string} input
+ *
+ * @return {AutocompleteParseResult}
+ */
 parse: function parse (input) {
     var self = this,
         stack = [0],
