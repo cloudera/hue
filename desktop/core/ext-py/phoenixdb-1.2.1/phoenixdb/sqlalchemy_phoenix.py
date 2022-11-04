89a3d65d926ebe3ad752ff25d7549c00694e47c6
Ayush Goyal <36241930+agl29@users.noreply.github.com>
2022-11-04 20:53:20 +0530

    [Phoenix] Upgrading phoenixdb into it's latest version (#3053)

--- desktop/core/ext-py/phoenixdb-1.2.1/phoenixdb/sqlalchemy_phoenix.py
+++ desktop/core/ext-py/phoenixdb-1.2.1/phoenixdb/sqlalchemy_phoenix.py
@@ -90,6 +90,8 @@ class PhoenixDialect(DefaultDialect):
 
     driver = "phoenixdb"
 
+    supports_statement_cache = False  # We only implement textual SQL anyway
+
     ddl_compiler = PhoenixDDLCompiler
 
     execution_ctx_cls = PhoenixExecutionContext
