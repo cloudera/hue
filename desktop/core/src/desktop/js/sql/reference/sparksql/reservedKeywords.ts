ef177b1e1f8ce14adb8304cb9806e9190ea78908
Johan Åhlén <johan@johanahlen.com>
2022-11-08 15:54:17 +0100

    [editor] Extend the reference repository with Spark SQL details for UDFs and keywords
    (cherry picked from commit 85ee523abba5e3ef31de35ecc67ef03666dcd723)

---
+++ desktop/core/src/desktop/js/sql/reference/sparksql/reservedKeywords.ts
@@ -0,0 +1,76 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export const RESERVED_WORDS: Set<string> = new Set([
+  'ALL',
+  'AND',
+  'AS',
+  'ASC',
+  'BIGINT',
+  'BY',
+  'CASE',
+  'COLUMN',
+  'COLUMNS',
+  'CROSS',
+  'DATABASE',
+  'DESC',
+  'DIRECTORY',
+  'DISTINCT',
+  'EXTENDED',
+  'FUNCTION',
+  'ELSE',
+  'END',
+  'EXISTS',
+  'FALSE',
+  'FROM',
+  'FULL',
+  'GROUP',
+  'HAVING',
+  'IF',
+  'IN',
+  'INTO',
+  'JOIN',
+  'LEFT',
+  'LIKE',
+  'LIMIT',
+  'LOCAL',
+  'NOT',
+  'NULL',
+  'ON',
+  'OPTION',
+  'OR',
+  'ORDER',
+  'OUTER',
+  'PARTITION',
+  'REGEXP',
+  'RIGHT',
+  'RLIKE',
+  'ROLE',
+  'SELECT',
+  'SEMI',
+  'TABLE',
+  'THEN',
+  'TRUE',
+  'TRUNCATE',
+  'UNION',
+  'WHEN',
+  'WHERE',
+  'WITH',
+  'MAP',
+  'OVERWRITE',
+  'QUERY',
+  'USER'
+]);
