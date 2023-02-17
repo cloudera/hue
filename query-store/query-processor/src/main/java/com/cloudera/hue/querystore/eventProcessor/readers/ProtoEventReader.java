8cddf18aba326338c8d3999b71ba8b51162290cd
ssomarajapuram <ssomarajapuram@cloudera.com>
2023-02-17 00:23:17 -0800

    CDPD-49715: Make file reading generic
    Change-Id: I2915078e5a83a1f3d25149955dca9bd5db1d23ca
    (cherry picked from commit 0bca23dc1df9ac00fc1f3a7ba304678ac90aa4d5)
    (cherry picked from commit 9cb31e19a657d02ece12501d44b65bddfe18d031)

---
+++ query-store/query-processor/src/main/java/com/cloudera/hue/querystore/eventProcessor/readers/ProtoEventReader.java
@@ -0,0 +1,78 @@
+package com.cloudera.hue.querystore.eventProcessor.readers;
+
+import java.io.EOFException;
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.tez.dag.history.logging.proto.DatePartitionedLogger;
+import org.apache.tez.dag.history.logging.proto.ProtoMessageReader;
+
+import com.cloudera.hue.querystore.eventProcessor.pipeline.FileProcessingStatus;
+import com.google.protobuf.MessageLite;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ProtoEventReader<T extends MessageLite> implements EventReader<T> {
+  private final ProtoMessageReader<T> protoMessageReader;
+  private final FileProcessingStatus fileStatus;
+  private final DatePartitionedLogger<T> partitionedLogger;
+
+  public ProtoEventReader(ProtoMessageReader<T> protoMessageReader, FileProcessingStatus fileStatus,
+      DatePartitionedLogger<T> partitionedLogger) {
+    this.protoMessageReader = protoMessageReader;
+    this.fileStatus = fileStatus;
+    this.partitionedLogger = partitionedLogger;
+  }
+
+  public long getOffset() throws IOException {
+    return protoMessageReader.getOffset();
+  }
+
+  public void setOffset(long offset) throws IOException {
+    protoMessageReader.setOffset(offset);
+  }
+
+  public Path getFilePath() {
+    return protoMessageReader.getFilePath();
+  }
+
+  public T read() {
+    T evt = null;
+    try {
+      evt  = protoMessageReader.readEvent();
+      if (evt == null) {
+        long fsPos = fileStatus.getPosition();
+        long readerOffset = protoMessageReader.getOffset();
+        if (readerOffset == fsPos + 20 + 4) {
+          // Handle multiple consecutive sync markers.
+          log.warn("Got multi sync marker for file: {}, at location: {}",
+          protoMessageReader.getFilePath(), fsPos);
+          protoMessageReader.setOffset(fsPos + 20);
+          return read();
+        } else if (fsPos < readerOffset) {
+          // Prevent getting stuck at this offset.
+          log.warn("Incrementing retry count for file: {}, at filePos: {}, offset: {}",
+            protoMessageReader.getFilePath(), fsPos, readerOffset);
+          fileStatus.readFailed();
+        }
+      } else {
+        // successful read, reset read retry count.
+        fileStatus.readSuccess();
+      }
+    } catch (EOFException e) {
+      // We are getting EOF for an old file, prevent getting stuck here.
+      if (fileStatus.getEntity().getDate().isBefore(partitionedLogger.getNow().toLocalDate())) {
+        fileStatus.readFailed();
+      }
+    } catch (Exception e) {
+      fileStatus.readFailed();
+    }
+    return evt;
+  }
+
+  @Override
+  public void close() throws IOException {
+    protoMessageReader.close();
+  }
+}
