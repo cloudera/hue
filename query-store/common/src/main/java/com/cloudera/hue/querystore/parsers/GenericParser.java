ab60a27631daccfc051dc611790209cd60970acd
Amlesh Kumar <amlesh.kumar@cloudera.com>
2023-02-28 23:24:50 +0530

    CDPD-49260: - HueQP - remove Parsers and it's dependent files
    Change-Id: Ifd7d761cdc8eeaa2192e761ecb0d2b880e645a69

--- query-store/common/src/main/java/com/cloudera/hue/querystore/parsers/GenericParser.java
+++
@@ -1,6 +0,0 @@
-// (c) Copyright 2020-2021 Cloudera, Inc. All rights reserved.
-package com.cloudera.hue.querystore.parsers;
-
-public interface GenericParser<T, S> {
-  T parse(S parseInput);
-}
--- query-store/common/src/main/java/com/cloudera/hue/querystore/parsers/SearchQueryParser.java
+++
@@ -1,7 +0,0 @@
-// (c) Copyright 2020-2021 Cloudera, Inc. All rights reserved.
-package com.cloudera.hue.querystore.parsers;
-
-/**
- * Interface to be implemented by the search parsers
- */
-public interface SearchQueryParser extends GenericParser<QueryParseResult, String> {}
