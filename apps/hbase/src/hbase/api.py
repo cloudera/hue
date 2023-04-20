ddc7bca7095b21a69f6201bbd82cee285016c841
Zoltán Iuhos <zoltan.iuhos@ukatemi.com>
2023-04-20 17:33:04 +0200

    Fix partial row scan

--- apps/hbase/src/hbase/api.py
+++ apps/hbase/src/hbase/api.py
@@ -29,7 +29,8 @@ from desktop.lib import thrift_util
 from desktop.lib.exceptions_renderable import PopupException
 
 from hbase import conf
-from hbase.hbase_site import get_server_principal, get_server_authentication, is_using_thrift_ssl, is_using_thrift_http, get_thrift_transport
+from hbase.hbase_site import get_server_principal, get_server_authentication, is_using_thrift_ssl, \
+  is_using_thrift_http, get_thrift_transport
 from hbase.server.hbase_lib import get_thrift_type, get_client_type
 
 if sys.version_info[0] > 2:
@@ -140,12 +141,14 @@ class HbaseApi(object):
 
   def createTable(self, cluster, tableName, columns):
     client = self.connectCluster(cluster)
-    client.createTable(tableName, [get_thrift_type('ColumnDescriptor')(**column['properties']) for column in columns], doas=self.user.username)
+    client.createTable(tableName, [get_thrift_type('ColumnDescriptor')(**column['properties'])
+                                   for column in columns], doas=self.user.username)
     return "%s successfully created" % tableName
 
   def getTableList(self, cluster):
     client = self.connectCluster(cluster)
-    return [{'name': name, 'enabled': client.isTableEnabled(name, doas=self.user.username)} for name in client.getTableNames(doas=self.user.username)]
+    return [{'name': name, 'enabled': client.isTableEnabled(name, doas=self.user.username)}
+            for name in client.getTableNames(doas=self.user.username)]
 
   def getRows(self, cluster, tableName, columns, startRowKey, numRows, prefix=False):
     client = self.connectCluster(cluster)
@@ -161,7 +164,9 @@ class HbaseApi(object):
     query = smart_str(query)
     try:
       client = self.connectCluster(cluster)
-      scan = get_thrift_type('TScan')(startRow=query, stopRow=None, timestamp=None, columns=[], caching=None, filterString="PrefixFilter('" + query + "') AND ColumnPaginationFilter(1,0)", batchSize=None)
+      scan = get_thrift_type('TScan')(startRow=query, stopRow=None, timestamp=None, columns=[],
+                                      caching=None, filterString="PrefixFilter('" + query + "') AND ColumnPaginationFilter(1,0)",
+                                      batchSize=None)
       scanner = client.scannerOpenWithScan(tableName, scan, None, doas=self.user.username)
       return [result.row for result in client.scannerGetList(scanner, numRows, doas=self.user.username)]
     except Exception as e:
@@ -176,7 +181,9 @@ class HbaseApi(object):
 
   def getRowsFull(self, cluster, tableName, startRowKey, numRows):
     client = self.connectCluster(cluster)
-    return self.getRows(cluster, tableName, [smart_str(column) for column in client.getColumnDescriptors(tableName, doas=self.user.username)], smart_str(startRowKey), numRows)
+    return self.getRows(cluster, tableName,
+                        [smart_str(column) for column in client.getColumnDescriptors(tableName, doas=self.user.username)],
+                        smart_str(startRowKey), numRows)
 
   def getRowFull(self, cluster, tableName, startRowKey, numRows):
     row = self.getRowsFull(cluster, tableName, smart_str(startRowKey), 1)
@@ -186,7 +193,9 @@ class HbaseApi(object):
 
   def getRowPartial(self, cluster, tableName, rowKey, offset, number):
     client = self.connectCluster(cluster)
-    scan = get_thrift_type('TScan')(startRow=rowKey, stopRow=None, timestamp=None, columns=[], caching=None, filterString="ColumnPaginationFilter(%i, %i)" % (number, offset), batchSize=None)
+    scan = get_thrift_type('TScan')(startRow=rowKey, stopRow=None, timestamp=None, columns=[],
+                                    caching=None, filterString="PrefixFilter('%s') AND ColumnPaginationFilter(%i, %i)"
+                                                               % (rowKey, number, offset), batchSize=None)
     scanner = client.scannerOpenWithScan(tableName, scan, None, doas=self.user.username)
     return client.scannerGetList(scanner, 1, doas=self.user.username)
 
@@ -218,7 +227,9 @@ class HbaseApi(object):
   def putUpload(self, cluster, tableName, row, column, value):
     client = self.connectCluster(cluster)
     Mutation = get_thrift_type('Mutation')
-    return client.mutateRow(tableName, smart_str(row), [Mutation(column=smart_str(column), value=value.file.read(value.size))], None, doas=self.user.username)
+    return client.mutateRow(tableName, smart_str(row),
+                            [Mutation(column=smart_str(column), value=value.file.read(value.size))],
+                            None, doas=self.user.username)
 
   def getRowQuerySet(self, cluster, tableName, columns, queries):
     client = self.connectCluster(cluster)
@@ -236,7 +247,9 @@ class HbaseApi(object):
         fs = " AND (" + fs.strip() + ")"
       filterstring = "(ColumnPaginationFilter(%i,0) AND PageFilter(%i))" % (limit, limit) + (fs or "")
       scan_columns = [smart_str(column.strip(':')) for column in query['columns']] or [smart_str(column.strip(':')) for column in columns]
-      scan = get_thrift_type('TScan')(startRow=smart_str(query['row_key']), stopRow=None, timestamp=None, columns=scan_columns, caching=None, filterString=filterstring, batchSize=None)
+      scan = get_thrift_type('TScan')(startRow=smart_str(query['row_key']),
+                                      stopRow=None, timestamp=None, columns=scan_columns,
+                                      caching=None, filterString=filterstring, batchSize=None)
       scanner = client.scannerOpenWithScan(tableName, scan, None, doas=self.user.username)
       aggregate_data += client.scannerGetList(scanner, query['scan_length'], doas=self.user.username)
     return aggregate_data
