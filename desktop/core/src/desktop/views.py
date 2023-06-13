fbc51cd9aca7e44ed18996558eceb70d10cc9c12
Athithyaa Selvam <33496652+athithyaaselvam@users.noreply.github.com>
2023-06-12 19:01:04 -0700

    CDPD-47849: Add support for XFF header in Knox and SAML logins (#3325)
    Co-authored-by: Athithyaa Selvam <aselvam@cloudera.com>
    (cherry picked from commit 74e93fedd066d1e77cacb9ac50ccc1dec5e9d34f)
    (cherry picked from commit a3185d571075f15691092ddb24d32f2e4f529995)

--- desktop/core/src/desktop/views.py
+++ desktop/core/src/desktop/views.py
@@ -114,6 +114,14 @@ def samlgroup_check(request):
       LOG.info("User %s found in the required SAML groups %s" % (request.user.username, ",".join(saml_group_found)))
   return True
 
+def saml_login_headers(request):
+  userprofile = get_profile(request.user)
+  try:
+    userprofile.update_data({'X-Forwarded-For': request.META['HTTP_X_FORWARDED_FOR']})
+    userprofile.save()
+  except KeyError as e:
+    LOG.error('X-FORWARDED-FOR header not found: %s' % smart_str(e))
+
 def hue(request):
   current_app, other_apps, apps_list = _get_apps(request.user, '')
   clusters = list(get_clusters(request.user).values())
