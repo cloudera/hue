c3e36f223a1213f766c1a6ca5a4eaf143b9c42b8
sreenaths <somarajapuram@gmail.com>
2023-06-14 17:57:39 -0700

    [llm] Adding LRU cache for vector search

---
+++ desktop/core/src/desktop/lib/utils/cache.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: str):
+        if key not in self.cache:
+            return None
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: str, value) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
