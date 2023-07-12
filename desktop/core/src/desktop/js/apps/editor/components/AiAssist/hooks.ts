320f8131cd36e3cd7e45d25776c078ab804079e8
Björn Alm <balm@cloudera.com>
2023-07-12 14:24:54 +0200

    CDPD-58365: [frontend] add custom history dropdown using local storage to AiAssistToolbar
    (cherry picked from commit 72fb233d8b1d6b577ba94d0fd5c6ffd9d3d17b4c)
    (cherry picked from commit e1a0e9bade880eb9fd1afc2842850a612fcc5627)
    Change-Id: I8cf7b84e686e478fbe7c617e8c25ff6d48058aef

--- desktop/core/src/desktop/js/apps/editor/components/AiAssist/hooks.ts
+++ desktop/core/src/desktop/js/apps/editor/components/AiAssist/hooks.ts
@@ -1,5 +1,7 @@
 import { useMemo, useState, useEffect, useRef } from 'react';
-import { throttle } from 'lodash';
+import { max, throttle } from 'lodash';
+
+import { getFromLocalStorage, setInLocalStorage } from 'utils/storageUtils';
 
 export const useKeywordCase = (parser: any, selectedStatement: string) => {
   const sqlKeywords = useMemo(() => {
@@ -84,4 +86,41 @@ export const useKeyboardShortcuts = (shortcuts: Record<string, () => void>) => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
+};
+
+export interface HistoryItem {
+  date: number;
+  value: string;
+}
+
+export const useLocalStorageHistory = (
+  key: string,
+  maxHistoryLength: number = 10
+): [HistoryItem[], (newItem: HistoryItem) => void] => {
+  const [history, setHistory] = useState<HistoryItem[]>(() => {
+    const savedHistory = getFromLocalStorage(key) as HistoryItem[];
+    return savedHistory?.length > 0 ? savedHistory : [];
+  });
+
+  useEffect(() => {
+    setInLocalStorage(key, history);
+  }, [history, key]);
+
+  const addNewItem = (newItem: HistoryItem) => {
+    setHistory(prevHistory => {
+      const oldItemIndex = prevHistory.findIndex(
+        existingItem => existingItem.value === newItem.value
+      );
+      if (oldItemIndex !== -1) {
+        prevHistory.splice(oldItemIndex, 1);
+      }
+      const newHistory = [newItem, ...prevHistory];
+      if (newHistory.length > maxHistoryLength) {
+        newHistory.pop();
+      }
+      return newHistory;
+    });
+  };
+
+  return [history, addNewItem];
 };
