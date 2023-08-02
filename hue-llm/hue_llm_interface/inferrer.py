dd215f4f2f61c6a74029906049e1048ded5df891
sreenaths <somarajapuram@gmail.com>
2023-08-02 01:07:44 -0700

    CDPD-58386: [llm]  Removing old implimentation of hue-llm server
    (cherry picked from commit 0b425a624daf87c1673747377ea1f77b770190d4)
    (cherry picked from commit 0376f939685497b7827884e4a54c5c9805f93e1b)
    Change-Id: I26fe3c712c213a26d7add5c95ab3a273ea69e288

--- hue-llm/hue_llm_interface/inferrer.py
+++
@@ -1,7 +0,0 @@
-from importlib import import_module
-
-def inferrer_factory(model, model_path):
-    mod = import_module(f'hue_llm_interface.llms.{model}', package=model)
-    build_inferer = getattr(mod, 'build')
-
-    return build_inferer(model_path)
