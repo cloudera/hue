8a93db38cb269c293c5821949015c87d444c5227
Nidhi Bhat G <nidhibhatg17@gmail.com>
2023-08-02 19:03:58 +0530

    [frontend]testchanges

---
+++ desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserPage/StorageBrowserTabContents/StorageBrowserTabContent.tsx
@@ -0,0 +1,72 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { useState, useEffect } from 'react';
+
+import BucketIcon from '@cloudera/cuix-core/icons/react/BucketIcon';
+import { fetchFiles } from '../../../../reactComponents/FileChooser/api';
+import { PathAndFileData } from '../../../../reactComponents/FileChooser/types';
+
+import './StorageBrowserTabContent.scss';
+
+interface StorageBrowserTabContentProps {
+  user_home_dir: string;
+  testId?: string;
+}
+
+const defaultProps = {
+  testId: 'hue-storage-browser-tabContent'
+};
+
+const StorageBrowserTabContent: React.FC<StorageBrowserTabContentProps> = ({
+  user_home_dir,
+  testId
+}): JSX.Element => {
+  const [filePath, setFilePath] = useState<string>(user_home_dir);
+  const [filesData, setFilesData] = useState<PathAndFileData | undefined>();
+  const [loadingFiles, setloadingFiles] = useState(true);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(filePath);
+    setloadingFiles(true);
+    fetchFiles(filePath)
+      .then(responseFilesData => {
+        setFilesData(responseFilesData);
+        // eslint-disable-next-line no-restricted-syntax
+        console.log(responseFilesData);
+      })
+      .catch(error => {
+        //TODO: handle errors
+      })
+      .finally(() => {
+        setloadingFiles(false);
+      });
+  }, [filePath]);
+
+  return (
+    <div className="hue-storage-browser-tabContent" data-testId={testId}>
+      <div>
+        <BucketIcon className="hue-storage-browser-icon" />
+        {user_home_dir}
+      </div>
+    </div>
+  );
+};
+
+StorageBrowserTabContent.defaultProps = defaultProps;
+
+export default StorageBrowserTabContent;
