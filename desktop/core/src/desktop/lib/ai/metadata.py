c2f2c022225dfff99d469ea1a72699b355fedec2
sreenaths <somarajapuram@gmail.com>
2023-09-08 18:27:18 -0700

    CDPD-58393: [llm] Improve semantic search
    (cherry picked from commit f8b381b120d38236950fa7a80536c39ab04e280c)
    (cherry picked from commit 3a0c62f7b5ceb83eb4ffb3a93ff6ce819f48ea12)
    Change-Id: I1faeec37378dbb3e8655808af622e4788e1e56a6

--- desktop/core/src/desktop/lib/ai/metadata.py
+++ desktop/core/src/desktop/lib/ai/metadata.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer, util
 import torch
+import re
 
 from ..utils.cache import LRUCache
 from desktop.conf import SEMANTIC_SEARCH
@@ -21,7 +22,8 @@ def _get_cached_embeddings(embedder, corpus):
     if new_sentences:
       sentence_embeddings = embedder.encode(new_sentences, convert_to_tensor=True)
       for idx, embedding in enumerate(sentence_embeddings):
-          embedding_dict[new_sentences[idx]] = embedding
+          sentence = new_sentences[idx]
+          embedding_dict[sentence] = embedding
           corpus_cache.put(sentence, embedding)
 
     embeddings = []
@@ -30,17 +32,38 @@ def _get_cached_embeddings(embedder, corpus):
 
     return torch.stack(embeddings)
 
+def _get_words(text) -> list:
+    words = re.findall(r'\b\w+\b', text)
+    # Remove 1 or 2 letter words
+    words = list(filter(lambda w : len(w) >= 3, words))
+    return list(set(words))
+
 # Perform semantic search on the corpus using the provided query
-def semantic_search(corpus, query):
+def semantic_search(corpus, query, limit = 10):
+    corpus = list(set(corpus))
+    k = min(len(corpus), limit)
     embedder = SentenceTransformer(_embedding_model)
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
+
     corpus_embeddings = _get_cached_embeddings(embedder, corpus)
 
-    cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
-    top_results = torch.topk(cos_scores, k=min(len(corpus), 10))
+    # Calculating embeddings for queries and each words, as embeddings can get lost in
+    query_embeddings = embedder.encode([query] + _get_words(query), convert_to_tensor=True)
+    cos_scores = util.cos_sim(query_embeddings, corpus_embeddings)
+
+    scores_dict = {}
+    distances, indexes = torch.topk(cos_scores[0], k)
+    for i, dist in enumerate(distances):
+        scores_dict[indexes[i].item()] = dist.item()
+
+    for scores in cos_scores[1:]:
+        dist, index = torch.topk(scores, 1)
+        idx = index.item()
+        scores_dict[idx] = max(scores_dict[idx], dist.item()) if idx in scores_dict else dist.item()
+
+    scores = sorted(scores_dict.items(), key=lambda score: score[1], reverse=True)[:k]
 
     results = []
-    for idx in top_results[1]:
+    for idx, dist in scores:
         results.append(corpus[idx])
 
     return results
