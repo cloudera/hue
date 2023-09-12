6330fbbf52b5a3474845238f62b301a0338e1845
Nidhi Bhat G <nidhibhatg17@gmail.com>
2023-09-12 21:10:33 +0530

    [frontend]Implement UI for actions toolbar

--- desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserPage/StorageBrowserTabContents/StorageBrowserTabContent.tsx
+++ desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserPage/StorageBrowserTabContents/StorageBrowserTabContent.tsx
@@ -15,10 +15,23 @@
 // limitations under the License.
 
 import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import { Spin, Input, Dropdown, Button } from 'antd';
+import type { MenuProps } from 'antd';
 
 import { i18nReact } from '../../../../utils/i18nReact';
 import BucketIcon from '@cloudera/cuix-core/icons/react/BucketIcon';
+import {
+  FileOutlined,
+  CaretDownOutlined,
+  PlusCircleOutlined,
+  CopyOutlined,
+  DownloadOutlined,
+  DeleteOutlined,
+  SwapOutlined,
+  FolderOutlined,
+  UploadOutlined
+} from '@ant-design/icons';
+
 import PathBrowser from '../../../../reactComponents/FileChooser/PathBrowser/PathBrowser';
 import { fetchFiles } from '../../../../reactComponents/FileChooser/api';
 import { PathAndFileData } from '../../../../reactComponents/FileChooser/types';
@@ -43,6 +56,71 @@ const StorageBrowserTabContent: React.FC<StorageBrowserTabContentProps> = ({
   const [loadingFiles, setloadingFiles] = useState(true);
 
   const { t } = i18nReact.useTranslation();
+
+  const newActionsMenuItems: MenuProps['items'] = [
+    {
+      key: 'create',
+      type: 'group',
+      label: 'CREATE',
+      children: [
+        {
+          icon: <FileOutlined />,
+          key: 'new_file',
+          label: 'New File'
+        },
+        {
+          icon: <FolderOutlined />,
+          key: 'new_folder',
+          label: 'New Folder'
+        }
+      ]
+    },
+    {
+      key: 'upload',
+      type: 'group',
+      label: 'UPLOAD',
+      children: [
+        {
+          icon: <UploadOutlined />,
+          key: 'upload_file',
+          label: 'File'
+        },
+        {
+          icon: <UploadOutlined />,
+          key: 'upload_folder',
+          label: 'Folder'
+        },
+        {
+          icon: <UploadOutlined />,
+          key: 'upload_zip',
+          label: 'Zip Folder'
+        }
+      ]
+    }
+  ];
+
+  const bulkActionsMenuItems: MenuProps['items'] = [
+    {
+      icon: <CopyOutlined />,
+      key: 'copy',
+      label: 'Copy'
+    },
+    {
+      icon: <SwapOutlined />,
+      key: 'move',
+      label: 'Move'
+    },
+    {
+      icon: <DownloadOutlined />,
+      key: 'download',
+      label: 'Download'
+    },
+    {
+      icon: <DeleteOutlined />,
+      key: 'delete',
+      label: 'Delete'
+    }
+  ];
 
   useEffect(() => {
     setloadingFiles(true);
@@ -78,6 +156,37 @@ const StorageBrowserTabContent: React.FC<StorageBrowserTabContentProps> = ({
             seperator={'/'}
             showIcon={false}
           />
+        </div>
+        <div className="hue-storage-browser__actions-bar">
+          <Input className="hue-storage-browser__search" placeholder={'Search'} />
+          <div className="hue-storage-browser__actions-bar-right">
+            <Dropdown
+              overlayClassName="hue-storage-browser__bulk-action"
+              menu={{
+                items: bulkActionsMenuItems,
+                className: 'hue-storage-browser__action-menu'
+              }}
+              trigger={['hover', 'click']}
+            >
+              <Button className="hue-storage-browser__bulk-action-btn">
+                Bulk Actions
+                <CaretDownOutlined />
+              </Button>
+            </Dropdown>
+            <Dropdown
+              overlayClassName="hue-storage-browser__new-action"
+              menu={{
+                items: newActionsMenuItems,
+                className: 'hue-storage-browser__action-menu'
+              }}
+              trigger={['hover', 'click']}
+            >
+              <Button className="hue-storage-browser__new-btn" icon={<PlusCircleOutlined />}>
+                New
+                <CaretDownOutlined />
+              </Button>
+            </Dropdown>
+          </div>
         </div>
       </div>
     </Spin>
