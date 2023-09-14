0fd8f23d7731d68ca496518b51717c326a71889f
Harshg999 <harshg999@gmail.com>
2023-09-14 23:51:22 +0530

    [gs] Add upload operation and some required boto code

---
+++ desktop/core/src/desktop/lib/fs/gc/upload.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# Licensed to Cloudera, Inc. under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Classes for a custom upload handler to stream into GS.
+
+See http://docs.djangoproject.com/en/1.9/topics/http/file-uploads/
+"""
+
+from future import standard_library
+standard_library.install_aliases()
+import logging
+import sys
+
+if sys.version_info[0] > 2:
+  from io import BytesIO as stream_io
+else:
+  from cStringIO import StringIO as stream_io
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.uploadhandler import FileUploadHandler, SkipFile, StopFutureHandlers, StopUpload, UploadFileException
+
+from desktop.lib.fsmanager import get_client
+from desktop.lib.fs.gc import parse_uri
+from desktop.lib.fs.gc.gs import S3FileSystemException
+
+if sys.version_info[0] > 2:
+  from django.utils.translation import gettext as _
+else:
+  from django.utils.translation import ugettext as _
+
+DEFAULT_WRITE_SIZE = 1024 * 1024 * 50  # TODO: set in configuration (currently 50 MiB)
+
+LOG = logging.getLogger()
+
+
+class GSFileUploadError(UploadFileException):
+  pass
+
+
+class GSFileUploadHandler(FileUploadHandler):
+  """
+  This handler is triggered by any upload field whose destination path starts with "GS" (case insensitive).
+
+  Streams data chunks directly to GS
+  """
+  def __init__(self, request):
+    super(GSFileUploadHandler, self).__init__(request)
+    self.chunk_size = DEFAULT_WRITE_SIZE
+    self.destination = request.GET.get('dest', None)  # GET param avoids infinite looping
+    self.target_path = None
+    self.file = None
+    self._request = request
+    self._mp = None
+    self._part_num = 1
+
+    if self._is_gs_upload():
+      self._fs = get_client(fs='gs', user=request.user.username)
+      self.bucket_name, self.key_name = parse_uri(self.destination)[:2]
+      # Verify that the path exists
+      self._fs._stats(self.destination)
+      self._bucket = self._fs._get_bucket(self.bucket_name)
+
+
+  def new_file(self, field_name, file_name, *args, **kwargs):
+    if self._is_gs_upload():
+      super(GSFileUploadHandler, self).new_file(field_name, file_name, *args, **kwargs)
+
+      LOG.info('Using GSFileUploadHandler to handle file upload.')
+      self.target_path = self._fs.join(self.key_name, file_name)
+
+      try:
+        # Check access permissions before attempting upload
+        self._check_access()
+        # Create a multipart upload request
+        LOG.debug("Initiating GS multipart upload to target path: %s" % self.target_path)
+        self._mp = self._bucket.initiate_multipart_upload(self.target_path)
+        self.file = SimpleUploadedFile(name=file_name, content='')
+        raise StopFutureHandlers()
+      except (GSFileUploadError, S3FileSystemException) as e:
+        LOG.error("Encountered error in GSUploadHandler check_access: %s" % e)
+        self.request.META['upload_failed'] = e
+        raise StopUpload()
+
+
+  def receive_data_chunk(self, raw_data, start):
+    if self._is_gs_upload():
+      try:
+        LOG.debug("GSFileUploadHandler uploading file part: %d" % self._part_num)
+        fp = self._get_file_part(raw_data)
+        self._mp.upload_part_from_file(fp=fp, part_num=self._part_num)
+        self._part_num += 1
+        return None
+      except Exception as e:
+        self._mp.cancel_upload()
+        LOG.exception('Failed to upload file to GS at %s: %s' % (self.target_path, e))
+        raise StopUpload()
+    else:
+      return raw_data
+
+
+  def file_complete(self, file_size):
+    if self._is_gs_upload():
+      # Finish the upload
+      LOG.info("GSFileUploadHandler has completed file upload to GS, total file size is: %d." % file_size)
+      self._mp.complete_upload()
+      self.file.size = file_size
+      return self.file
+    else:
+      return None
+
+
+  # def _get_s3fs(self, request):
+  #   # Pre 6.0 request.fs did not exist, now it does. The logic for assigning request.fs is not correct for FileUploadHandler.
+  #   fs = get_client(user=request.user.username)
+
+  #   if not fs:
+  #     raise S3FileUploadError(_("No S3 filesystem found."))
+
+  #   return fs
+
+
+  def _is_gs_upload(self):
+    return self._get_scheme() and self._get_scheme().startswith('gs')
+
+
+  def _check_access(self):
+    if not self._fs.check_access(self.destination, permission='WRITE'):
+      raise S3FileSystemException('Insufficient permissions to write to GS path "%s".' % self.destination)
+
+
+  def _get_scheme(self):
+    if self.destination:
+      dst_parts = self.destination.split('://')
+      if dst_parts:
+        return dst_parts[0].lower()
+      else:
+        raise S3FileSystemException('Destination does not start with a valid scheme.')
+    else:
+      return None
+
+
+  def _get_file_part(self, raw_data):
+    fp = stream_io()
+    fp.write(raw_data)
+    fp.seek(0)
+    return fp
