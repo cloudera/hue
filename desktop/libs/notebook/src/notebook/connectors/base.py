cloudera/hue

commit 283676c5f971e799cabfb3a57e1bda10d321851c

[core] Moved namespaces and computes to database
The existing implementation was connecting to external Altus and DWX
api's to fetch the available computes. This commit changes that by
moving the cluster config to database. Two new tables have been added
(a) `beeswax_namespace` to hold config for a namespace/dialect
(b) `beeswax_compute` to hold configs for individual compute clusters
linked to the namespaces.
This change currently support hive and impala clusters.

There is also a service-discovery component that keeps the list of
namespaces and computes updated in the corresponding tables.
`sync_warehouses.py` performs the service discovery in the CDW environ
by talking to kubernetes api's. It creates one Hive and one Impala
namespace and one compute for each virtual warehouse. The command is
supposed to run every minute and keep the list of warehouses updated.

There are related changes and fixes to the rest of the code to support
the query execution on different computes.

Change-Id: Ifd8dbc8d716dfe2000fbfa8121e39f2610051fa1

desktop/libs/notebook/src/notebook/connectors/base.py
@@ -25,6 +25,7 @@ import uuid
 
 from django.utils.encoding import smart_str
 
+from beeswax.models import Compute
 from desktop.auth.backend import is_admin
 from desktop.conf import TASK_SERVER, has_connectors
 from desktop.lib import export_csvxls
@@ -406,6 +407,9 @@ def patch_snippet_for_connector(snippet):
   Connector backward compatibility switcher.
   # TODO Connector unification
   """
+  if snippet['type'] == 'hive-compute' or snippet['type'] == 'impala-compute':
+    # No patching is needed
+    return
   if snippet.get('connector') and snippet['connector'].get('type'):
     if snippet['connector']['dialect'] != 'hplsql':   # this is a workaround for hplsql describe not working
       snippet['type'] = snippet['connector']['type']  # To rename to 'id'
@@ -430,7 +434,14 @@ def get_api(request, snippet):
   if has_connectors() and snippet.get('type') == 'hello' and is_admin(request.user):
     interpreter = snippet.get('interpreter')
   else:
-    interpreter = get_interpreter(connector_type=connector_name, user=request.user)
+    if snippet.get('compute'):
+      interpreter = snippet['compute']
+    elif snippet.get('connector'):
+      interpreter = snippet['connector']
+    elif snippet.get('type') in ('hive-compute', 'impala-compute'):
+      interpreter = Compute.objects.get(id=snippet['id']).to_dict()
+    else:
+      interpreter = get_interpreter(connector_type=connector_name, user=request.user)
 
   interface = interpreter['interface']
 
@@ -438,13 +449,6 @@ def get_api(request, snippet):
   if snippet.get('type') and snippet.get('type') == 'custom': 
     interface = snippet.get('interface') 
     interpreter['options'] = snippet.get('options')
-
-  if get_cluster_config(request.user)['has_computes']:
-    compute = json.loads(request.POST.get('cluster', '""'))  # Via Catalog autocomplete API or Notebook create sessions.
-    if compute == '""' or compute == 'undefined':
-      compute = None
-    if not compute and snippet.get('compute'):  # Via notebook.ko.js
-      interpreter['compute'] = snippet['compute']
 
   LOG.debug('Selected interpreter %s interface=%s compute=%s' % (
     interpreter['type'],

desktop/libs/notebook/src/notebook/connectors/base_tests.py
@@ -44,7 +44,7 @@ class TestNotebook(object):
   def test_get_api(self):
     request = Mock()
     snippet = {
-      'connector': {'optimizer': 'api'},
+      'connector': {'optimizer': 'api', 'interface': 'hiveserver2', 'type': 'hive-compute', 'dialect': 'hive'},
       'type': 'hive'  # Backward compatibility
     }
 
@@ -81,7 +81,7 @@ class TestNotebook(object):
     with patch('notebook.api.Document2.objects.get_by_uuid') as get_by_uuid:
       with patch('notebook.api.get_api') as get_api:
         with patch('notebook.api.Notebook') as NotebookMock:
-          get_api.return_value=Mock(
+          get_api.return_value = Mock(
             check_status=Mock(return_value={'status': 0})
           )
           resp = query.check_status(request=request, operation_id=operation_id)

apps/beeswax/src/beeswax/migrations/0003_compute_namespace.py
@@ -1,0 +1,52 @@
+# Generated by Django 3.2.16 on 2023-09-07 14:30
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('beeswax', '0002_auto_20200320_0746'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Namespace',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(default='', max_length=255)),
+                ('description', models.TextField(default='')),
+                ('dialect', models.CharField(db_index=True, help_text='Type of namespace, e.g. hive, mysql... ', max_length=32)),
+                ('interface', models.CharField(db_index=True, default='sqlalchemy', help_text='Type of interface, e.g. sqlalchemy, hiveserver2... ', max_length=32)),
+                ('external_id', models.CharField(db_index=True, max_length=255, null=True)),
+                ('last_modified', models.DateTimeField(auto_now=True, db_index=True, verbose_name='Time last modified')),
+            ],
+            options={
+                'verbose_name': 'namespace',
+                'verbose_name_plural': 'namespaces',
+                'unique_together': {('name',)},
+            },
+        ),
+        migrations.CreateModel(
+            name='Compute',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(default='', max_length=255)),
+                ('description', models.TextField(default='')),
+                ('dialect', models.CharField(db_index=True, help_text='Type of compute, e.g. hive, impala... ', max_length=32)),
+                ('interface', models.CharField(db_index=True, default='sqlalchemy', help_text='Type of interface, e.g. sqlalchemy, hiveserver2... ', max_length=32)),
+                ('is_ready', models.BooleanField(default=True)),
+                ('external_id', models.CharField(db_index=True, max_length=255, null=True)),
+                ('ldap_groups_json', models.TextField(default='[]')),
+                ('settings', models.TextField(default='{}')),
+                ('last_modified', models.DateTimeField(auto_now=True, db_index=True, verbose_name='Time last modified')),
+                ('namespace', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='beeswax.namespace')),
+            ],
+            options={
+                'verbose_name': 'compute',
+                'verbose_name_plural': 'computes',
+                'unique_together': {('name',)},
+            },
+        ),
+    ]

apps/beeswax/src/beeswax/models.py
@@ -35,7 +35,7 @@ from desktop.lib.exceptions_renderable import PopupException
 from desktop.models import Document, Document2
 from desktop.redaction import global_redaction_engine
 from librdbms.server import dbms as librdbms_dbms
-from useradmin.models import User
+from useradmin.models import User, UserProfile
 
 from beeswax.design import HQLdesign
 
@@ -607,3 +607,107 @@ class MetaInstall(models.Model):
       return MetaInstall.objects.get(id=1)
     except MetaInstall.DoesNotExist:
       return MetaInstall(id=1)
+
+class Namespace(models.Model):
+  name = models.CharField(default='', max_length=255)
+  description = models.TextField(default='')
+  dialect = models.CharField(max_length=32, db_index=True, help_text=_t('Type of namespace, e.g. hive, mysql... '))
+  interface = models.CharField(
+    max_length=32,
+    db_index=True,
+    help_text=_t('Type of interface, e.g. sqlalchemy, hiveserver2... '),
+    default='sqlalchemy'
+  )
+  external_id = models.CharField(max_length=255, null=True, db_index=True)
+  last_modified = models.DateTimeField(auto_now=True, db_index=True, verbose_name=_t('Time last modified'))
+
+  class Meta:
+    verbose_name = _t('namespace')
+    verbose_name_plural = _t('namespaces')
+    unique_together = ('name',)
+
+  def get_computes(self, user):
+    """Returns the computes belonging to the current namespace that are available to the current user."""
+    if user is None:
+      return []
+    profile = UserProfile.objects.get(user=user)
+    user_groups = set(profile.data.get("saml_attributes", {}).get("groups", []))
+
+    computes = Compute.objects.filter(namespace=self)
+    computes = [co.to_dict() for co in computes if not co.ldap_groups or co.ldap_groups.intersection(user_groups)]
+    computes.sort(key=lambda c: (not c['is_ready'], c['name']))
+    return computes
+
+  def __str__(self):
+    return '%s (%s)' % (self.name, self.dialect)
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'type': str(self.id),
+      'name': self.name,
+      'description': self.description,
+      'dialect': self.dialect,
+      'interface': self.interface,
+      'external_id': self.external_id,
+      'last_modified': self.last_modified
+    }
+
+class Compute(models.Model):
+  """
+  Instance of a compute type pointing to a Hive or Impala compute resources.
+  """
+  name = models.CharField(default='', max_length=255)
+  description = models.TextField(default='')
+  dialect = models.CharField(max_length=32, db_index=True, help_text=_t('Type of compute, e.g. hive, impala... '))
+  interface = models.CharField(
+      max_length=32,
+      db_index=True,
+      help_text=_t('Type of interface, e.g. sqlalchemy, hiveserver2... '),
+      default='sqlalchemy'
+  )
+  namespace = models.ForeignKey(Namespace, on_delete=models.CASCADE, null=True)
+  is_ready = models.BooleanField(default=True)
+  external_id = models.CharField(max_length=255, null=True, db_index=True)
+  ldap_groups_json = models.TextField(default='[]')
+  settings = models.TextField(default='{}')
+  last_modified = models.DateTimeField(auto_now=True, db_index=True, verbose_name=_t('Time last modified'))
+
+  class Meta:
+    verbose_name = _t('compute')
+    verbose_name_plural = _t('computes')
+    unique_together = ('name',)
+
+  def __str__(self):
+    return '%s (%s)' % (self.name, self.dialect)
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'type': self.dialect + '-compute',
+      'name': self.name,
+      'namespace': self.namespace.name,
+      'description': self.description,
+      'dialect': self.dialect,
+      'interface': self.interface,
+      'is_ready': self.is_ready,
+      'options': self.options,
+      'external_id': self.external_id,
+      'last_modified': self.last_modified
+    }
+
+  @property
+  def ldap_groups(self):
+    if not self.ldap_groups_json:
+      self.ldap_groups_json = json.dumps([])
+    return set(json.loads(self.ldap_groups_json))
+
+  @ldap_groups.setter
+  def ldap_groups(self, val):
+    self.ldap_groups_json = json.dumps(list(val or []))
+
+  @property
+  def options(self):
+    if not self.settings:
+      self.settings = json.dumps([])
+    return {setting['name']: setting['value'] for setting in json.loads(self.settings)}

apps/beeswax/src/beeswax/server/dbms.py
@@ -27,6 +27,7 @@ from django.core.cache import caches
 from django.urls import reverse
 from kazoo.client import KazooClient
 
+from beeswax.models import Compute
 from desktop.conf import CLUSTER_ID, has_connectors
 from desktop.lib.django_util import format_preserving_redirect
 from desktop.lib.exceptions_renderable import PopupException
@@ -142,8 +143,9 @@ def get(user, query_server=None, cluster=None):
 
 
 def get_query_server_config(name='beeswax', connector=None):
-  if connector and has_connectors(): # TODO: Give empty connector when no connector in use
-    LOG.debug("Query via connector %s" % name)
+  if connector and (has_connectors() or connector.get('compute')
+                    or connector.get('type') in ('hive-compute', 'impala-compute')):
+    LOG.debug("Query via connector %s (%s)" % (name, connector.get('type')))
     query_server = get_query_server_config_via_connector(connector)
   else:
     LOG.debug("Query via ini %s" % name)
@@ -306,28 +308,27 @@ def get_query_server_config(name='beeswax', connector=None):
 
 def get_query_server_config_via_connector(connector):
   # TODO: connector is actually a notebook interpreter
-  connector_name = full_connector_name = connector['type']
-  compute_name = None
-  if connector.get('compute'):
-    compute_name = connector['compute']['name']
-    full_connector_name = '%s-%s' % (connector_name, compute_name)
-  LOG.debug("Query cluster connector %s compute %s" % (connector_name, compute_name))
+  compute = connector.get('compute', connector)
+  connector_name = connector['type']
+  compute_name = compute['name']
+  if compute.get('id'):
+    compute = Compute.objects.get(id=compute['id']).to_dict() #Reload the full compute from db
+  LOG.debug("Query cluster connector %s compute %s" % (connector_name, compute))
 
-  if connector['options'].get('has_ssh') == 'true':
+  if compute['options'].get('has_ssh') == 'true':
     server_host = '127.0.0.1'
-    server_port = connector['options']['server_port']
   else:
-    server_host = (connector['compute']['options'] if 'compute' in connector else connector['options'])['server_host']
-    server_port = int((connector['compute']['options'] if 'compute' in connector else connector['options'])['server_port'])
+    server_host = compute['options']['server_host']
+  server_port = int(compute['options']['server_port'])
 
-  if 'impersonation_enabled' in connector['options']:
-    impersonation_enabled = connector['options']['impersonation_enabled'] == 'true'
+  if 'impersonation_enabled' in compute['options']:
+    impersonation_enabled = bool(compute['options']['impersonation_enabled'])
   else:
     impersonation_enabled = hiveserver2_impersonation_enabled()
 
   return {
-      'dialect': connector['dialect'],
-      'server_name': full_connector_name,
+      'dialect': compute['dialect'],
+      'server_name': compute_name,
       'server_host': server_host,
       'server_port': server_port,
       'principal': 'TODO',
@@ -335,10 +336,12 @@ def get_query_server_config_via_connector(connector):
       'auth_password': AUTH_PASSWORD.get(),
 
       'impersonation_enabled': impersonation_enabled,
-      'use_sasl': connector['options'].get('use_sasl', 'true') == 'true',
+      'use_sasl': str(compute['options'].get('use_sasl', True)).upper() == 'TRUE',
       'SESSION_TIMEOUT_S': 15 * 60,
       'querycache_rows': 1000,
       'QUERY_TIMEOUT_S': 15 * 60,
+      'transport_mode': compute['options'].get('transport_mode', 'http'),
+      'http_url': compute['options'].get('http_url', 'http://%s:%s/cliservice' % (server_host, server_port)),
   }
 
 

apps/beeswax/src/beeswax/server/hive_server2_lib.py
@@ -667,7 +667,10 @@ class HiveServerClient(object):
         'configuration': {},
     }
     connector_type = 'hive' if self.query_server['server_name'] == 'beeswax' else self.query_server['server_name']
-    interpreter = get_interpreter(connector_type=connector_type, user=self.user)
+    interpreter_dialect = self.query_server['dialect']
+    if not interpreter_dialect:
+      interpreter = get_interpreter(connector_type=connector_type, user=self.user)
+      interpreter_dialect = interpreter.get('dialect')
 
     if self.impersonation_enabled:
       kwargs.update({'username': DEFAULT_USER})
@@ -684,7 +687,7 @@ class HiveServerClient(object):
       if csrf_header and ENABLE_X_CSRF_TOKEN_FOR_HIVE_IMPALA.get():
         kwargs['configuration'].update({'X-CSRF-TOKEN': csrf_header})
 
-    if self.query_server['server_name'] == 'hplsql' or interpreter['dialect'] == 'hplsql': # All the time
+    if self.query_server['server_name'] == 'hplsql' or interpreter_dialect == 'hplsql': # All the time
       kwargs['configuration'].update({'hive.server2.proxy.user': user.username, 'set:hivevar:mode': 'HPLSQL'})
 
     if self.query_server['server_name'] == 'llap': # All the time

apps/beeswax/src/beeswax/server/hive_server2_lib_tests.py
@@ -56,6 +56,8 @@ class TestHiveServerClient():
         'use_sasl': True,
         'server_host': 'localhost',
         'server_port': 10000,
+        'dialect': 'hive',
+        'interface': 'hiveserver2'
     }
 
   def test_open_session(self):

apps/impala/src/impala/dbms_tests.py
@@ -48,8 +48,10 @@ class TestDbms():
 
   def test_get_connector_config(self):
     connector = {
-      'type': 'impala-1',
+      'type': 'impala-compute',
+      'name': 'impala-1',
       'dialect': 'impala',
+      'interface': 'hiveserver2',
       'options': {'server_host': 'gethue.com', 'server_port': 10000}
     }
 

commit 1ea125d350ab969edfa0c44f147082e7d40c9b7f

[core] Fixed the compute selection logic for query exec
There are multiple different ways that the various calls from UI pass
the compute information. Sometimes, it is a compute, other times it
is a connector etc. This change looks at config. So, if computes are
enabled then look up for compute in snippet, if connectors are
enabled then look for connector in snippet. If none of these are found,
then fallback to the default mechanism of picking up interpreter as
defined in the hue.ini

This bug got introduced in the earlier commit 283676c. Also, reverted
the unneeded change to connectors/base_test.py from that commit.

Change-Id: I8148f7dbf031c329f738ed94ba073161d8d8b4df

desktop/libs/notebook/src/notebook/connectors/base.py
@@ -432,16 +432,21 @@ def get_api(request, snippet):
   connector_name = snippet['type']
 
   if has_connectors() and snippet.get('type') == 'hello' and is_admin(request.user):
+    LOG.debug('Using the interpreter from snippet')
     interpreter = snippet.get('interpreter')
-  else:
-    if snippet.get('compute'):
-      interpreter = snippet['compute']
-    elif snippet.get('connector'):
-      interpreter = snippet['connector']
-    elif snippet.get('type') in ('hive-compute', 'impala-compute'):
+  elif get_cluster_config(request.user).get('has_computes'):
+    if snippet.get('type') in ('hive-compute', 'impala-compute') and snippet.get('id'):
+      LOG.debug("Loading the compute from db using snippet['id']: %" % snippet['id'])
       interpreter = Compute.objects.get(id=snippet['id']).to_dict()
-    else:
-      interpreter = get_interpreter(connector_type=connector_name, user=request.user)
+    if snippet.get('compute'):
+      LOG.debug("Using the compute as is from snippet['compute']")
+      interpreter = snippet['compute']
+  elif has_connectors() and snippet.get('connector'):
+    LOG.debug("Connectors are enabled and picking the connector from snippet['connector']")
+    interpreter = snippet['connector']
+  else:
+    LOG.debug("Picking up the connectors from the configs using connector_name: %s" % connector_name)
+    interpreter = get_interpreter(connector_type=connector_name, user=request.user)
 
   interface = interpreter['interface']
 

desktop/libs/notebook/src/notebook/connectors/base_tests.py
@@ -44,7 +44,7 @@ class TestNotebook(object):
   def test_get_api(self):
     request = Mock()
     snippet = {
-      'connector': {'optimizer': 'api', 'interface': 'hiveserver2', 'type': 'hive-compute', 'dialect': 'hive'},
+      'connector': {'optimizer': 'api'},
       'type': 'hive'  # Backward compatibility
     }
 