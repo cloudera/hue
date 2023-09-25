cf5ffee7417b42b4f919b06db1e7ab4306aa790d
sreenaths <somarajapuram@gmail.com>
2023-09-25 10:22:12 -0700

    CDPD-61640: [backend-services] Azure integration
    (cherry picked from commit a0cc64ce28d9bad1a6f8ecbd7ade0341a7032eb1)
    (cherry picked from commit cb7a3555715a391c085f09b207a7d69d0047709a)
    Change-Id: I4ba5dd34b202c9c928c7f702df237bd7401dcf67

---
+++ desktop/core/src/desktop/lib/ai/services/azure.py
@@ -0,0 +1,32 @@
+from .base_service import BaseService
+
+from desktop.conf import AI_INTERFACE, get_ai_service_token
+
+_default_model = "gpt"
+_model_name = AI_INTERFACE.MODEL_NAME.get() or "gpt-3.5-turbo-16k"
+_api_key = get_ai_service_token()
+
+class AzureService(BaseService):
+    def __init__(self):
+        import openai
+        openai.api_type = "azure"
+        openai.api_base = AI_INTERFACE.BASE_URL.get()
+        openai.api_key = AI_INTERFACE.TOKEN.get()
+        openai.api_version = "2023-07-01-preview" # TODO: This could be a configuration
+
+        self.openai = openai
+
+    def get_default_model(self) -> str:
+        return _default_model
+
+    def process(self, prompt: str) -> str:
+        response = self.openai.ChatCompletion.create(
+            engine=AI_INTERFACE.MODEL_NAME.get(),
+            temperature=0,
+            messages=[{
+                "role": "user",
+                "content": prompt
+            }]
+        )
+        choices = response.choices[0]
+        return choices.message.content.strip()
