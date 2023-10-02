7900c15520bdebd4c14a2fb10a0d3a48fa043ea1
Björn Alm <balm@cloudera.com>
2023-10-02 21:35:43 +0200

    CDPD-61923: [prompt-engineering] align titan templates with gpt changes
    (cherry picked from commit a77fe3e155deec4d7de7c3670c22f5d45ff50860)
    (cherry picked from commit b448a528453a712a6e844f5e9096af82009a31f2)
    Change-Id: I6a21add23d3bb015b9f34ec89592a37b7fdb4365

--- desktop/core/src/desktop/lib/ai/models/titan.py
+++ desktop/core/src/desktop/lib/ai/models/titan.py
@@ -3,44 +3,47 @@ from .base_model import BaseModel
 from ..utils.xml import extract_tag_content
 from ..types import SQLResponse
 
-_GENERATE = """Act as an {dialect} SQL expert. Translate the NQL statement into SQL using the following metadata: {metadata}.
+_GENERATE = """
+YOUR MAIN GOAL:
+Act as an {dialect} SQL expert and Translate the NQL statement into SQL. Return the SQL wrapped in a <code> tag.
 Use lower() and LIKE '%%' unless you are sure about how to match the data.
 
-Always list any assumptions not covered by the supplied metadata.
-Always warn if the generated SQL modifies or deletes data or risk being very resource-intensive. The warning should inform why the SQL could be dangeroues to execute.
-If the NQL can't be interpreted then there is a semantic error and you should explain the reason for it.
-If the SQL can be generated is should be placed in the code tag.
+YOUR ADDITIONAL GOALS:
+List any assumptions not covered by the supplied metadata wrapped in an <assumptions> tag.
+Always inlcude a warning in a <warning> tag if the generated SQL modifies or deletes data or risk being very resource-intensive. The warning should inform why the SQL could be dangeroues to execute.
+Return an error message wrapped in a <semanticerror> tag if the NQL can't be interpreted.
 
 NQL: {input}
 
-Return the result in the following format:
-<code></code>
-<semanticerror></semanticerror>
-<assumptions></assumptions>
-<warning></warning>
+METADATA: {metadata}
 """
-
 
 # NOTE: Can't get the approach of using a semanticerror tag to work with this template. Likely too much noice. 
 # If the EDIT_INSTRUCTION is non-sensical then the LLM will often return the SQL as is which is then handled as a non diff in the UI. 
-_EDIT = """Act as an {dialect} SQL expert. Use the EDIT_INSTRUCTION below and modify the SQL using the following metadata: {metadata}.
+_EDIT= """
+YOUR MAIN GOAL:
+Act as an {dialect} SQL expert and modify the SQL based on the INPUT. Return the SQL wrapped in a <code> tag.
 Use lower() and LIKE '%%' unless you are sure about how to match the data.
 
+YOUR ADDITIONAL GOALS:
+List any assumptions not covered by the supplied metadata wrapped in an <assumptions> tag.
+Always include a warning in a <warning> tag if the generated SQL modifies or deletes data or risk being very resource-intensive. The warning should inform why the SQL could be dangeroues to execute.
+
 SQL: {sql}
-EDIT_INSTRUCTION: {input}
 
-Always list any assumptions not covered by the supplied metadata.
-Always warn if the generated SQL modifies or deletes data. The warning should inform why the SQL could be dangeroues to execute.
-Only return any SQL if you have modified it based on the EDIT_INSTRUCTION in wrapped in <code>.
+INPUT: {input}
 
-Return the result in the following format:
-<code></code>
-<assumptions></assumptions>
-<warning></warning>
+METADATA: {metadata}
 """
 
-_SUMMARIZE = """Act as an {dialect} SQL expert. Based on the input Summarize the SQL using the following metadata: {metadata}.
-Explain in natural language using non technical terms, what this query does: {sql}.
+_SUMMARIZE = """
+SQL: {sql}
+
+METADATA: {metadata}
+
+Act as an {dialect} SQL expert. Explain in detail what the provided SQL query does and wrap the explation in an <explain> tag with closing </explain>.
+Provide a short summary in natural language of the expected result. Wrap the summary in a <summary> tag tag with closing </summary>.
+
 """
 
 _OPTIMIZE = """Act as an {dialect} SQL expert.
@@ -49,10 +52,10 @@ Use the following metadata: {metadata}.
 
 SQL: {sql}
 
-Always explain the optimzation or suggest alternative options if any. The explanation should be wrapped in an <explain> tag.
+Always explain the optimization or suggest alternative options if any. The explanation should be wrapped in an <explain> tag but should not contain SQL.
 If the SQL can't be interpreted then there is a sqlerror and you should explain the reason for it.
 If the SQL can't be optimized then shortly explain alternative options if any.
-If the SQL can be optimized is should be placed in the code tag and an explanation in the explain tag.
+If the SQL can be optimized is should be placed in the code tag. 
 
 Return the result in the following format:
 <code></code>
@@ -94,10 +97,13 @@ def _code_explain_parser(response: str) -> SQLResponse:
         sqlerror=extract_tag_content('sqlerror', response),
     )
 
+
 def _summary_parser(response: str) -> SQLResponse:
     return SQLResponse(
-        summary=response,
+        summary=extract_tag_content('summary', response),
+        explain=extract_tag_content('explain', response),
     )
+
 
 _TASKS = {
     TaskType.GENERATE: Task(_GENERATE, _code_assumptions_parser),
