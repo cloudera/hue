e90195b03d2982c60eee397748cc389e87b51e5e
tabraiz <tabraiz@cloudera.com>
2023-10-09 13:47:06 +0530

    CDPD-62113: [lint] Linting issues
    (cherry picked from commit 8931e30cfaaf567f9af5486f2602e0c41473b652)
    (cherry picked from commit 10bcdf4bd4934834e7b160181feaaadac4fb65ea)
    Change-Id: I7e68060802f7f6cc42c82cb895e27cc43114dd53

--- desktop/core/src/desktop/js/reactComponents/loadCatalogs.ts
+++ desktop/core/src/desktop/js/reactComponents/loadCatalogs.ts
@@ -1,8 +1,6 @@
-
 import { getLastKnownConfig } from 'config/hueConfig';
 import huePubSub from 'utils/huePubSub';
 import sleep from 'utils/timing/sleep';
-
 
 const fetchColumnsData = async (databaseName: string, tableName: string, executor: any) => {
   const dbEntry = await executor.dataCatalog.getEntry({
@@ -15,7 +13,11 @@ const fetchColumnsData = async (databaseName: string, tableName: string, executo
   return columns;
 };
 
-export const getRelevantTableDetails = async (databaseName: string, tableNames: string[], executor: any) => {
+export const getRelevantTableDetails = async (
+  databaseName: string,
+  tableNames: string[],
+  executor: any
+): Promise<any[]> => {
   const relevantTables: any[] = [];
   for (const tableName of tableNames) {
     const columns = await fetchColumnsData(databaseName, tableName, executor);
@@ -29,49 +31,47 @@ export const getRelevantTableDetails = async (databaseName: string, tableNames: 
   return relevantTables;
 };
 
-const getCacheProgress = (databaseName: string) => {
-  const progressKey = 'database_cache_progress_' + databaseName;
-  const progress = localStorage.getItem(progressKey);
-  return progress ? parseInt(progress, 10) : 0;
-};
-
-export async function loadDataCatalogs() {
+export async function loadDataCatalogs(): Promise<void> {
   const config = getLastKnownConfig();
-  if (!(config?.hue_config?.is_ai_interface_enabled)) {
+  if (!config?.hue_config?.is_ai_interface_enabled) {
     return;
   }
 
-  huePubSub.subscribe('fetch_tables_metadata', async (data: { sourceMetaPromise: Promise<any>; entry: any }) => {
-    const dataCatalogEntry = await data.sourceMetaPromise;
-    if (!dataCatalogEntry.tables_meta) {
-      return;
-    }
-
-    const databaseName = data.entry.path[0];
-    const totalTables = dataCatalogEntry.tables_meta.length;
-
-    const config = getLastKnownConfig();
-    const configLimit = config?.hue_config?.auto_fetch_table_meta_limit
-    if (configLimit === 0) {
-      return;
-    }
-
-    const numTablesToLoad = configLimit === -1 ? totalTables : Math.min(configLimit || 20, totalTables);
-
-    let completedTables = 0;
-
-    for (let i = 0; i < numTablesToLoad; i++) {
-      const eachTable = dataCatalogEntry.tables_meta[i];
-      await sleep(100);
-      try {
-        await fetchColumnsData(databaseName, eachTable.name, data.entry);
-        completedTables++;
-      } catch (error) {
-        console.error('Error loading table: ' + eachTable.name, error);
-        continue;
+  huePubSub.subscribe(
+    'fetch_tables_metadata',
+    async (data: { sourceMetaPromise: Promise<any>; entry: any }) => {
+      const dataCatalogEntry = await data.sourceMetaPromise;
+      if (!dataCatalogEntry.tables_meta) {
+        return;
       }
-      const progress = Math.floor((completedTables / numTablesToLoad) * 100);
-      localStorage.setItem('database_cache_progress_' + databaseName, progress.toString());
+
+      const databaseName = data.entry.path[0];
+      const totalTables = dataCatalogEntry.tables_meta.length;
+
+      const config = getLastKnownConfig();
+      const configLimit = config?.hue_config?.auto_fetch_table_meta_limit;
+      if (configLimit === 0) {
+        return;
+      }
+
+      const numTablesToLoad =
+        configLimit === -1 ? totalTables : Math.min(configLimit || 20, totalTables);
+
+      let completedTables = 0;
+
+      for (let i = 0; i < numTablesToLoad; i++) {
+        const eachTable = dataCatalogEntry.tables_meta[i];
+        await sleep(100);
+        try {
+          await fetchColumnsData(databaseName, eachTable.name, data.entry);
+          completedTables++;
+        } catch (error) {
+          console.error('Error loading table: ' + eachTable.name, error);
+          continue;
+        }
+        const progress = Math.floor((completedTables / numTablesToLoad) * 100);
+        localStorage.setItem('database_cache_progress_' + databaseName, progress.toString());
+      }
     }
-  });
+  );
 }
