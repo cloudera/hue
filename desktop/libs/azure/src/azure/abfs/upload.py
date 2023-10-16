223187a93a847213df3c9eb88613d3fc9e9813d3
Prakash Ranade <ranade@cloudera.com>
2023-10-16 14:03:44 -0700

    Working S3, HDFS version, Code added for ADLS and Ozone with Test cases
    Testing Done:
    1. Tested on HDFS, S3, ADLS and Ozone upload

--- desktop/libs/azure/src/azure/abfs/upload.py
+++ desktop/libs/azure/src/azure/abfs/upload.py
@@ -17,6 +17,7 @@ from future import standard_library
 standard_library.install_aliases()
 import logging
 import sys
+import unicodedata
 
 if sys.version_info[0] > 2:
   from io import StringIO as string_io
@@ -26,6 +27,7 @@ else:
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files.uploadhandler import FileUploadHandler, SkipFile, StopFutureHandlers, StopUpload, UploadFileException
 
+from desktop.lib.exceptions_renderable import PopupException
 from desktop.lib.fsmanager import get_client
 from azure.abfs.__init__ import parse_uri
 from azure.abfs.abfs import ABFSFileSystemException
@@ -35,10 +37,92 @@ if sys.version_info[0] > 2:
 else:
   from django.utils.translation import ugettext as _
 
-DEFAULT_WRITE_SIZE = 30 * 1000 * 1000 # TODO: set in configuration
+DEFAULT_WRITE_SIZE = 100 * 1024 * 1024 # As per Azure doc, maximum blob size is 100MB
 
 LOG = logging.getLogger()
 
+from filebrowser.utils import generate_chunks, calculate_total_size
+
+class ABFSFineUploaderChunkedUpload(object):
+  def __init__(self, request, *args, **kwargs):
+    self.destination = kwargs.get('dest', None)  # GET param avoids infinite looping
+    self.target_path = None
+    self.file = None
+    self._request = request
+    self._part_size = DEFAULT_WRITE_SIZE
+
+    self.qquuid = kwargs.get('qquuid')
+    self.qqtotalparts = kwargs.get('qqtotalparts')
+    self.totalfilesize = kwargs.get('qqtotalfilesize')
+    self.file_name = kwargs.get('qqfilename')
+    if self.file_name:
+      self.file_name = unicodedata.normalize('NFC', self.file_name) # Normalize unicode
+
+    if self._is_abfs_upload():
+      self._fs = self._get_abfs(request)
+      self.filesystem, self.directory = parse_uri(self.destination)[:2]
+       # Verify that the path exists
+      self._fs.stats(self.destination)
+    LOG.debug("Chunk size = %d" % DEFAULT_WRITE_SIZE)
+
+  def check_access(self):
+    LOG.info('ABFSFineUploaderChunkedUpload: handle file upload wit temp file %s.' % self.file_name)
+    self.target_path = self._fs.join(self.destination, self.file_name)
+
+    try:
+      # Check access permissions before attempting upload
+      #self._check_access() #implement later
+      LOG.debug("ABFSFineUploaderChunkedUpload: Initiating ABFS upload to target path: %s" % self.target_path)
+      self._fs.create(self.target_path)
+    except (ABFSFileUploadError, ABFSFileSystemException) as e:
+      LOG.error("ABFSFineUploaderChunkedUpload: Encountered error in ABFSUploadHandler check_access: %s" % e)
+      self.request.META['upload_failed'] = e
+      raise PopupException("ABFSFineUploaderChunkedUpload: Initiating ABFS upload to target path: %s failed %s" % (self.target_path, e))
+
+    if self.totalfilesize != calculate_total_size(self.qquuid, self.qqtotalparts):
+      raise PopupException(_('ABFSFineUploaderChunkedUpload: Sorry, the file size is not correct. %(name)s %(qquuid)s %(size)s') %
+                            {'name': self.file_name, 'qquuid': self.qquuid, 'size': self.totalfilesize})
+
+  def upload_chunks(self):
+    try:
+      for i, (chunk, total) in enumerate(generate_chunks(self.qquuid, self.qqtotalparts, default_write_size=DEFAULT_WRITE_SIZE), 1):
+        LOG.debug("ABFSFineUploaderChunkedUpload: uploading file %s, part %d, size %d, dest: %s" %
+                  (self.file_name, i, total, self.destination))
+        self._fs._append(self.target_path, chunk)
+    except Exception as e:
+      self._fs.remove(self.target_path)
+      LOG.exception('ABFSFineUploaderChunkedUpload: Failed to upload file to ABFS at %s: %s' % (self.target_path, e))
+      raise PopupException("ABFSFineUploaderChunkedUpload: S3FileUploadHandler uploading file %s part: %d failed" % (self.filepath, i))
+    finally:
+      #finish the upload
+      self._fs.flush(self.target_path, {'position': self.totalfilesize})
+      LOG.info("ABFSFineUploaderChunkedUpload: has completed file upload to ABFS, total file size is: %d." % self.totalfilesize)
+      LOG.debug("%s" % self._fs.stats(self.target_path))
+
+  def upload(self):
+    self.check_access()
+    self.upload_chunks()
+
+  def _get_abfs(self, request):
+    fs = get_client(fs='abfs', user=request.user.username)
+
+    if not fs:
+      raise ABFSFileUploadError(_("ABFSFineUploaderChunkedUpload: No ABFS filesystem found"))
+
+    return fs
+
+  def _is_abfs_upload(self):
+    return self._get_scheme() and self._get_scheme().startswith('ABFS')
+
+  def _get_scheme(self):
+    if self.destination:
+      dst_parts = self.destination.split('://')
+      if dst_parts:
+        return dst_parts[0].upper()
+      else:
+        raise ABFSFileSystemException('Destination does not start with a valid scheme.')
+    else:
+      return None
 
 class ABFSFileUploadError(UploadFileException):
   pass
