f319ac05dc43778837b176be45d8a7e1dc8835c2
Ananya_Agarwal <68558847+ananya-agarwal@users.noreply.github.com>
2023-11-03 22:24:18 +0530

    CDPD-62397: [frontend] Make it possible to configure the default editor to use for all users in Hue (#3514)
    - When a user visits Hue we open the editor by default after logging in. If Hue is configured to have multiple editors the first one is used by default.
    
    - Each user have the possibility to pick the default editor by clicking the star icon in the editor header. There are cases when an organisation wants to control this for all users through configuration so we should add such a config setting in the .ini.
    
    The order of deciding which editor to display would be:
    
    - If the user has "stared" an editor use that one
    - If there's a config for which editor to use use that one (new)
    - And lastly, pick the first editor in the list
    
    ---------
    
    Co-authored-by: Johan Åhlén <johan@johanahlen.com>
    (cherry picked from commit 45667d5216fc364a9c5a6bf356fc1bafcccbab30)
    (cherry picked from commit 414b4b49d44caaf059c315864783f8480b66c602)
    Change-Id: I43ae0f58274c436bac8b0f77854ee4dc614ad861
    (cherry picked from commit 0195177692d032d6f585003ea1a3fad1b3a8ff84)

--- desktop/libs/notebook/src/notebook/conf.py
+++ desktop/libs/notebook/src/notebook/conf.py
@@ -207,6 +207,13 @@ INTERPRETERS_SHOWN_ON_WHEEL = Config(
   default=[]
 )
 
+DEFAULT_INTERPRETER = Config(
+  key="default_interpreter",
+  help=_t("Set the default interpreter for all users. Starred interpreters at user level will get more priority than the value below."),
+  type=str,
+  default=''
+)
+
 DEFAULT_LIMIT = Config(
   "default_limit",
   help="Default limit to use in SELECT statements if not present. Set to 0 to disable.",
