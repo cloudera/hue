58f1d2a2f59dd2740de276a7c201c5f15902d7b1
Ananya_Agarwal <ananyaag06@gmail.com>
2023-12-18 18:40:13 +0530

    Fixes made after running npm run lint

--- desktop/core/src/desktop/js/ko/bindings/ko.dropzone.js
+++ desktop/core/src/desktop/js/ko/bindings/ko.dropzone.js
@@ -17,8 +17,8 @@
 import Dropzone from 'dropzone';
 import $ from 'jquery';
 import * as ko from 'knockout';
-
 import huePubSub from 'utils/huePubSub';
+
 import I18n from 'utils/i18n';
 
 // TODO: Depends on Dropzone
