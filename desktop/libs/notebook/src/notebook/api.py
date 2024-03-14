9da147d6645bd18a99c6aaec2251ef9b994dc92d
Johan Åhlén <johan@johanahlen.com>
2024-03-14 20:54:47 +0100

    CDPD-68773: [editor] Fix a bug where saving an actual notebook has the incorrect document type (#3655)
    (cherry picked from commit c8872fceea705232d014846356403a2959a34225)
    (cherry picked from commit dca4884e4fc6760750b9e6c84ce2254a3db19725)
    Change-Id: I9d726e876bdd37711a2ae78d578fde5c46a667ac
    (cherry picked from commit bbf0c735ae80a300256c86163302e1cf8e125f17)

--- desktop/libs/notebook/src/notebook/api.py
+++ desktop/libs/notebook/src/notebook/api.py
@@ -527,7 +527,8 @@ def get_logs(request):
   return JsonResponse(response)
 
 def _save_notebook(notebook, user):
-  if notebook['snippets'][0].get('connector') and notebook['snippets'][0]['connector'].get('dialect'):  # TODO Connector unification
+  if notebook.get('type') != 'notebook' and notebook['snippets'][0].get('connector') and \
+          notebook['snippets'][0]['connector'].get('dialect'):  # TODO Connector unification
     notebook_type = 'query-%(dialect)s' % notebook['snippets'][0]['connector']
     if notebook['snippets'][0] and notebook['snippets'][0].get('executor'):
       notebook['snippets'][0]['executor']['executables'] = []
