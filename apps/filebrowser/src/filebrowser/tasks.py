bd4398b7edf2ead3bf99c6bd87b31b6420d01b13
Athithyaa Selvam <aselvam@cloudera.com>
2024-03-21 17:01:26 -0700

    [Backend] Fixing the upload popup, the stay until all uploads are complete. Fixing document clean up install dir and duration calculation
    Change-Id: I901af89a258217e0584657b7e5e3d53b924f8ef8
    (cherry picked from commit 65bb8d40d329629605341314f95b3855cd8aa709)

--- apps/filebrowser/src/filebrowser/tasks.py
+++ apps/filebrowser/src/filebrowser/tasks.py
@@ -14,13 +14,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import datetime
 import logging
+import subprocess
+import os
 
 from celery.utils.log import get_task_logger
 from celery import states
 from django.db import transaction
 from django.http import HttpRequest
+from django.utils import timezone
 
 from desktop.auth.backend import rewrite_user
 from desktop.celery import app
@@ -60,7 +62,9 @@ def upload_file_task(**kwargs):
   kwargs["username"] = request.user.username
   kwargs["task_name"] = "fileupload"
   kwargs["state"] = "STARTED"
-  kwargs["task_start"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+  now = timezone.now()
+  kwargs["task_start"]=now.strftime("%Y-%m-%dT%H:%M:%S")
+  # kwargs["task_start"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
   upload_file_task.update_state(task_id=task_id, state='STARTED', meta=kwargs)
   try:
     from filebrowser.views import UPLOAD_CLASSES
@@ -75,8 +79,8 @@ def upload_file_task(**kwargs):
     raise Exception(f"Upload failed %s" % err)
 
   kwargs["state"] = "SUCCESS"
-  kwargs["started"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-  kwargs["task_end"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+  kwargs["started"] = now.strftime("%Y-%m-%dT%H:%M:%S")
+  kwargs["task_end"] = now.strftime("%Y-%m-%dT%H:%M:%S")
   upload_file_task.update_state(task_id=task_id, state='SUCCESS', meta=kwargs)
   return
   
@@ -92,3 +96,44 @@ def _get_request(postdict=None, user_id=None, scheme=None):
   request.user = user
 
   return request
+
+@app.task()
+def document_cleanup_task( **kwargs):
+    keep_days = kwargs.get('keep_days')
+    user_id = kwargs.get('user_id')
+    # user = User.objects.get(id=user_id)
+    # user = rewrite_user(user)
+    task_id = kwargs.get("qquuid")
+    now = timezone.now()
+
+    kwargs["username"] = kwargs["username"]
+    kwargs["task_name"] = "document_cleanup"
+    kwargs["state"] = "STARTED"
+    kwargs["parameters"] = keep_days
+    kwargs["task_id"] = task_id
+    kwargs["task_start"] = now.strftime("%Y-%m-%dT%H:%M:%S")
+
+    try:
+
+        # INSTALL_DIR = "/Users/aselvam/Desktop/work_cloudera/hues/cdh/hue"
+        INSTALL_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+
+        # Update task state to RUNNING
+        document_cleanup_task.update_state(task_id=task_id, state='RUNNING', meta=kwargs)
+
+        # Run the cleanup command
+        subprocess.check_call([INSTALL_DIR + '/build/env/bin/hue', 'desktop_document_cleanup', f'--keep-days={keep_days}'])
+
+        # Update task state to SUCCESS
+        kwargs["state"] = "SUCCESS"
+        print(f"Done success")
+    except Exception as err:
+        # Update task state to FAILURE
+        document_cleanup_task.update_state(task_id=task_id, state='FAILURE', meta=kwargs)
+        raise Exception(f"Upload failed %s" % err)
+
+    kwargs["state"] = "SUCCESS"
+    kwargs["task_end"] = now.strftime("%Y-%m-%dT%H:%M:%S")
+    document_cleanup_task.update_state(task_id=task_id, state='SUCCESS', meta=kwargs)
+
+    return
