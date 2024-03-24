804e7f42e84640bb01f17caf3fd3d3647517a535
tabraiz <tabraiz@cloudera.com>
2024-03-25 05:19:25 +0530

    CDPD-65912: add a push hook as well
    Change-Id: Iac0df09d415511b0034245330bf3c29eb18e70b2
    (cherry picked from commit 47db78adfa5e89f13a1fe9bfd5bc78e5624958f3)
    (cherry picked from commit c7b56355f3d58091bd69dfca3d358f92dda3a84e)

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# Source path of the precommit hook
-SOURCE_HOOK="tools/git-hooks/pre-commit"
+# Function to install a git hook
+install_hook() {
+  local hook_name=$1
 
-# Destination path of the pre-commit hook
-DEST_HOOK=".git/hooks/pre-commit"
+  # Source and destination paths
+  local source_hook="tools/git-hooks/${hook_name}"
+  local dest_hook=".git/hooks/${hook_name}"
 
-# Copy the precommit hook to .git/hooks directory
-cp "$SOURCE_HOOK" "$DEST_HOOK"
+  # Copy the hook and make it executable
+  if [[ -e "${source_hook}" ]]; then
+    cp "${source_hook}" "${dest_hook}" && chmod +x "${dest_hook}"
+    echo "${hook_name} hook installed successfully."
+  else
+    echo "Error: The hook '${source_hook}' does not exist."
+    exit 1
+  fi
+}
 
-# Make the pre-commit hook executable
-chmod +x "$DEST_HOOK"
+# Select the hook to install based on argument or use default (pre-commit)
+hook_to_install=${1:-pre-commit}
 
-# Install ESLint and related dependencies
-npm install eslint -g
+# Install the selected hook
+install_hook "${hook_to_install}"
 
-echo "ESLint and related dependencies installed."
-echo "Pre-commit hook copied and made executable successfully!"
+# Install ESLint and related dependencies globally if pre-commit is the selected hook
+if ! npm list -g eslint --depth=0 >/dev/null 2>&1; then
+  npm install eslint -g
+  echo "ESLint and related dependencies installed."
+fi
