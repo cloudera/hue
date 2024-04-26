c966163ce349aa0287c5e4d9216fd6d1eed8bdce
Nidhi Bhat G <44032758+nidhibhatg@users.noreply.github.com>
2024-04-26 21:34:04 +0530

    CDPD-68361: [frontend] Implement placeholder for empty table scenario (#3682)
    (cherry picked from commit eb37ea6333d7d91f4af08fba3dba6e74f3b05fd0)
    (cherry picked from commit 4aed948cad4b4006f81a50d3dee8f0d353f72a83)
    Change-Id: I27d343b3abcdc65b9a506bfe422e3c92a331303a
    (cherry picked from commit 7bb81d2b1bfada511699bae90a9661dd580b4ef3)

--- desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserPage/StorageBrowserTable/StorageBrowserTable.tsx
+++ desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserPage/StorageBrowserTable/StorageBrowserTable.tsx
@@ -179,19 +179,29 @@ const StorageBrowserTable: React.FC<StorageBrowserTableProps> = ({
     };
   }, []);
 
+  //function removes ..(previous folder) and .(current folder) from table data
+  const removeDots = (dataSource: StorageBrowserTableData[]) => {
+    return dataSource.length > 2 ? dataSource.slice(2) : [];
+  };
+
+  const locale = {
+    emptyText: t('Folder is empty')
+  };
+
   if (dataSource && pageStats) {
     return (
       <>
         <Table
           className={className}
           columns={getColumns(dataSource[0])}
-          dataSource={dataSource}
+          dataSource={removeDots(dataSource)}
           onRow={onRowClicked}
           pagination={false}
           rowClassName={rowClassName}
           rowKey={(record, index) => record.path + '' + index}
           scroll={{ y: tableHeight }}
           data-testid={`${testId}`}
+          locale={locale}
           {...restProps}
         ></Table>
         <Pagination
