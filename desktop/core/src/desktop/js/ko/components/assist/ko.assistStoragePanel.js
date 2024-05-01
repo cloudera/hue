94687fec1fb2c1fbecb95b1031bca29aa50c29b7
Ananya_Agarwal <ananyaag06@gmail.com>
2024-05-01 17:15:32 +0530

    adding more files for refreshButton-10th query

--- desktop/core/src/desktop/js/ko/components/assist/ko.assistStoragePanel.js
+++ desktop/core/src/desktop/js/ko/components/assist/ko.assistStoragePanel.js
@@ -85,7 +85,7 @@ const TEMPLATE = `
       <!-- /ko -->
       <a class="inactive-action" href="javascript:void(0)" data-bind="click: function () { huePubSub.publish('assist.storage.refresh'); }" title="${I18n(
         'Manual refresh'
-      )}"><i class="pointer fa fa-refresh" data-bind="css: { 'fa-spin blue' : loading }"></i></a>
+      )}"><i class="pointer fa fa-refresh" data-testid="assist-sql--refresh--button" data-bind="css: { 'fa-spin blue' : loading }"></i></a>
     </div>
   </script>
 
