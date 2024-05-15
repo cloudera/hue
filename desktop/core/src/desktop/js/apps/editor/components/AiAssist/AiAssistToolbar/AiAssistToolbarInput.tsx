69aa2c4f64c81f3f90972e0d87a1702fb54809d3
Björn Alm <balm@cloudera.com>
2024-05-15 10:37:21 +0200

    CDPD-73619: CDPD-69921 [ui-editor] include DB name in SQL prompt placeholder and preview title
    In order to clarify to the user that the SQL AI Assistant only works on the currently selected DB we
    add the DB name in two places; prompt placeholder and preview modal title.
    
    - adds unit test
    - auto lint fixes a couple of existing unit tests
    
    Change-Id: I9ea7d6d2c83e0e33798ad5e0ddb6bc58a25fa6d8
    (cherry picked from commit 55f309188e82fbc145d894c7be1ef0a4053ab9c9)
    (cherry picked from commit 87ab248ecd64428bdef15fe364ebf86ed86c76ef)
    (cherry picked from commit 4d99a83e1c1c91689dbde39a65745e157d75ab95)

--- desktop/core/src/desktop/js/apps/editor/components/AiAssist/AiAssistToolbar/AiAssistToolbarInput.tsx
+++ desktop/core/src/desktop/js/apps/editor/components/AiAssist/AiAssistToolbar/AiAssistToolbarInput.tsx
@@ -193,7 +193,7 @@ function AiAssistToolbarInput({
       setHistoryDropdownPostion(calculateDropdownPosition(textareaRef.current, isMultiLine));
       setHistoryDropdownWidth(textareaRef.current.parentElement?.clientWidth);
     }
-  }, [value, prefill, resizeAwareMaxWidth]);
+  }, [inputPromptValue, prefill, resizeAwareMaxWidth, placeholderText]);
 
   useEffect(() => {
     if (!isAnimating && isExpanded) {
