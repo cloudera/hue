83bd921e4ecf36d6f5de8392d3a01c02950d53b7
Harsh Gupta <42064744+Harshg999@users.noreply.github.com>
2024-06-03 18:34:18 +0530

    add tests

--- desktop/libs/aws/src/aws/s3/s3fs_test.py
+++ desktop/libs/aws/src/aws/s3/s3fs_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Licensed to Cloudera, Inc. under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -13,36 +14,145 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import absolute_import
 
-import json
 import os
-import pytest
-import tempfile
+import json
 import string
-import sys
+import tempfile
+from unittest.mock import Mock, patch
 
-from desktop.lib.django_test_util import make_logged_in_client
-from desktop.lib.test_utils import grant_access, add_to_group, add_permission, remove_from_group
-from useradmin.models import User
+import pytest
 
+from aws.conf import AWS_ACCOUNTS
 from aws.s3 import join, parse_uri
-from aws.s3.s3fs import S3FileSystem, S3FileSystemException
+from aws.s3.s3fs import S3FileSystem, S3FileSystemException, get_s3_home_directory
 from aws.s3.s3test_utils import S3TestBase, generate_id
 from aws.s3.upload import DEFAULT_WRITE_SIZE
+from desktop.conf import RAZ
+from desktop.lib.django_test_util import make_logged_in_client
+from desktop.lib.test_utils import add_permission, add_to_group, grant_access, remove_from_group
+from filebrowser.conf import REMOTE_STORAGE_HOME
+from useradmin.models import User
 
 
-if sys.version_info[0] > 2:
-  from unittest.mock import patch, Mock
-else:
-  from mock import patch, Mock
+@pytest.mark.django_db
+def test_get_s3_home_directory():
+  client = make_logged_in_client(username="test", groupname="test", recreate=True, is_superuser=False)
+  user = User.objects.get(username="test")
+
+  client_not_me = make_logged_in_client(username="test_not_me", groupname="test_not_me", recreate=True, is_superuser=False)
+  user_not_me = User.objects.get(username="test_not_me")
+
+  # When REMOTE_STORAGE_HOME ends with /user in RAZ S3 environment.
+  resets = [RAZ.IS_ENABLED.set_for_testing(True), REMOTE_STORAGE_HOME.set_for_testing('s3a://gethue-bucket/user')]
+
+  try:
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://gethue-bucket/user/test'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://gethue-bucket/user/test_not_me'
+  finally:
+    for reset in resets:
+      reset()
+
+  # When S3 filesystem's DEFAULT_HOME_PATH ends with /user in RAZ S3 environment.
+  resets = [
+    RAZ.IS_ENABLED.set_for_testing(True),
+    AWS_ACCOUNTS.set_for_testing(
+      {'default': {'region': 'us-west-2', 'host': 's3-us-west-2.amazonaws.com', 'default_home_path': 's3a://gethue-other-bucket/user'}}
+    ),
+  ]
+
+  try:
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/user/test'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/user/test_not_me'
+  finally:
+    for reset in resets:
+      reset()
+
+  # When S3 filesystem's DEFAULT_HOME_PATH is set in non-RAZ S3 environment.
+  resets = [
+    RAZ.IS_ENABLED.set_for_testing(False),
+    AWS_ACCOUNTS.set_for_testing(
+      {'default': {'region': 'us-west-2', 'host': 's3-us-west-2.amazonaws.com', 'default_home_path': 's3a://gethue-other-bucket/test-dir'}}
+    ),
+  ]
+
+  try:
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/test-dir'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/test-dir'
+  finally:
+    for reset in resets:
+      reset()
+
+  # When both REMOTE_STORAGE_HOME and S3 filesystem's DEFAULT_HOME_PATH are set in RAZ S3 environment.
+  resets = [
+    RAZ.IS_ENABLED.set_for_testing(True),
+    REMOTE_STORAGE_HOME.set_for_testing('s3a://gethue-bucket/user'),
+    AWS_ACCOUNTS.set_for_testing(
+      {'default': {'region': 'us-west-2', 'host': 's3-us-west-2.amazonaws.com', 'default_home_path': 's3a://gethue-other-bucket/user'}}
+    ),
+  ]
+
+  try:
+    # Gives preference to REMOTE_STORAGE_HOME for of backward compatibility.
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://gethue-bucket/user/test'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://gethue-bucket/user/test_not_me'
+  finally:
+    for reset in resets:
+      reset()
+
+  # When S3 filesystem's DEFAULT_HOME_PATH is set but path does not end with ../user or ../user/ in RAZ S3 environment.
+  resets = [
+    RAZ.IS_ENABLED.set_for_testing(True),
+    AWS_ACCOUNTS.set_for_testing(
+      {'default': {'region': 'us-west-2', 'host': 's3-us-west-2.amazonaws.com', 'default_home_path': 's3a://gethue-other-bucket/dir'}}
+    ),
+  ]
+
+  try:
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/dir'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://gethue-other-bucket/dir'
+  finally:
+    for reset in resets:
+      reset()
+
+  # When some different path is set in both RAZ and non-RAZ S3 environment.
+  resets = [
+    RAZ.IS_ENABLED.set_for_testing(True),
+    REMOTE_STORAGE_HOME.set_for_testing('abfs://gethue-container/user'),
+    AWS_ACCOUNTS.set_for_testing(
+      {'default': {'region': 'us-west-2', 'host': 's3-us-west-2.amazonaws.com', 'default_home_path': 'abfs://gethue-other-container/dir'}}
+    ),
+  ]
+
+  try:
+    default_s3_home_path = get_s3_home_directory(user)
+    assert default_s3_home_path == 's3a://'
+
+    default_s3_home_path = get_s3_home_directory(user_not_me)
+    assert default_s3_home_path == 's3a://'
+  finally:
+    for reset in resets:
+      reset()
 
 
-class TestS3FileSystem():
-
+class TestS3FileSystem:
   def test_rmtree_bucket(self):
     with patch('aws.s3.s3fs.S3FileSystem._delete_bucket') as _delete_bucket:
-
       fs = S3FileSystem(s3_connection=Mock())
 
       fs.rmtree(path='s3a://gethue')
@@ -52,19 +162,11 @@ class TestS3FileSystem():
   def test_rmtree_key(self):
     with patch('aws.s3.s3fs.S3FileSystem._get_key') as _get_key:
       with patch('aws.s3.s3fs.S3FileSystem.isdir') as isdir:
-
         key = Mock(
           name='data',
           exists=Mock(return_value=True),
-          bucket=Mock(
-            list=Mock(return_value=[]),
-            delete_key=Mock()
-          ),
-          delete=Mock(
-            return_value=Mock(
-              exists=Mock(return_value=False)
-            )
-          )
+          bucket=Mock(list=Mock(return_value=[]), delete_key=Mock()),
+          delete=Mock(return_value=Mock(exists=Mock(return_value=False))),
         )
         _get_key.return_value = key
         isdir.return_value = False
@@ -79,19 +181,11 @@ class TestS3FileSystem():
   def test_rmtree_empty_dir(self):
     with patch('aws.s3.s3fs.S3FileSystem._get_key') as _get_key:
       with patch('aws.s3.s3fs.S3FileSystem.isdir') as isdir:
-
         key = Mock(
           name='data',
           exists=Mock(return_value=True),
-          bucket=Mock(
-            list=Mock(return_value=[]),
-            delete_key=Mock()
-          ),
-          delete=Mock(
-            return_value=Mock(
-              exists=Mock(return_value=False)
-            )
-          )
+          bucket=Mock(list=Mock(return_value=[]), delete_key=Mock()),
+          delete=Mock(return_value=Mock(exists=Mock(return_value=False))),
         )
         _get_key.return_value = key
         isdir.return_value = True
@@ -107,23 +201,11 @@ class TestS3FileSystem():
   def test_rmtree_non_empty_dir(self):
     with patch('aws.s3.s3fs.S3FileSystem._get_key') as _get_key:
       with patch('aws.s3.s3fs.S3FileSystem.isdir') as isdir:
-
         key = Mock(
           name='data',
           exists=Mock(return_value=True),
-          bucket=Mock(
-            list=Mock(return_value=['data/1', 'data/2']),
-            delete_keys=Mock(
-              return_value=Mock(
-                errors=[]
-              )
-            )
-          ),
-          delete=Mock(
-            return_value=Mock(
-              exists=Mock(return_value=False)
-            )
-          )
+          bucket=Mock(list=Mock(return_value=['data/1', 'data/2']), delete_keys=Mock(return_value=Mock(errors=[]))),
+          delete=Mock(return_value=Mock(exists=Mock(return_value=False))),
         )
         _get_key.return_value = key
         isdir.return_value = True
@@ -138,7 +220,6 @@ class TestS3FileSystem():
 
 
 class S3FSTest(S3TestBase):
-
   @classmethod
   def setup_class(cls):
     S3TestBase.setup_class()
@@ -149,7 +230,6 @@ class S3FSTest(S3TestBase):
       grant_access('test', 'test', 'filebrowser')
       add_to_group('test')
       cls.user = User.objects.get(username="test")
-
 
   def test_open(self):
     path = self.get_test_path('test_open.txt')
@@ -174,7 +254,6 @@ class S3FSTest(S3TestBase):
       with pytest.raises(Exception):
         self.fs.open(path, mode='?r')
 
-
   def test_read(self):
     path = self.get_test_path('test_read.txt')
     with self.cleaning(path):
@@ -184,14 +263,11 @@ class S3FSTest(S3TestBase):
       assert 'Hel' == self.fs.read(path, 0, 3)
       assert 'ell' == self.fs.read(path, 1, 3)
 
-
   def test_isfile(self):
     pass
 
-
   def test_isdir(self):
     pass
-
 
   def test_exists(self):
     dir_path = self.get_test_path('test_exists')
@@ -210,7 +286,6 @@ class S3FSTest(S3TestBase):
     fake_bucket = 'fake%s' % generate_id(8, string.ascii_lowercase + string.digits)
     assert not self.fs.exists('s3a://%s' % fake_bucket)
 
-
   def test_stats(self):
     with pytest.raises(ValueError):
       self.fs.stats('ftp://archive')
@@ -226,7 +301,6 @@ class S3FSTest(S3TestBase):
     assert True == bucket_stat.isDir
     assert 's3a://%s' % self.bucket_name == bucket_stat.path
 
-
   def test_copyfile(self):
     src_path = self.get_test_path('test_copy_file_src')
     dst_path = self.get_test_path('test_copy_file_dst')
@@ -238,7 +312,6 @@ class S3FSTest(S3TestBase):
       self.fs.copyfile(src_path, dst_path)
       actual = self.fs.read(dst_path, 0, len(data) + 100)
       assert data == actual
-
 
   def test_full_copy(self):
     src_path = self.get_test_path('test_full_copy_src')
@@ -269,7 +342,6 @@ class S3FSTest(S3TestBase):
       with pytest.raises(S3FileSystemException):
         self.fs.copy(src_path, dst_file_path, True)
 
-
   def test_copy_remote_dir(self):
     src_dir = self.get_test_path('test_copy_remote_dir_src')
     dst_dir = self.get_test_path('test_copy_remote_dir_dst')
@@ -291,7 +363,6 @@ class S3FSTest(S3TestBase):
       assert src_names
       assert src_names == dst_names
 
-
   def test_copy_from_local(self):
     src_name = 'test_copy_from_local_src'
     src_path = os.path.join(tempfile.gettempdir(), src_name)
@@ -306,7 +377,6 @@ class S3FSTest(S3TestBase):
       self.fs.copyFromLocal(src_path, dst_path)
       actual = self.fs.read(dst_path, 0, len(data) + 100)
       assert data == actual
-
 
   def test_rename_dir(self):
     src_dir = self.get_test_path('test_rename_dir_src')
@@ -343,7 +413,6 @@ class S3FSTest(S3TestBase):
       # Assert that only the renamed directory, and not an empty file, exists
       assert 1 == len([key for key in bucket_ls if key.name.strip('/') == self.get_key(dst_dir).name.strip('/')])
 
-
   def test_rename_star(self):
     src_dir = self.get_test_path('test_rename_star_src')
     dst_dir = self.get_test_path('test_rename_star_dst')
@@ -370,7 +439,6 @@ class S3FSTest(S3TestBase):
       assert src_names
       assert src_names == dst_names
 
-
   def test_rmtree(self):
     with pytest.raises(NotImplementedError):
       self.fs.rmtree('universe', skipTrash=False)
@@ -390,11 +458,9 @@ class S3FSTest(S3TestBase):
       assert not self.fs.exists(nested_dir)
       assert not self.fs.exists(directory)
 
-
   def test_listing_buckets(self):
     buckets = self.fs.listdir('s3a://')
     assert len(buckets) > 0
-
 
   def test_mkdir(self):
     dir_path = self.get_test_path('test_mkdir')
@@ -402,7 +468,6 @@ class S3FSTest(S3TestBase):
 
     self.fs.mkdir(dir_path)
     assert self.fs.exists(dir_path)
-
 
   def test_upload_file(self):
     with tempfile.NamedTemporaryFile() as local_file:
@@ -430,7 +495,6 @@ class S3FSTest(S3TestBase):
       actual = f.read(file_size)
       expected = file(local_file).read()
       assert actual == expected, 'files do not match: %s != %s' % (len(actual), len(expected))
-
 
   def test_check_access(self):
     dir_path = self.get_test_path('test_check_access')
