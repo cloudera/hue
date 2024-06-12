14d49b6d55f3f97f7975a56d7aeb36d415918f92
Harsh Gupta <42064744+Harshg999@users.noreply.github.com>
2024-06-13 00:44:45 +0530

    [tablebrowser] Fix viewsSQL tab for Hive

--- desktop/core/src/desktop/js/apps/tableBrowser/metastoreTable.js
+++ desktop/core/src/desktop/js/apps/tableBrowser/metastoreTable.js
@@ -320,7 +320,7 @@ class MetastoreTable {
           const found =
             analysis.properties &&
             analysis.properties.some(property => {
-              if (property.col_name.toLowerCase() === 'view original text:') {
+              if (['view original text:', 'original query:'].includes(property.col_name.toLowerCase())) {
                 apiHelper
                   .formatSql({ statements: property.data_type })
                   .then(formatResponse => {
