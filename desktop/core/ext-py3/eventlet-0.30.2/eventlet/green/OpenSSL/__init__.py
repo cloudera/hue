ae0cc433a336fb1a01cda2230e3f2009276f1cba
raylliu <raylliu@tencent.com>
2024-06-28 20:05:55 +0800

    [cve] Bump dnspython package to 2.6.1 and eventlet package to 0.35.2 [#3772]

--- desktop/core/ext-py3/eventlet-0.30.2/eventlet/green/OpenSSL/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import crypto
-from . import SSL
-try:
-    # pyopenssl tsafe module was deprecated and removed in v20.0.0
-    # https://github.com/pyca/pyopenssl/pull/913
-    from . import tsafe
-except ImportError:
-    pass
-from .version import __version__
--- desktop/core/ext-py3/eventlet-0.30.2/tests/isolated/test_sub_module_in_import_patched/sample_main_module/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This file is used together with sample_sub_module/__init__.py to setup a
-scenario where symbols are imported from sub modules. It is used to test that
-pacher.import_patched can correctly patch such symbols.
-"""
-
-from .sample_sub_module import function_use_socket
