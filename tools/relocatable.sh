e58830c00c1c46f4075c0b137d1f23385d4d1585
agl29 <ayush.goyal@cloudera.com>
2024-08-12 23:01:43 +0530

    [Python3.11] Add support for Python 3.11 in SLES15 builds

--- tools/relocatable.sh
+++ tools/relocatable.sh
@@ -84,8 +84,12 @@ fi
 export PATH=$(dirname $ENV_PYTHON):$PATH
 
 PYVER=$($ENV_PYTHON -V 2>&1 | awk '{print $2}' | cut -d '.' -f 1,2)
+
+# Define supported Python versions
+supported_versions=("3.8" "3.9" "3.10" "3.11")
+
 # Step 1. Fix virtualenv
-if [[ "$PYVER" == "3."[8-9]* || "$PYVER" == "3.10" ]]; then
+if [[ " ${supported_versions[@]} " =~ " $PYVER " ]]; then
   echo "Python version is 3.8 or greater"
   pushd .
   cd $HUE_ROOT
