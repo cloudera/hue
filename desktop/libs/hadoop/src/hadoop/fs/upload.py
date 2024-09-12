7bdaa681b58099abc2ac9e7931320d8df4fb8d79
Harsh Gupta <42064744+Harshg999@users.noreply.github.com>
2024-09-12 20:16:13 +0530

    [hdfs] Improve log message for left-over temporary file during upload op (#3843)

--- desktop/libs/hadoop/src/hadoop/fs/upload.py
+++ desktop/libs/hadoop/src/hadoop/fs/upload.py
@@ -198,7 +198,7 @@ class HDFStemporaryUploadedFile(object):
     if self._do_cleanup:
       # Do not do cleanup here. It's hopeless. The self._fs threadlocal states
       # are going to be all wrong.
-      LOG.error("Left-over upload file is not cleaned up: %s" % (self._path,))
+      LOG.debug(f"Check for left-over upload file for cleanup if the upload op was unsuccessful: {self._path}")
 
   def get_temp_path(self):
     return self._path
