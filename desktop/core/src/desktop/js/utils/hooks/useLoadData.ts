9d94e920d114856b19b1058ea55338bda8e03c96
Ram Prasad Agarwal <work.ramprasad@gmail.com>
2024-09-17 16:49:08 +0530

    [ui-core]: add onSuccess and onError callback for useLoadData hook (#3849)
    * [ui-core]: add onSuccess and onError callback for useLoadData hook
    
    * fix the interfacing naming convention

--- desktop/core/src/desktop/js/utils/hooks/useLoadData.ts
+++ desktop/core/src/desktop/js/utils/hooks/useLoadData.ts
@@ -17,22 +17,27 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { ApiFetchOptions, get } from '../../api/utils';
 
-export type IOptions<T, U> = {
+export interface Options<T, U> {
   urlPrefix?: string;
   params?: U;
   fetchOptions?: ApiFetchOptions<T>;
   skip?: boolean;
-};
+  onSuccess?: (data: T) => void;
+  onError?: (error: Error) => void;
+}
 
-type IUseLoadData<T> = {
+interface UseLoadDataProps<T> {
   data?: T;
   loading: boolean;
   error?: Error;
   reloadData: () => void;
-};
+}
 
-const useLoadData = <T, U = unknown>(url?: string, options?: IOptions<T, U>): IUseLoadData<T> => {
-  const [localOptions, setLocalOptions] = useState<IOptions<T, U> | undefined>(options);
+const useLoadData = <T, U = unknown>(
+  url?: string,
+  options?: Options<T, U>
+): UseLoadDataProps<T> => {
+  const [localOptions, setLocalOptions] = useState<Options<T, U> | undefined>(options);
   const [data, setData] = useState<T | undefined>();
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | undefined>();
@@ -61,8 +66,14 @@ const useLoadData = <T, U = unknown>(url?: string, options?: IOptions<T, U>): IU
         const fetchUrl = localOptions?.urlPrefix ? `${localOptions.urlPrefix}${url}` : url;
         const response = await get<T, U>(fetchUrl, localOptions?.params, fetchOptions);
         setData(response);
+        if (localOptions?.onSuccess) {
+          localOptions.onSuccess(response);
+        }
       } catch (error) {
         setError(error instanceof Error ? error : new Error(error));
+        if (localOptions?.onError) {
+          localOptions.onError(error);
+        }
       } finally {
         setLoading(false);
       }
