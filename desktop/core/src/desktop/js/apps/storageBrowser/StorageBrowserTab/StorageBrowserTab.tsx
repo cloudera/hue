522abe68eaed8d2a858641f2a0c13320d69b727d
Ram Prasad Agarwal <work.ramprasad@gmail.com>
2025-01-07 14:52:48 +0530

    [ui-storagebrowser] fixes routing between multiple fileSystem (#3937)
    * [ui-storagebrowser] fixes routing between multiple fileSystem
    
    * fixes linting

--- desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserTab/StorageBrowserTab.tsx
+++ desktop/core/src/desktop/js/apps/storageBrowser/StorageBrowserTab/StorageBrowserTab.tsx
@@ -29,9 +29,11 @@ import useLoadData from '../../../utils/hooks/useLoadData';
 import './StorageBrowserTab.scss';
 import StorageFilePage from '../StorageFilePage/StorageFilePage';
 import changeURL from '../../../utils/url/changeURL';
+import { getFileSystemAndPath } from '../../../reactComponents/PathBrowser/PathBrowser.util';
 
 interface StorageBrowserTabProps {
   homeDir: string;
+  fileSystem: string;
   testId?: string;
 }
 
@@ -39,9 +41,16 @@ const defaultProps = {
   testId: 'hue-storage-browser-tab-content'
 };
 
-const StorageBrowserTab = ({ homeDir, testId }: StorageBrowserTabProps): JSX.Element => {
+const StorageBrowserTab = ({
+  homeDir,
+  fileSystem,
+  testId
+}: StorageBrowserTabProps): JSX.Element => {
   const [urlPathname, urlFilePath] = decodeURIComponent(window.location.pathname).split('view=');
-  const [filePath, setFilePath] = useState<string>(urlFilePath || homeDir);
+  const { fileSystem: urlFileSystem } = getFileSystemAndPath(urlFilePath);
+  const initialFilePath = urlFileSystem === fileSystem ? urlFilePath : homeDir;
+
+  const [filePath, setFilePath] = useState<string>(initialFilePath);
   const fileName = filePath.split('/').pop() ?? '';
 
   const { t } = i18nReact.useTranslation();
@@ -51,9 +60,7 @@ const StorageBrowserTab = ({ homeDir, testId }: StorageBrowserTabProps): JSX.Ele
     loading,
     reloadData
   } = useLoadData<FileStats>(FILE_STATS_API_URL, {
-    params: {
-      path: filePath
-    },
+    params: { path: filePath },
     skip: !filePath
   });
 
