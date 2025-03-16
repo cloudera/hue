533501f67499183c5eb1985bb413d0b944d35570
Ananya_Agarwal <ananyaag06@gmail.com>
2025-03-16 13:51:41 +0530

    Fixes based on review comments

--- desktop/core/src/desktop/js/apps/admin/Overview/OverviewTab.test.tsx
+++ desktop/core/src/desktop/js/apps/admin/Overview/OverviewTab.test.tsx
@@ -13,3 +13,79 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Overview from './OverviewTab';
+import Analytics from './Analytics';
+// import ApiHelper from '../../../api/apiHelper';
+// import saveCollectUsagePreference from 
+
+jest.mock('./ConfigStatus', () => () => <div>MockedConfigStatusComponent</div>);
+jest.mock('./Examples', () => () => <div>MockedExamplesComponent</div>);
+jest.mock('./Analytics', () => () => <div>MockedAnalyticsComponent</div>);
+
+  jest.mock('../../../api/apiHelper', () => ({
+    ...jest.requireActual('../../../api/apiHelper'),
+    updatePreferences: jest.fn(() => Promise.resolve({ status: 0, data: 'Success message' }))
+  }));
+describe('OverviewTab', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test('renders the Tabs with the correct tab labels', () => {
+    render(<Overview />);
+    expect(screen.getByText('ConfigStatus')).toBeInTheDocument();
+    expect(screen.getByText('Examples')).toBeInTheDocument();
+    expect(screen.getByText('Analytics')).toBeInTheDocument();
+  });
+
+  test('renders the ConfigStatus tab by default', () => {
+    render(<Overview />);
+    expect(screen.getByText('MockedConfigStatusComponent')).toBeInTheDocument();
+    expect(screen.queryByText('MockedExamplesComponent')).not.toBeInTheDocument();
+    expect(screen.queryByText('MockedAnalyticsComponent')).not.toBeInTheDocument();
+  });
+
+  test('shows the trademark text', () => {
+    render(<Overview />);
+    expect(screen.getByText('Hue and the Hue logo are trademarks of Cloudera, Inc.')
+    ).toBeInTheDocument();
+  });
+
+  jest.mock('../../../config/hueConfig', () => ({
+    getLastKnownConfig: () => ({ hue_config: { is_admin: false } })
+  }));
+
+  test('it should not render Overview for non-admin users', () => {
+    const { queryByTestId } = render(<Overview />);
+    const overviewComponent = queryByTestId('overview-component');
+
+    expect(overviewComponent).toBeNull();
+  });
+
+
+  //verify table contents
+
+    // describe('Analytics Component', () => {
+    //   test('renders Analytics tab and can interact with the checkbox', async () => {
+    //     render(<Overview />);
+    //     const analyticsTabButton = screen.getByText('Analytics');
+    //     userEvent.click(analyticsTabButton); 
+
+    //     const checkbox = await screen.findByTitle('Check to enable usage analytics');
+
+    //     expect(checkbox).not.toBeChecked(); 
+    //     userEvent.click(checkbox);
+    //     await waitFor(() => expect(checkbox).toBeChecked());
+    //     expect(ApiHelper.updatePreferences).toHaveBeenCalledWith({ collect_usage: 'on' });
+    //     userEvent.click(checkbox);
+    //     await waitFor(() => expect(checkbox).not.toBeChecked()); 
+    //     expect(ApiHelper.updatePreferences).toHaveBeenCalledWith({ collect_usage: null });
+    //   });
+    // });
+});
+
+//click on hive app and api of hive is called (toHaveBeenCalled())
