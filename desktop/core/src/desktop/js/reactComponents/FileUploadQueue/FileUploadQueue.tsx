8f0c46bdca34ed08f66275702f03a6fbb2d70932
sanyam43 <sanyam4392@gmail.com>
2025-08-14 16:29:22 +0530

    Update FileUploadQueue.tsx

--- desktop/core/src/desktop/js/reactComponents/FileUploadQueue/FileUploadQueue.tsx
+++ desktop/core/src/desktop/js/reactComponents/FileUploadQueue/FileUploadQueue.tsx
@@ -13,6 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import React, { useEffect, useState } from 'react';
 import FileIcon from '@cloudera/cuix-core/icons/react/DocumentationIcon';
 import CloseIcon from '@cloudera/cuix-core/icons/react/CloseIcon';
