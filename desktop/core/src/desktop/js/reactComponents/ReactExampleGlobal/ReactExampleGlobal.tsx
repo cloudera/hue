30e8b719639003056cb96929da8fcf461b9b7eae
Björn Alm <balm@cloudera.com>
2025-08-26 10:11:21 +0200

    [ui-core] add cursor rules and improve code examples

--- desktop/core/src/desktop/js/reactComponents/ReactExampleGlobal/ReactExampleGlobal.tsx
+++ desktop/core/src/desktop/js/reactComponents/ReactExampleGlobal/ReactExampleGlobal.tsx
@@ -1,3 +1,19 @@
+// Licensed to Cloudera, Inc. under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  Cloudera, Inc. licenses this file
+// to you under the Apache License, Version 2.0 (the
+// 'License'); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
 import React, { useState } from 'react';
@@ -19,7 +35,8 @@ const ReactExampleGlobal = ({
   onClick,
   children,
   version = '1',
-  myObj
+  myObj,
+  className
 }: ReactExampleGlobalProps): JSX.Element => {
   const [isClicked, setIsClicked] = useState(false);
 
@@ -30,7 +47,7 @@ const ReactExampleGlobal = ({
 
   return (
     <button
-      className="react-example-global"
+      className={`react-example-global ${className || ''}`}
       disabled={isClicked}
       onClick={e => {
         onClick && onClick(e);
