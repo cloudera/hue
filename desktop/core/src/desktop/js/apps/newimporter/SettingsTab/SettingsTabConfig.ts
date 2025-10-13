4dd05f6c522aa6bb49a46463ba4911e833d6bfff
Ram Prasad Agarwal <work.ramprasad@gmail.com>
2025-10-13 11:15:38 +0530

    [ui-impoter] fix: settings tab for max width in input fields

--- desktop/core/src/desktop/js/apps/newimporter/SettingsTab/SettingsTabConfig.ts
+++ desktop/core/src/desktop/js/apps/newimporter/SettingsTab/SettingsTabConfig.ts
@@ -51,7 +51,7 @@ export const ADVANCED_SETTINGS_CONFIG: Record<string, SettingsFieldConfig[]> = {
   description: [
     {
       name: 'description',
-      type: FieldType.INPUT,
+      type: FieldType.TEXTAREA,
       label: 'Description',
       placeholder: "A table to store customer data imported from the marketing team's CRM.",
       tooltip:
@@ -139,7 +139,6 @@ export const ADVANCED_SETTINGS_CONFIG: Record<string, SettingsFieldConfig[]> = {
       label: 'Field',
       placeholder: 'Choose an option',
       options: DELIMITER_OPTIONS,
-      tooltip: 'Field delimiter',
       isHidden: (context: SettingsContext) => !context.customCharDelimiters
     },
     {
@@ -148,7 +147,6 @@ export const ADVANCED_SETTINGS_CONFIG: Record<string, SettingsFieldConfig[]> = {
       label: 'Array Map',
       placeholder: 'Choose an option',
       options: DELIMITER_OPTIONS,
-      tooltip: 'Array map delimiter',
       isHidden: (context: SettingsContext) => !context.customCharDelimiters
     },
     {
@@ -157,7 +155,6 @@ export const ADVANCED_SETTINGS_CONFIG: Record<string, SettingsFieldConfig[]> = {
       label: 'Struct',
       placeholder: 'Choose an option',
       options: DELIMITER_OPTIONS,
-      tooltip: 'Struct delimiter',
       isHidden: (context: SettingsContext) => !context.customCharDelimiters
     }
   ]
