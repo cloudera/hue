0ee9b0345664692bf9c8f4d570890337111b23ce
Sonia Park <sonia@Sonias-MacBook-Air.local>
2026-02-12 15:53:40 +0900

    upgrade oracledb version for support python3.11

--- desktop/core/generate_requirements.py
+++ desktop/core/generate_requirements.py
@@ -54,7 +54,7 @@ class RequirementsGenerator:
       "channels==4.2.2",
       "channels-redis==4.2.1",
       "configobj==5.0.9",
-      "oracledb==1.0.3",
+      "oracledb==2.0.0",
       "daphne==3.0.2",
       "Django==4.2.23",
       "django-auth-ldap==4.3.0",
